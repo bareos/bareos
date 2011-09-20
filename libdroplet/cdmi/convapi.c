b5e552174adfaf4e044352bf3e103971d1c43a1d
vr <vianney.rancurel@gmail.com>
2011-09-20 19:36:40 +0200

    add configure to package since autoconf might not work

--- libdroplet/cdmi/convapi.c
+++ libdroplet/cdmi/convapi.c
@@ -673,7 +673,193 @@ dpl_cdmi_get(dpl_ctx_t *ctx,
            unsigned int *data_lenp,
            dpl_dict_t **metadatap)
 {
-  return DPL_ENOTSUPP;
+  char          *host;
+  int           ret, ret2;
+  dpl_conn_t   *conn = NULL;
+  char          header[1024];
+  u_int         header_len;
+  struct iovec  iov[10];
+  int           n_iov = 0;
+  int           connection_close = 0;
+  char          *data_buf = NULL;
+  u_int         data_len;
+  dpl_dict_t    *headers_request = NULL;
+  dpl_dict_t    *headers_reply = NULL;
+  dpl_dict_t    *metadata = NULL;
+  dpl_req_t     *req = NULL;
+
+  DPL_TRACE(ctx, DPL_TRACE_CONV, "get bucket=%s resource=%s", bucket, resource);
+
+  req = dpl_req_new(ctx);
+  if (NULL == req)
+    {
+      ret = DPL_ENOMEM;
+      goto end;
+    }
+
+  dpl_req_set_method(req, DPL_METHOD_GET);
+
+  ret2 = dpl_req_set_bucket(req, bucket);
+  if (DPL_SUCCESS != ret2)
+    {
+      ret = ret2;
+      goto end;
+    }
+
+  ret2 = dpl_req_set_resource(req, resource);
+  if (DPL_SUCCESS != ret2)
+    {
+      ret = ret2;
+      goto end;
+    }
+
+  if (NULL != subresource)
+    {
+      ret2 = dpl_req_set_subresource(req, subresource);
+      if (DPL_SUCCESS != ret2)
+        {
+          ret = ret2;
+          goto end;
+        }
+    }
+
+  if (NULL != condition)
+    {
+      dpl_req_set_condition(req, condition);
+    }
+
+  //contact default host
+  dpl_req_rm_behavior(req, DPL_BEHAVIOR_VIRTUAL_HOSTING);
+
+  metadata = dpl_dict_new(13);
+  if (NULL == metadata)
+    {
+      ret = DPL_ENOMEM;
+      goto end;
+    }
+
+  //build request
+  ret2 = dpl_cdmi_req_build(req, &headers_request, NULL, NULL);
+  if (DPL_SUCCESS != ret2)
+    {
+      ret = DPL_FAILURE;
+      goto end;
+    }
+
+  host = dpl_dict_get_value(headers_request, "Host");
+  if (NULL == host)
+    {
+      ret = DPL_FAILURE;
+      goto end;
+    }
+
+  conn = dpl_conn_open_host(ctx, host, ctx->port);
+  if (NULL == conn)
+    {
+      ret = DPL_FAILURE;
+      goto end;
+    }
+
+  ret2 = dpl_req_gen_http_request(ctx, req, headers_request, NULL, header, sizeof (header), &header_len);
+  if (DPL_SUCCESS != ret2)
+    {
+      ret = DPL_FAILURE;
+      goto end;
+    }
+
+  iov[n_iov].iov_base = header;
+  iov[n_iov].iov_len = header_len;
+  n_iov++;
+
+  //final crlf
+  iov[n_iov].iov_base = "\r\n";
+  iov[n_iov].iov_len = 2;
+  n_iov++;
+
+  ret2 = dpl_conn_writev_all(conn, iov, n_iov, conn->ctx->write_timeout);
+  if (DPL_SUCCESS != ret2)
+    {
+      DPLERR(1, "writev failed");
+      connection_close = 1;
+      ret = DPL_ENOENT; //mapped to 404
+      goto end;
+    }
+
+  ret2 = dpl_read_http_reply(conn, 1, &data_buf, &data_len, &headers_reply);
+  if (DPL_SUCCESS != ret2)
+    {
+      if (DPL_ENOENT == ret2)
+        {
+          ret = DPL_ENOENT;
+          goto end;
+        }
+      else
+        {
+          DPLERR(0, "read http answer failed");
+          connection_close = 1;
+          ret = DPL_ENOENT; //mapped to 404
+          goto end;
+        }
+    }
+  else
+    {
+      connection_close = dpl_connection_close(ctx, headers_reply);
+
+      ret2 = dpl_cdmi_get_metadata_from_headers(headers_reply, metadata);
+      if (DPL_SUCCESS != ret2)
+        {
+          ret = DPL_FAILURE;
+          goto end;
+        }
+    }
+
+  (void) dpl_log_charged_event(ctx, "DATA", "OUT", data_len);
+
+  if (NULL != data_bufp)
+    {
+      *data_bufp = data_buf;
+      data_buf = NULL; //consume it
+    }
+
+  if (NULL != data_lenp)
+    *data_lenp = data_len;
+
+  if (NULL != metadatap)
+    {
+      *metadatap = metadata;
+      metadata = NULL; //consume it
+    }
+
+  ret = DPL_SUCCESS;
+
+ end:
+
+  if (NULL != data_buf)
+    free(data_buf);
+
+  if (NULL != conn)
+    {
+      if (1 == connection_close)
+        dpl_conn_terminate(conn);
+      else
+        dpl_conn_release(conn);
+    }
+
+  if (NULL != metadata)
+    dpl_dict_free(metadata);
+
+  if (NULL != headers_reply)
+    dpl_dict_free(headers_reply);
+
+  if (NULL != headers_request)
+    dpl_dict_free(headers_request);
+
+  if (NULL != req)
+    dpl_req_free(req);
+
+  DPRINTF("ret=%d\n", ret);
+
+  return ret;
 }
 
 /**
