ee171942580a593bde02980da15dc5c34761684d
Marco van Wieringen <marco.van.wieringen@bareos.com>
2013-06-11 16:47:19 +0200

    Update version date.

--- src/include/version.h
+++ src/include/version.h
@@ -1,7 +1,7 @@
 #undef  VERSION
 #define VERSION "13.2.0"
-#define BDATE   "09 April 2013"
-#define LSMDATE "09Apr13"
+#define BDATE   "12 June 2013"
+#define LSMDATE "12Jun13"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2012 Free Software Foundation Europe e.V.\n" \
                        "Copyright (C) 2013-2013 Bareos GmbH & Co. KG\n"
