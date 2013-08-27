7cebd146e7325096b201e8ade45754a54c1231c6
Greg Banks <gbanks@sgi.com>
2013-08-27 13:49:01 -0700

    Document the UKS and GC functions

--- libdroplet/scal/gc.c
+++ libdroplet/scal/gc.c
@@ -36,6 +36,27 @@
 #include <droplet/uks/uks.h>
 #include <droplet/scal/gc.h>
 
+/**
+ * @addtogroup scality
+ * @{
+ */
+
+/**
+ * Generate a GC key.
+ *
+ * Generates into @a id a special UKS key which does not
+ * refer to an actual object but which can be used to invoke the
+ * Scality GC service.  The object ID is stored in a `BIGNUM` structure,
+ * which you should create with `BN_new()` and free with `BN_free()`.
+ *
+ * To delete multiple objects in a single protocol call, use this GC key
+ * as the @a id parameter of a `dpl_put_id()` call, and pass the contents
+ * of a GC index as the PUT data.  See `dpl_scal_gc_index_init()` for
+ * details on how to create a GC index.
+ *
+ * @param[out] id the UKS key is returned here
+ * @param cl the UKS class (e.g. 2)
+ */
 void
 dpl_scal_gc_gen_key(BIGNUM *id,
 		    int cl)
@@ -58,6 +79,20 @@ dpl_scal_gc_gen_key(BIGNUM *id,
   ret = dpl_uks_set_replica(id, 0);
   assert(0 == ret);
 }
+
+/**
+ * Creates and initialises a new dbuf which can be used to create a GC
+ * index.  GC indexes are effectively a list of object IDs and can be used
+ * to delete multiple objects in a single API call.  Individual object IDs
+ * can be added to the GC index by calling `dpl_scal_gc_index_serialize()`.
+
+ * You should free the dbuf using `dpl_dbuf_free()` when you have
+ * finished using it.
+ *
+ * @param[out] indexp on success, a new dbuf is returned here
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+ */
 
 dpl_status_t
 dpl_scal_gc_index_init(dpl_dbuf_t **indexp)
@@ -91,13 +126,27 @@ dpl_scal_gc_index_init(dpl_dbuf_t **indexp)
 #define CHECKRES(expr) if (0 == (expr)) return DPL_ENOMEM;
 
 /**
- * @brief   Serialize a chunk entry into an index
+ * Add an object ID to a GC index.
  *
- * @param   _it     current item
- * @param   cb_arg  output buffer
+ * Adds an object ID to a GC index.  The GC index @a buffer should have
+ * been created using `dpl_scal_gc_index_init()`, the object ID
+ * @a chunkkey using `dpl_uks_gen_key()`.
  *
- * @return  success/failure
- **/
+ * If this function returns failure, the GC index cannot be used and
+ * you should free it immediately using `dpl_dbuf_free()`.
+ *
+ * If the object is a part of a logical file at some higher level of
+ * abstraction, you may pass the byte range of the object within that
+ * logical file as @a offset and @a size, but this is not necessary.
+ *
+ * @param chunkkey the binary UKS key to add, may not be NULL
+ * @param offset byte offset in some logical file (optional)
+ * @param size byte size in some logical file (optional)
+ * @param buffer the dbuf in which the GC index is being
+ *		 built, may not be NULL
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+ */
 dpl_status_t
 dpl_scal_gc_index_serialize(BIGNUM *chunkkey,
 			    uint64_t offset,
@@ -135,3 +184,5 @@ dpl_scal_gc_index_serialize(BIGNUM *chunkkey,
 
   return DPL_SUCCESS;
 }
+
+/** @} */
--- libdroplet/uks/uks.c
+++ libdroplet/uks/uks.c
@@ -35,12 +35,81 @@
 #include <droplet/uks/uks.h>
 #include <sys/param.h>
 
+/**
+ * @defgroup scality Scality specific functions
+ * @addtogroup scality
+ * @{
+ * Functions specific to Scality backends
+ *
+ * This module contains utility functions for dealing with the Scality
+ * backend cloud provider.
+ *
+ * Several of these functions deal with Scality's UKS (Universal Key Scheme)
+ * which is the binary object ID format used in Scality's RING software.
+ * UKS keys are 160 bits long and are divided into several fixed-length
+ * fields which encode specific information.  Fields include:
+ *
+ * @arg @b hashing/dispersion (24b) used to help distribute objects
+ * around multiple servers in a ring, normally an MD5 hash of the
+ * payload field.
+ *
+ * @arg @b payload (128b) Further information, broken out below.
+ *
+ * @arg @b class (4b).  Classes from 0-5 specify the number of *additional*
+ * copies to be stored, i.e. class 2 means 3 copies will be stored.
+ * Some of the class numbers 6-15 are used for other purposes.
+ *
+ * @arg @b replica (4b).  The replica number for classes 0-5, i.e. 0 for
+ * the first copy, 1 for the second copy.
+ *
+ * You can store anything you like in the @b payload field, but a
+ * recommended format is to use the following bit fields.
+ *
+ * @arg @b Object @b ID (64b).  Identifies an object, e.g. an inode number.
+ *
+ * @arg @b Volume @b ID (32b).  Identifies a virtual volume, e.g. a
+ * filesystem.
+ *
+ * @arg @b Application @b ID (8b).  Also known as @b Service @b ID.  A fixed
+ * field identifying your application, to allow multiple applications to use
+ * the same RING storage.  To avoid clashing with future Scality software,
+ * use a number greater than 0xc0.
+ *
+ * @arg @b Application @b Specific (24b), e.g. block offset within
+ * the file.
+ */
+
+
 //#define DPRINTF(fmt,...) fprintf(stderr, fmt, ##__VA_ARGS__)
 #define DPRINTF(fmt,...)
 
 #define BIT_SET(bit)   (entropy[(bit)/NBBY] |= (1<<((bit)%NBBY)))
 #define BIT_CLEAR(bit) (entropy[(bit)/NBBY] &= ~(1<<((bit)%NBBY)))
 
+/**
+ * Generate a UKS key from raw data
+ *
+ * Generates a binary UKS key in @a id using raw data for each of the
+ * bitfields in the generic format.  Note the class and replica fields
+ * should be set separately using `dpl_uks_set_class()` and
+ * `dpl_uks_set_replica()`.  The binary key @a id is stored in a
+ * `BIGNUM` structure, which you should create with `BN_new()` and
+ * free with `BN_free()`.
+ *
+ * Only use this function if you know what you are doing.  Note
+ * particularly that this function requires you to calculate and set the
+ * hashing/dispersion field yourself.  For most applications you should
+ * use either `dpl_uks_gen_key_ext()` or `dpl_uks_gen_key()` which will
+ * calculate the hashing/dispersion field for you.
+ *
+ * @param id the binary UKS key will be generated here
+ * @param hash will be used as the hashing/dispersion field
+ * @param oid will be used as the Object ID field
+ * @param volid will be used as the Volume ID field
+ * @param serviceid will be used as the Service ID field
+ * @param specific will be used as the Application Specific field
+ * @retval DPL_SUCCESS this function currently cannot fail
+ */
 dpl_status_t
 dpl_uks_gen_key_raw(BIGNUM *id,
 		    uint32_t hash,
@@ -128,6 +197,35 @@ dpl_uks_gen_key_raw(BIGNUM *id,
   return DPL_SUCCESS;
 }
 
+/**
+ * Set some fields in a binary UKS key.
+ *
+ * Sets some fields in a binary UKS key, according to a mask of which
+ * fields to set.  Fields not specified in the mask are preserved.
+ * Automatically recalculates the hashing/dispersion field.  You should
+ * also call `dpl_uks_set_class()` to set the class field. The binary
+ * key @a id is stored in a `BIGNUM` structure, which you should create
+ * with `BN_new()` and free with `BN_free()`.
+ *
+ * @param id the binary UKS key
+ * @param mask a bitmask of the following values indicating which fields to set
+ * @arg `DPL_UKS_MASK_OID` use the @a oid parameter as the Object ID field
+ * @arg `DPL_UKS_MASK_VOLID` use the @a volid parameter as the Volume ID field
+ * @arg `DPL_UKS_MASK_SERVICEID` use the @serviceid parameter as the
+ * Service ID field
+ * @arg `DPL_UKS_MASK_SPECIFIC` use the @specific parameter as the
+ * Application Specific field.
+ * @param oid will be used as the Object ID field if `DPL_UKS_MASK_OID`
+ * is present in @a mask
+ * @param volid will be used as the Volume ID field if
+ * `DPL_UKS_MASK_VOLID` is present in @a mask
+ * @param serviceid will be used as the Service ID field if
+ * `DPL_UKS_MASK_SERVICEID` is present in @a mask
+ * @param specific will be used as the Application Specific field if
+ * `DPL_UKS_MASK_SPECIFIC` is present in @a mask
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+*/
 dpl_status_t
 dpl_uks_gen_key_ext(BIGNUM *id,
 		    dpl_uks_mask_t mask,
@@ -271,6 +369,22 @@ dpl_uks_gen_key_ext(BIGNUM *id,
   return DPL_SUCCESS;
 }
 
+/**
+ * Generate a binary UKS key.
+ *
+ * Sets all the fields in a binary UKS key and automatically calculates
+ * the hashing/dispersion field.  You should also call `dpl_uks_set_class()`
+ * to set the class field.  The binary key @a id is stored in a `BIGNUM`
+ * structure, which you should create with `BN_new()` and free with `BN_free()`.
+ *
+ * @param id the binary UKS key
+ * @param oid will be used as the Object ID field
+ * @param volid will be used as the Volume ID field
+ * @param serviceid will be used as the Service ID field
+ * @param specific will be used as the Application Specific field
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+*/
 dpl_status_t
 dpl_uks_gen_key(BIGNUM *id,
                 uint64_t oid,
@@ -281,6 +395,18 @@ dpl_uks_gen_key(BIGNUM *id,
   return dpl_uks_gen_key_ext(id, ~0, oid, volid, serviceid, specific);
 }
 
+/**
+ * Set the class field in a UKS key.
+ *
+ * Set the class field in a UKS key.  The binary key @a id is stored in a
+ * `BIGNUM` structure, which you should create with `BN_new()` and free
+ * with `BN_free()`.
+ *
+ * @param k the binary UKS key
+ * @param cl will be used as the class field
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+ */
 dpl_status_t
 dpl_uks_set_class(BIGNUM *k,
                   int cl)
@@ -300,6 +426,18 @@ dpl_uks_set_class(BIGNUM *k,
   return DPL_SUCCESS;
 }
 
+/**
+ * Set the replica field in a UKS key.
+ *
+ * Set the replica field in a UKS key.  The binary key @a id is stored in a
+ * `BIGNUM` structure, which you should create with `BN_new()` and free
+ * with `BN_free()`.
+ *
+ * @param k the binary UKS key
+ * @param replica will be used as the replica field
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+ */
 dpl_status_t
 dpl_uks_set_replica(BIGNUM *k,
 		    int replica)
@@ -406,6 +544,21 @@ dpl_uks_gen_random_key(dpl_ctx_t *ctx,
   return ret;
 }
 
+/**
+ * Convert a binary UKS key to string form.
+ *
+ * Converts the binary UKS key in @a id to a string form in
+ * @a id_str.  The string form is suitable for use as the @a id
+ * parameter of the RESTful functions such as `dpl_put_id()`.
+ * The binary key @a id is stored in a `BIGNUM` structure, which
+ * you should create with `BN_new()` and free with `BN_free()`.
+ *
+ * @param id the binary UKS key
+ * @param[out] id_str on success the string form is written here, must
+ *  be at least `DPL_UKS_BCH_LEN+1` bytes long
+ * @retval DPL_SUCCESS on success, or
+ * @retval DPL_* a Droplet error code on failure
+ */
 dpl_status_t
 dpl_uks_bn2hex(const BIGNUM *id,
 	       char *id_str)
@@ -440,3 +593,5 @@ dpl_id_scheme_t dpl_id_scheme_uks =
     .name = "uks",
     .gen_random_key = dpl_uks_gen_random_key,
   };
+
+/** @} */
