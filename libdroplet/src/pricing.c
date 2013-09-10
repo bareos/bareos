3f22d9adc5bc18c3d13ab66c74e8fe8ce51bfa9d
Greg Banks <gbanks@sgi.com>
2013-09-09 18:39:16 -0700

    Fix inconsistency in buf_point_error()
    Errors in the pricing model file were reported partly to stderr and
    partly to stdout.  Report them entirely to stderr.

--- libdroplet/src/pricing.c
+++ libdroplet/src/pricing.c
@@ -883,18 +883,18 @@ buf_point_error(char *buf,
   for (i = 0;i < len;i++)
     {
       if (nl == lineno)
-        putchar(buf[i]);
+        fputc(buf[i], stderr);
 
       if (buf[i] == '\n')
         nl++;
     }
 
-  printf("\n");
+  fputc('\n', stderr);
   for (i = 1; i < byteno;i++)
     {
-      putchar(' ');
+      fputc(' ', stderr);
     }
-  printf("^\n");
+  fputs("^\n", stderr);
 }
 
 static struct dpl_parse_ctx *
