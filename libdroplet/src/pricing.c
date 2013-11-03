60a7e49f486d8eb8cd344acea89d217b2e20754b
Greg Banks <gbanks@sgi.com>
2013-11-03 10:26:20 -0800

    Remove extraneous \n from DPL_LOG() calls

--- libdroplet/src/pricing.c
+++ libdroplet/src/pricing.c
@@ -939,7 +939,7 @@ dpl_pricing_parse(dpl_ctx_t *ctx,
   fd = open(path, O_RDONLY);
   if (-1 == fd)
     {
-      DPL_LOG(ctx, DPL_ERROR, "error opening '%s': %s\n", path, strerror(errno));
+      DPL_LOG(ctx, DPL_ERROR, "error opening '%s': %s", path, strerror(errno));
       ret = DPL_FAILURE;
       goto end;
     }
