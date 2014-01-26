8af1dc0279d8f874791af627495949a70982b0ba
Marco van Wieringen <marco.van.wieringen@bareos.com>
2014-01-26 15:22:36 +0100

    Fix compiler warning.

--- src/win32/plugins/filed/mssqlvdi-fd.c
+++ src/win32/plugins/filed/mssqlvdi-fd.c
@@ -396,6 +396,7 @@ static bRC handlePluginEvent(bpContext *ctx, bEvent *event, void *value)
    switch (event->eventType) {
    case bEventLevel:
       p_ctx->backup_level = (int64_t)value;
+      retval = bRC_OK;
       break;
    case bEventRestoreCommand:
      /*
