4173da1f9cee0c1f71a1a578c4e6430b4abeadd0
Marco van Wieringen <marco.van.wieringen@bareos.com>
2014-07-28 16:59:16 +0200

    Sync LMDB code.

--- src/lmdb/lmdb.h
+++ src/lmdb/lmdb.h
@@ -194,7 +194,7 @@ typedef int mdb_filehandle_t;
 	MDB_VERINT(MDB_VERSION_MAJOR,MDB_VERSION_MINOR,MDB_VERSION_PATCH)
 
 /** The release date of this library version */
-#define MDB_VERSION_DATE	"July  8, 2014"
+#define MDB_VERSION_DATE	"July 24, 2014"
 
 /** A stringifier for the version info */
 #define MDB_VERSTR(a,b,c,d)	"LMDB " #a "." #b "." #c ": (" d ")"
@@ -411,7 +411,7 @@ typedef enum MDB_cursor_op {
 #define MDB_CURSOR_FULL	(-30787)
 	/** Page has not enough space - internal error */
 #define MDB_PAGE_FULL	(-30786)
-	/** Environment mapsize was changed by another process */
+	/** Database contents grew beyond environment mapsize */
 #define MDB_MAP_RESIZED	(-30785)
 	/** MDB_INCOMPATIBLE: Operation and DB incompatible, or DB flags changed */
 #define MDB_INCOMPATIBLE	(-30784)
@@ -785,12 +785,15 @@ int  mdb_env_get_fd(MDB_env *env, mdb_filehandle_t *fd);
 	 * This function should be called after #mdb_env_create() and before #mdb_env_open().
 	 * It may be called at later times if no transactions are active in
 	 * this process. Note that the library does not check for this condition,
-	 * the caller must ensure it explicitly. The new size takes effect
-	 * immediately for the current process but will not be persisted to
-	 * any others until a write transaction has been committed by the
-	 * current process.
+	 * the caller must ensure it explicitly.
 	 *
-	 * If the mapsize is changed by another process, #mdb_txn_begin() will
+	 * The new size takes effect immediately for the current process but
+	 * will not be persisted to any others until a write transaction has been
+	 * committed by the current process. Also, only mapsize increases are
+	 * persisted into the environment.
+	 *
+	 * If the mapsize is increased by another process, and data has grown
+	 * beyond the range of the current mapsize, #mdb_txn_begin() will
 	 * return #MDB_MAP_RESIZED. This function may be called with a size
 	 * of zero to adopt the new size.
 	 *
