ea4f8b6fc249c5b6d2d020c73e3f22b8ab4dc2e4
Frank Bergkemper <frank.bergkemper@dass-it.de>
2014-09-03 12:27:07 +0200

    Application entry point changed to dashboard

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Dashboard\Controller\Dashboard',
                         'action'     => 'index',
                     ),
                 ),
