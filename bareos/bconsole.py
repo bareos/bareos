b2145c79b66685cd92b991d2b5f12bdd22d2f778
Joerg Steffens <joerg.steffens@dass-it.de>
2014-09-12 12:17:26 +0200

    added bsock status codes

--- bareos/bconsole.py
+++ bareos/bconsole.py
@@ -16,6 +16,81 @@ import struct
 import sys
 import time
 
+class bsock:
+    """
+    translated enum from https://github.com/bareos/bareos/blob/master/src/lib/bsock.h
+    """
+    BNET_EOD            = -1    #,          /* End of data stream, new data may follow */
+    BNET_EOD_POLL       = -2    #,          /* End of data and poll all in one */
+    BNET_STATUS         = -3    #,          /* Send full status */
+    BNET_TERMINATE      = -4    #,          /* Conversation terminated, doing close() */
+    BNET_POLL           = -5    #,          /* Poll request, I'm hanging on a read */
+    BNET_HEARTBEAT      = -6    #,          /* Heartbeat Response requested */
+    BNET_HB_RESPONSE    = -7    #,          /* Only response permited to HB */
+    BNET_xxxxxxPROMPT   = -8    #,          /* No longer used -- Prompt for subcommand */
+    BNET_BTIME          = -9    #,          /* Send UTC btime */
+    BNET_BREAK          = -10   #,         /* Stop current command -- ctl-c */
+    BNET_START_SELECT   = -11   #,         /* Start of a selection list */
+    BNET_END_SELECT     = -12   #,         /* End of a select list */
+    BNET_INVALID_CMD    = -13   #,         /* Invalid command sent */
+    BNET_CMD_FAILED     = -14   #,         /* Command failed */
+    BNET_CMD_OK         = -15   #,         /* Command succeeded */
+    BNET_CMD_BEGIN      = -16   #,         /* Start command execution */
+    BNET_MSGS_PENDING   = -17   #,         /* Messages pending */
+    BNET_MAIN_PROMPT    = -18   #,         /* Server ready and waiting */
+    BNET_SELECT_INPUT   = -19   #,         /* Return selection input */
+    BNET_WARNING_MSG    = -20   #,         /* Warning message */
+    BNET_ERROR_MSG      = -21   #,         /* Error message -- command failed */
+    BNET_INFO_MSG       = -22   #,         /* Info message -- status line */
+    BNET_RUN_CMD        = -23   #,         /* Run command follows */
+    BNET_YESNO          = -24   #,         /* Request yes no response */
+    BNET_START_RTREE    = -25   #,         /* Start restore tree mode */
+    BNET_END_RTREE      = -26   #,         /* End restore tree mode */
+    BNET_SUB_PROMPT     = -27   #,         /* Indicate we are at a subprompt */
+    BNET_TEXT_INPUT     = -28   #          /* Get text input from user */
+
+    description = {
+        BNET_EOD:           "End of data stream, new data may follow",
+        BNET_EOD_POLL:      "End of data and poll all in one",
+        BNET_STATUS:        "Send full status",
+        BNET_TERMINATE:     "Conversation terminated, doing close()",
+        BNET_POLL:          "Poll request, I'm hanging on a read",
+        BNET_HEARTBEAT:     "Heartbeat Response requested",
+        BNET_HB_RESPONSE:   "Only response permited to HB",
+        BNET_xxxxxxPROMPT:  "No longer used -- Prompt for subcommand",
+        BNET_BTIME:         "Send UTC btime",
+        BNET_BREAK:         "Stop current command -- ctl-c",
+        BNET_START_SELECT:  "Start of a selection list",
+        BNET_END_SELECT:    "End of a select list",
+        BNET_INVALID_CMD:   "Invalid command sent",
+        BNET_CMD_FAILED:    "Command failed",
+        BNET_CMD_OK:        "Command succeeded",
+        BNET_CMD_BEGIN:     "Start command execution",
+        BNET_MSGS_PENDING:  "Messages pending",
+        BNET_MAIN_PROMPT:   "Server ready and waiting",
+        BNET_SELECT_INPUT:  "Return selection input",
+        BNET_WARNING_MSG:   "Warning message",
+        BNET_ERROR_MSG:     "Error message -- command failed",
+        BNET_INFO_MSG:      "Info message -- status line",
+        BNET_RUN_CMD:       "Run command follows",
+        BNET_YESNO:         "Request yes no response",
+        BNET_START_RTREE:   "Start restore tree mode",
+        BNET_END_RTREE:     "End restore tree mode",
+        BNET_SUB_PROMPT:    "Indicate we are at a subprompt",
+        BNET_TEXT_INPUT:    "Get text input from user"
+    }
+
+    @staticmethod
+    def get_description( code ):
+        try:
+            description = bsock.description[code]
+        except KeyError:
+            #self.logger.error( "unknown bsock code " + code )
+            return
+        else:
+            return description
+
+
 class bconsole:
     '''use to send and receive the response from director'''
 
@@ -27,11 +102,6 @@ class bconsole:
     # If not, we assume, something is wrong with our connection
     DIRECTOR_MIN_COMMANDS=10
     RE_dot_help = re.compile( r"(?P<command>\w+) (?P<description>.*) -- *(?P<parameter>.*)" )
-
-    STATUS_CODES={
-        -1:  "prompt",
-        -27: "sub prompt",
-    }
 
     def __init__(self, host="localhost", port=9101, dirname=None ):
         self.logger = logging.getLogger()
@@ -112,7 +182,7 @@ class bconsole:
                     # get the message
                     submsg_length = struct.unpack("!i", header)[0]
                     if submsg_length <= 0:
-                        self.logger.debug( "msg len: " + str(submsg_length) )
+                        #self.logger.debug( "msg len: " + str(submsg_length) )
                         self.setStatus( submsg_length )
                         return msg
                     submsg = ""
@@ -128,12 +198,8 @@ class bconsole:
 
     def setStatus( self, status ):
         self.status = status
-        try:
-            status_text = self.STATUS_CODES[status]
-        except KeyError:
-            self.logger.warning( "set to unknown status code " + status )
-        else:
-            self.logger.debug( status_text + " (" + str(status) + ")" )
+        status_text = bsock.get_description( status )
+        self.logger.debug( str(status_text) + " (" + str(status) + ")" )
 
 
     def login(self, password=None, password_plain=None, password_md5=None, clientname="*UserAgent*"):
