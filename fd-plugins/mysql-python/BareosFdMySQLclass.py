dcfc13effcacf6963dfd66f116ea6d480e0f580f
aussendorf <maik.aussendorf@bareos.com>
2015-04-13 12:42:18 +0200

    error handling, more paramaters, documentation

--- fd-plugins/mysql-python/BareosFdMySQLclass.py
+++ fd-plugins/mysql-python/BareosFdMySQLclass.py
@@ -8,6 +8,7 @@
 from bareosfd import *
 from bareos_fd_consts import *
 import os
+from subprocess import *
 from  BareosFdPluginBaseclass import *
 import BareosFdWrapper
 
@@ -19,34 +20,70 @@ class BareosFdMySQLclass (BareosFdPluginBaseclass):
     def __init__(self, context, plugindef):
         BareosFdPluginBaseclass.__init__(self, context, plugindef)
         self.file=None
-        DebugMessage(context, 100, "since: %d\n"%(self.since));
-        DebugMessage(context, 100, "level: %d\n"%(self.level));
 
     def parse_plugin_definition(self,context, plugindef):
         '''
         '''
         BareosFdPluginBaseclass.parse_plugin_definition(self, context, plugindef)
 
+        # mysql host and credentials, by default we use localhost and root and
+        # prefer to have a my.cnf with mysql credentials
+        self.mysqlconnect = ''
+
+        if 'dumpbinary' in self.options:
+            self.dumpbinary = self.options['dumpbinary']
+        else:
+            self.dumpbinary = "mysqldump"
+
+        if 'dumpoptions' in self.options:
+            self.dumpoptions = self.options['dumpoptions']
+        else:
+            self.dumpoptions = " --events --single-transaction"
+
+        if 'mysqlhost' in self.options:
+            self.mysqlhost = self.options['mysqlhost']
+            self.mysqlconnect += " -h " + self.mysqlhost
+
+        if 'mysqluser' in self.options:
+            self.mysqluser = self.options['mysqluser']
+            self.mysqlconnect += " -u " + self.mysqluser
+
+        if 'mysqlpassword' in self.options:
+            self.mysqlpassword = self.options['mysqlpassword']
+            self.mysqlconnect += " --password=" + self.mysqlpassword
+
         # if plugin has db configured (a list of comma separated databases to backup
-        # we use it here as lists of databases to backup
-        if self.options['db']:
+        # we use it here as list of databases to backup
+        if 'db' in self.options:
             self.databases = self.options['db'].split(',')
         # Otherwise we backup all existing databases
         else:
-            # TODO: exception handling, prereq check: does mysql binary exist?
-            f = os.popen("mysql -B -N -e 'show databases'")
-            self.databases = f.read().splitlines()
+            showDbCommand = "mysql %s -B -N -e 'show databases'" %self.mysqlconnect
+            showDb = Popen(showDbCommand, shell=True, stdout=PIPE, stderr=PIPE)
+            self.databases = showDb.stdout.read().splitlines()
+            showDb.wait()
+            returnCode = showDb.poll()
+            if returnCode != 0:
+                (stdOut, stdError) = showDb.communicate()    
+                JobMessage(context, bJobMessageType['M_FATAL'], "No databases specified and show databases failed. %s" %stdError);
+                DebugMessage(context, 10, "Failed mysql command: '%s'" %showDbCommand)
+                return bRCs['bRC_Error'];
 
-        DebugMessage(context, 100, "databases: %s\n"%(self.databases));
+        DebugMessage(context, 100, "databases to backup: %s\n" %(self.databases));
         return bRCs['bRC_OK'];
 
 
     def start_backup_file(self,context, savepkt):
         '''
+        This method is called, when Bareos is ready to start backup a file
+        For each database to backup we create a mysqldump subprocess, wrting to
+        the pipe self.stream.stdout
         '''
         DebugMessage(context, 100, "start_backup called\n");
         if not self.databases:
-            DebugMessages(context,100,"No databases to backup")
+            DebugMessage(context,100,"No databases to backup")
+            JobMessage(context, bJobMessageType['M_ERROR'], "No databases to backup.\n");
+            return bRCs['bRC_Skip']
 
         db = self.databases.pop()
         statp = StatPacket()
@@ -54,19 +91,20 @@ class BareosFdMySQLclass (BareosFdPluginBaseclass):
         savepkt.fname = "/_mysqlbackups_/"+db+".sql"
         savepkt.type = bFileType['FT_REG']
 
-        # TODO: exception handling, prereq check: does mysqldump binary exist?
-        # TODO: make dumpcommand and options configurable
-        dumpcommand = ("/usr/bin/mysqldump %s --events --single-transaction " %db)
+        dumpcommand = ("%s %s %s %s" %(self.dumpbinary, db, self.mysqlconnect, self.dumpoptions))
         DebugMessage(context, 100, "Dumper: '" + dumpcommand + "'\n")
-        #self.stream = os.popen("/usr/bin/mysqldump %s --events --single-transaction " %db)
-        # TODO: exception handling
-        self.stream = os.popen(dumpcommand)
+        self.stream = Popen(dumpcommand, shell=True, stdout=PIPE, stderr=PIPE)
 
-        JobMessage(context, bJobMessageType['M_INFO'], "Starting backup of " + savepkt.fname + "\n");
+        JobMessage(context, bJobMessageType['M_DEBUG'], "Starting backup of " + savepkt.fname + "\n");
         return bRCs['bRC_OK'];
 
+
     def plugin_io(self, context, IOP):
-        DebugMessage(context, 100, "plugin_io called with " + str(IOP) + "\n");
+        '''
+        Called for io operations. We read from pipe into buffers or on restore
+        create a file for each database and write into it.
+        '''
+        DebugMessage(context, 100, "plugin_io called with " + str(IOP.func) + "\n");
 
         if IOP.func == bIOPS['IO_OPEN']:
             try:
@@ -81,7 +119,7 @@ class BareosFdMySQLclass (BareosFdPluginBaseclass):
 
         elif IOP.func == bIOPS['IO_READ']:
             IOP.buf = bytearray(IOP.count)
-            IOP.status = self.stream.readinto(IOP.buf)
+            IOP.status = self.stream.stdout.readinto(IOP.buf)
             IOP.io_errno = 0
             return bRCs['bRC_OK']
         
@@ -109,12 +147,23 @@ class BareosFdMySQLclass (BareosFdPluginBaseclass):
 
     def end_backup_file(self, context):
         '''
+        Check, if dump was successfull.
         '''
-        DebugMessage(context, 100, "end_backup_file() entry point in Python called\n")
+        returnCode = self.stream.poll()
+        DebugMessage(context, 100, "end_backup_file() entry point in Python called. Returncode: %d\n" %self.stream.returncode)
+        if returnCode != 0:
+            (stdOut, stdError) = self.stream.communicate()
+            if stdError == None:
+                stdError = ''
+            JobMessage(context, bJobMessageType['M_ERROR'], "Dump command returned non-zero value: %d, message: %s\n" %(returnCode,stdError));
+            
         if self.databases:
-            return bRCs['bRC_More']
+                return bRCs['bRC_More']
         else:
-            return bRCs['bRC_OK'];
+            if returnCode == 0:
+                return bRCs['bRC_OK'];
+            else:
+                return bRCs['bRC_Error']
 
 
 # vim: ts=4 tabstop=4 expandtab shiftwidth=4 softtabstop=4
--- fd-plugins/mysql-python/README.md
+++ fd-plugins/mysql-python/README.md
@@ -4,7 +4,9 @@ This plugin makes a backup of each database found in mysql in a single file.
 For restore select the needed database file, found in `/_mysqlbackups_` in the catalog.
 
 ## Prerequisites
-The `mysqldump` and `mysql` command must be installed and user root (running the fd) must have read-access to the databases.
+The `mysqldump` and `mysql` command must be installed and user root (running the fd) must have read-access to the databases. See below, if you
+want to use another user / password or want to restrict database access for the backup user.
+
 You need the packages bareos-filedaemon-python-plugin installed on your client.
 
 ## Configuration
@@ -36,6 +38,7 @@ FileSet {
 
 #### Options ####
 
+##### Databases #####
 By default all found databases are backuped, if the paramater 'db' is unset. You can have the plugin only certain databases saved, if you add
 the parameter db=db1,db2, with a comma separated list of datbases.
 Example plugin string:
@@ -43,3 +46,24 @@ Example plugin string:
                Plugin = "python:module_path=/usr/lib64/bareos/plugins:module_name=bareos-fd-mysql:db=test,mysql"
 ```
 This will backup only the databases with name 'test' and 'mysql'.
+
+##### Database host #####
+
+By default the mysql connection is made to localhost. If want to connect to another host, use
+```
+mysqlhost=hostname
+```
+
+##### Database access /  user and password  #####
+
+By default the root user (the user, which runs the Bareos filedaemon) is used to connect to the database. We recommend that you set
+a password for the user and store it in your my.cnf. You can however set a user and / or password as plugin options:
+```
+mysqluser=username:mysqlpassword=secret
+```
+
+Minimum privileges for the backup user are:
+```
+GRANT USAGE ON *.* TO 'bareos'@'localhost' IDENTIFIED BY 'bareos';
+GRANT SELECT, LOCK TABLES, SHOW VIEW, EVENT, TRIGGER ON *.* TO 'bareos'@'localhost'
+```
