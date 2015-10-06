a9adcb21d67a195fb056cacb9912252c6c686fd7
Frank Bergkemper <frank.bergkemper@dass-it.de>
2015-10-06 15:21:58 +0200

    Media: Display media details fix
    More flexible details routing.
    
    Fixes #532: webui fails to display media details
    
    Signed-off-by: Frank Bergkemper <frank.bergkemper@dass-it.de>

--- module/Media/config/module.config.php
+++ module/Media/config/module.config.php
@@ -43,7 +43,7 @@ return array(
 					'route' => '/media[/][:action][/][:id][order_by/:order_by][/:order][/][limit/:limit]',
 					'constraints' => array(
 						'action' => '(?!\blimit\b)(?!\border_by\b)[a-zA-Z][a-zA-Z0-9_-]*',
-						'id' => '[a-zA-Z][a-zA-Z0-9_-]*',
+						'id' => '[a-zA-Z0-9_-]*',
 						'order_by' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                                 'order' => 'ASC|DESC',
                                                 'limit' => '[0-9]+',
