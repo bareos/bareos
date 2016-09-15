a4f8a977d5413bfb6b509acea134c9047fb38be9
Joerg Steffens <joerg.steffens@bareos.com>
2016-09-15 17:14:09 +0200

    some fixes

--- misc/bareos-migrate-config/bareos-migrate-config.sh
+++ misc/bareos-migrate-config/bareos-migrate-config.sh
@@ -11,8 +11,8 @@ BCONSOLE=${BCONSOLE:-/usr/bin/bconsole}
 
 bconsole()
 {
-  local out=$1
-  local cmd=$2
+  local out="$1"
+  local cmd="$2"
   local temp=`mktemp`
   printf "%s\n%s\n%s\n" "gui on" "@out $temp" "$cmd" | $BCONSOLE > /dev/null
   # The send command is also written to the output. Remove it.
@@ -20,20 +20,25 @@ bconsole()
   grep -v -e "$cmd" "$temp" > "$out"
 }
 
-for restype in catalog client console counter director fileset job jobdefs message pool profile schedule storage; do
+for restype in catalog client console counter director fileset job jobdefs messages pool profile schedule storage; do
     printf "\n%s:\n" "$restype"
     printf "==========\n"
     mkdir $restype 2>/dev/null
-    dotcommand=".${restype}"
-    if [ $restype = message ]; then
-        dotcommand=".msgs"
+    if [ $restype = director ]; then
+        bconsole "$restype/bareos-dir.conf" "show director"
+    else
+        dotcommand=".${restype}"
+        if [ $restype = messages ]; then
+            dotcommand=".msgs"
+        fi
+        TEMP=`mktemp`
+        bconsole "$TEMP" "$dotcommand"
+        cat $TEMP
+        while read res; do
+            #CONFFILENAME=`sed 's/ /_/g' <<< ${res}`
+            CONFFILENAME="${res}"
+            bconsole "$restype/${CONFFILENAME}.conf" "show ${restype}=\"${res}\""
+        done < $TEMP
     fi
-    TEMP=`mktemp`
-    bconsole "$TEMP" "$dotcommand"
-    cat $TEMP
-    while read res; do
-      #CONFFILENAME=`sed 's/ /_/g' <<< ${res}`
-      CONFFILENAME="${res}"
-      bconsole "$restype/${CONFFILENAME}.conf" "show ${restype}=\"${res}\""
-    done < $TEMP
 done
+
