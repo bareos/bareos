721c56e91196d25586c74ca0cb13a76087739754
Joerg Steffens <joerg.steffens@bareos.com>
2016-12-02 18:48:47 +0100

    fix and active tests that creates sparse files
    replaces the gigaslam tool by scripts/create_sparse_file.sh,
    to be able to create sparse files at arbitrary sizes.
    Creating a sparse file of 100M (real size about 8K).

---
+++ scripts/create_sparse_file.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+filename=${1:-sparse.dat}
+size=${2:-100M}
+
+echo "start" > $filename
+dd if=/dev/zero of=$filename bs=1 count=0 seek=$size 2>/dev/null
+echo "end" >> $filename
+
+size="`du --block-size=1 --apparent-size ${filename} | cut -f 1`"
+realsize="`du --block-size=1 ${filename} | cut -f 1`"
+
+printf "$filename created.\n"
+printf "size=%s\n" "$size"
+printf "realsize=%s\n" "$realsize"
+
+if ! [ "$realsize" -lt "$size" ]; then
+    printf "ERROR: realsize has to be smaller than size.\n"
+    exit 1
+fi
