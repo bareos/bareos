927b04d60246b3c772d20be11b4b02c3c1a77c87
Frank Bergkemper <frank.bergkemper@bareos.com>
2017-05-24 11:26:27 +0200

    Composer: Cleanup and Update
    - Removes composer.phar from source tree
    - Updates composer.json (minimum required PHP Version)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit6df5b8a1471c61873b9f5b638848d4d7
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit6df5b8a1471c61873b9f5b638848d4d7', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
