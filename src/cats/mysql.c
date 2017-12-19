d8a5b3efccedf8f6f6f3831b6dea64dbbaa7b1e3
Philipp Storz <philipp.storz@bareos.com>
2017-12-19 12:24:40 +0100

    mysql: use mysql_options instead of m_instance to set reconnect setting

--- src/cats/mysql.c
+++ src/cats/mysql.c
@@ -147,6 +147,7 @@ bool B_DB_MYSQL::db_open_database(JCR *jcr)
 {
    bool retval = false;
    int errstat;
+   my_bool reconnect = 1;
 
    P(mutex);
    if (m_connected) {
@@ -191,7 +192,7 @@ bool B_DB_MYSQL::db_open_database(JCR *jcr)
       bmicrosleep(5,0);
    }
 
-   m_instance.reconnect = 1;             /* so connection does not timeout */
+   mysql_options(&m_instance, MYSQL_OPT_RECONNECT, &reconnect);    /* so connection does not timeout */
    Dmsg0(50, "mysql_real_connect done\n");
    Dmsg3(50, "db_user=%s db_name=%s db_password=%s\n", m_db_user, m_db_name,
         (m_db_password == NULL) ? "(NULL)" : m_db_password);
