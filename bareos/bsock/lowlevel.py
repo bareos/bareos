fa8ff57d72aa4d0a883fc22e97fa7df977574364
spirit <spirit@fcc>
2018-01-16 15:55:54 +0300

    Fix recv_msg

--- bareos/bsock/lowlevel.py
+++ bareos/bsock/lowlevel.py
@@ -205,7 +205,7 @@ class LowLevel(object):
                         # Bareos indicates end of command result by line starting with 4 digits
                         if match:
                             self.logger.debug("msg \"{0}\" matches regex \"{1}\"".format(self.receive_buffer.strip(), regex))
-                            result = self.receive_buffer[0:lastlineindex] + self.receive_buffer[lastlineindex:match.end()]
+                            result = self.receive_buffer[0:lastlineindex] + self.receive_buffer[lastlineindex:lastlineindex+match.end()]
                             self.receive_buffer = self.receive_buffer[lastlineindex+match.end()+1:]
                             return result
                         #elif re.search("^\d\d\d\d .*$", msg, re.MULTILINE):
