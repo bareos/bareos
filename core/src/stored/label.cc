82364db8ab49cedb80e34fedf373de2002de06e5
Philipp Storz <philipp.storz@bareos.com>
2018-05-04 19:17:43 +0200

    cleanup: fix windows build because of get_current_time()

--- core/src/stored/label.cc
+++ core/src/stored/label.cc
@@ -655,7 +655,7 @@ static void create_volume_label_record(DeviceControlRecord *dcr, Device *dev, De
       /* OLD WAY DEPRECATED */
       ser_float64(dev->VolHdr.label_date);
       ser_float64(dev->VolHdr.label_time);
-      GetCurrentTime(&dt);
+      get_current_time(&dt);
       dev->VolHdr.write_date = dt.julian_day_number;
       dev->VolHdr.write_time = dt.julian_day_fraction;
    }
