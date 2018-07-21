0fca1075cc92ccd15e7a5bdba569d3cc461a1ab3
Frank Ueberschar <frank.ueberschar@bareos.com>
2018-07-21 12:15:04 +0200

    unittests-lib: added BareosDeamonType
    needed for compatibility:
    
      bs->local_daemon_type_ = BareosDaemonType::kDirector;
      bs->remote_daemon_type_ = BareosDaemonType::kConsole;

--- core/src/lib/unittests/bsock_test.cc
+++ core/src/lib/unittests/bsock_test.cc
@@ -29,8 +29,11 @@
 #include "console/console_conf.h"
 
 #include "include/jcr.h"
+
 class UaContext {
   BareosSocket *UA_sock;
+  BareosDaemonType local_daemon_type_;
+  BareosDaemonType remote_daemon_type_;
 };
 class StorageResource;
 #include "dird/authenticate.h"
@@ -99,6 +102,8 @@ void start_bareos_server(std::promise<bool> *promise, std::string console_name,
   BareosSocket *bs;
   bs = New(BareosSocketTCP);
   bs->sleep_time_after_authentication_error = 0;
+  bs->local_daemon_type_ = BareosDaemonType::kDirector;
+  bs->remote_daemon_type_ = BareosDaemonType::kConsole;
   bs->fd_ = newsockfd;
   bs->SetWho(bstrdup("client"));
   memset(&bs->peer_addr, 0, sizeof(bs->peer_addr));
@@ -157,6 +162,8 @@ int connect_to_server(std::string console_name, std::string console_password,
   BareosSocketTCP *UA_sock = New(BareosSocketTCP);
   UA_sock->sleep_time_after_authentication_error = 0;
   jcr.dir_bsock = UA_sock;
+  UA_sock->local_daemon_type_ = BareosDaemonType::kConsole;
+  UA_sock->remote_daemon_type_ = BareosDaemonType::kDirector;
 
   if (!UA_sock->connect(NULL, 1, 15, heart_beat, "Director daemon", (char *)server_address.c_str(),
                         NULL, server_port, false)) {
