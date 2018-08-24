2b94ca92951ccbda07cb6cab260c869fbead096a
Philipp Storz <philipp.storz@bareos.com>
2018-08-24 16:28:52 +0200

    docs: developers: enable plantuml

--- docs/manuals/en/developers/source/conf.py
+++ docs/manuals/en/developers/source/conf.py
@@ -31,7 +31,11 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinxcontrib.plantuml']
+
+plantuml = 'java  -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
+
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
