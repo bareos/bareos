f2aadd4398363cbfc1041c20c1977c3c58362145
Philipp Storz <philipp.storz@bareos.com>
2019-01-09 03:43:43 -0800

    NDMP: check if callbacks are set in ndmca_query_register_callbacks()

--- core/src/ndmp/ndma_cops_query.c
+++ core/src/ndmp/ndma_cops_query.c
@@ -44,9 +44,9 @@ void ndmca_query_register_callbacks (struct ndm_session *sess,
       struct ndmca_query_callbacks *callbacks)
 {
    /*
-    * Only allow one register.
+    * Only allow one register and make sure callbacks are set
     */
-   if (!sess->query_cbs) {
+   if (!sess->query_cbs && callbacks) {
       sess->query_cbs = NDMOS_API_MALLOC (sizeof(struct ndmca_query_callbacks));
       if (sess->query_cbs) {
          memcpy (sess->query_cbs, callbacks, sizeof(struct ndmca_query_callbacks));
