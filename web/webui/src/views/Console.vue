dd4d1939718b5c03ae8562d05f1ceaf4f9b649ab
Frank Bergkemper <frank.bergkemper@bareos.com>
2019-02-13 15:18:56 +0100

    webui: xterm.js input processing

--- web/webui/src/views/Console.vue
+++ web/webui/src/views/Console.vue
@@ -7,7 +7,9 @@
 </template>
 
 <script>
+
 import Console from '../components/Console'
+
 export default {
   data () {
     return {
@@ -19,85 +21,56 @@ export default {
       input: 0,
       command: '',
 			 terminal: {
-			        pid: 1,
-			        name: 'terminal',
-			        cols: 1000,
-              rows: 1000,
-              cursorBlink: true
-			      }
+			  pid: 1,
+			  name: 'terminal',
+			  cols: 1000,
+        rows: 1000,
+        cursorBlink: true
+      }
+      
     }
   },
   methods: {
 
   },
   mounted () {
+    this.$refs.xterm.term.write('* ')
 
-    // this.$refs.xterm.term.write('* help\n')
-    // this.$socket.send("help")
-    // console.log(this.$store.state.socket.message)
-    // this.$store.state.socket.message.forEach(element => {
-    //   element = element.replace(/\n/g, '\r\n')
-    //   this.$refs.xterm.term.write(element)
-    // })
+    this.$refs.xterm.term.on('data', (data) => {
+      const code = data.charCodeAt(0)
 
-    //this.$refs.xterm.term.fit()
-
-    this.$refs.xterm.term.on('key', (key, ev) => {
-      console.log(key.charCodeAt(0))
-      
-      if(key.charCodeAt(this.input) == 118) {
-        this.command += 'v'
-        this.$refs.xterm.term.write('v')
-      }
-
-      if(key.charCodeAt(this.input) == 101) {
-        this.command += 'e'
-        this.$refs.xterm.term.write('e')
-      }
-
-      if(key.charCodeAt(this.input) == 114) {
-        this.command += 'r'
-        this.$refs.xterm.term.write('r')
-      }
-
-      if(key.charCodeAt(this.input) == 115) {
-        this.command += 's'
-        this.$refs.xterm.term.write('s')
-      }
-
-      if(key.charCodeAt(this.input) == 105) {
-        this.command += 'i'
-        this.$refs.xterm.term.write('i')
-      }
-
-      if(key.charCodeAt(this.input) == 111) {
-        this.command += 'o'
-        this.$refs.xterm.term.write('o')
-      }
-
-      if(key.charCodeAt(this.input) == 110) {
-        this.command += 'n'
-        this.$refs.xterm.term.write('n')
-      }
-
-      if(key.charCodeAt(this.input) == 13) {
-        this.$refs.xterm.term.write('\n')
+      if (code == 13) {
+        // CR received
+        console.log(this.command)
         this.$socket.send(this.command)
-        this.input = 0
-        this.command = ''
+
         this.$store.state.socket.message.forEach(element => {
           element = element.replace(/\n/g, '\r\n')
           this.$refs.xterm.term.write(element)
         })
+        
+        this.$refs.xterm.term.writeln('')
         this.$refs.xterm.term.fit()
+
         this.$store.state.socket.message = []
+
+        // this.$refs.xterm.term.write("\r\nYou typed: '" + this.command + "'\r\n")
+        this.$refs.xterm.term.write('* ')
+        this.command = ''
+      } else if (code < 32 || code == 127) {
+        // Control codes
+
+      } else {
+        // Visible
+        this.$refs.xterm.term.write(data)
+        this.command += data
       }
 
     })
     
   },
   created () {
-    // this.$store.state.socket.message = []
+    this.$store.state.socket.message = []
   },
   destroyed () {
     this.$store.state.socket.message = []
