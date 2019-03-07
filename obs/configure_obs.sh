0efdff661d501c929b48999585b079b37308751b
Philipp Storz <philipp.storz@bareos.com>
2019-03-07 15:04:11 +0100

    fix

--- obs/configure_obs.sh
+++ obs/configure_obs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 OSC=osc
 
-rm -Rvf jenkins:master/${pkg}/
-
+rm -Rvf jenkins:master
 # update project config and create project if it does not exist:
 
 cat prj.xml.in  | sed 's#@BASEPROJECT@#jenkins#g' | sed 's#@BRANCH@#master#' | $OSC meta prj jenkins:master -F -
@@ -14,17 +13,20 @@ $OSC meta prjconf jenkins:master -f  prjconf
 # for every package, create it via setting the package meta info:
 
 for pkg in `cat packages`; do
-cat ${pkg}/_meta.in  | sed 's#@BASEPROJECT@#jenkins#g' | sed 's#@BRANCH@#master#' | $OSC meta pkg jenkins:master ${pkg} -F -
+cat "${pkg}"/_meta.in  | sed 's#@BASEPROJECT@#jenkins#g' | sed 's#@BRANCH@#master#' | $OSC meta pkg jenkins:master "${pkg}" -F -
 done
 # for every package, add files that belong to this package, especially service files
 
 for pkg in `cat packages`; do
   $OSC co jenkins:master/${pkg};
-  cp -v "${pkg}/*" jenkins:master/${pkg}/;
-  cd jenkins:master/${pkg}/
+  pwd
+  ls "${pkg}"
+  cp -v "${pkg}"/* jenkins:master/"${pkg}"/;
+  cd jenkins:master/"${pkg}"/
   rm _meta.in
   osc add *
   osc commit -m "import"
   cd -
 done
-rm -Rvf jenkins:master/${pkg}/
+rm -Rvf jenkins:master
+
