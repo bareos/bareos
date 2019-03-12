850e96b2d2a8033cb92d12c7180df4d54cbe5c5f
Philipp Storz <philipp.storz@bareos.com>
2019-03-12 09:21:48 +0100

    matrix.yml

--- CD/jenkins/yaml_to_distreleases.sh
+++ CD/jenkins/yaml_to_distreleases.sh
@@ -4,7 +4,7 @@ DISTS=$(yq '.OS | (keys)' matrix.yml -c | tr '[],\"' '  ')
 for dist in ${DISTS}; do
   for version in $(yq ".OS.${dist} | (keys)" matrix.yml -c | tr '[],\"' '  '); do
     for arch in $(yq -c ".OS.${dist}.\"${version}\"" matrix.yml | tr '[],\"' '  '); do
-      echo "${dist}:${version}:${arch}"
+      echo "${dist}-${version}-${arch}"
     done
   done
 done
