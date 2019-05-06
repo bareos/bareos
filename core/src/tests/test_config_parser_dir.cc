721e15fac46fdc0d951166acaed134c8efa12acd
Frank Ueberschar <frank.ueberschar@bareos.com>
2019-05-06 16:28:00 +0200

    tests: added runscript_test as a unittest

--- core/src/tests/test_config_parser_dir.cc
+++ core/src/tests/test_config_parser_dir.cc
@@ -27,12 +27,29 @@
 
 namespace directordaemon {
 
-TEST(ConfigParser, test_director_config)
+TEST(ConfigParser_Dir, bareos_configparser_tests)
 {
   InitMsg(NULL, NULL); /* initialize message handler */
 
   std::string path_to_config_file = std::string(
       PROJECT_SOURCE_DIR "/src/tests/configs/bareos-configparser-tests");
+  my_config = InitDirConfig(path_to_config_file.c_str(), M_ERROR_TERM);
+  my_config->ParseConfig();
+
+  my_config->DumpResources(PrintMessage, NULL);
+
+  delete my_config;
+
+  TermMsg();         /* Terminate message handler */
+  CloseMemoryPool(); /* release free memory in pool */
+}
+
+TEST(ConfigParser_Dir, runscript_test)
+{
+  InitMsg(NULL, NULL); /* initialize message handler */
+
+  std::string path_to_config_file = std::string(
+      PROJECT_SOURCE_DIR "/src/tests/configs/runscript-tests/bareos-dir.conf");
   my_config = InitDirConfig(path_to_config_file.c_str(), M_ERROR_TERM);
   my_config->ParseConfig();
 
