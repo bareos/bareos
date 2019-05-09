2d61b76a0d3f9f6bf086991d871e3c0379c8bce5
Philipp Storz <philipp.storz@bareos.com>
2019-05-09 08:58:26 +0200

    build: reduce sign-compare warnings
    To solve the problem, we static_cast the smc_elem_aa members(uint64_t)
    to slot_number_t(int64_t) before comparing to slot_numbers.

--- core/src/dird/ndmp_slot2elemaddr.cc
+++ core/src/dird/ndmp_slot2elemaddr.cc
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2018-2018 Bareos GmbH & Co. KG
+   Copyright (C) 2018-2019 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -33,33 +33,41 @@ slot_number_t GetElementAddressByBareosSlotNumber(
     slot_number_t slotnumber)
 {
   if (slot_type == slot_type_storage) {
-    if ((slotnumber > (smc_elem_aa->se_count)) || (slotnumber < 1)) {
+    if ((slotnumber > static_cast<slot_number_t>(smc_elem_aa->se_count)) ||
+        (slotnumber < 1)) {
       return -1;
     } else {
       return (slotnumber + smc_elem_aa->se_addr -
               1);  // normal slots count start from 1
     }
   } else if (slot_type == slot_type_import) {
-    if ((slotnumber < (smc_elem_aa->se_count + 1)) ||
-        (slotnumber > (smc_elem_aa->se_count + smc_elem_aa->iee_count + 1))) {
+    if ((slotnumber <
+         (static_cast<slot_number_t>(smc_elem_aa->se_count) + 1)) ||
+        (slotnumber >
+         (static_cast<slot_number_t>(smc_elem_aa->se_count) +
+          static_cast<slot_number_t>(smc_elem_aa->iee_count) + 1))) {
       return -1;
     } else {
       return (slotnumber -
-              smc_elem_aa->se_count  // i/e slots follow after normal slots
-              - 1                    // normal slots count start from 1
-              + smc_elem_aa->iee_addr);
+              static_cast<slot_number_t>(
+                  smc_elem_aa->se_count)  // i/e slots follow after normal slots
+              - 1                         // normal slots count start from 1
+              + static_cast<slot_number_t>(smc_elem_aa->iee_addr));
     }
   } else if (slot_type == slot_type_picker) {
-    if ((slotnumber < 0) || slotnumber > (smc_elem_aa->mte_count - 1)) {
+    if ((slotnumber < 0) ||
+        slotnumber > (static_cast<slot_number_t>(smc_elem_aa->mte_count) - 1)) {
       return -1;
     } else {
-      return (slotnumber + smc_elem_aa->mte_addr);
+      return (slotnumber + static_cast<slot_number_t>(smc_elem_aa->mte_addr));
     }
   } else if (slot_type == slot_type_drive) {
-    if ((slotnumber > (smc_elem_aa->dte_count) - 1) || (slotnumber < 0)) {
+    if ((slotnumber >
+         (static_cast<slot_number_t>(smc_elem_aa->dte_count)) - 1) ||
+        (slotnumber < 0)) {
       return -1;
     } else {
-      return (slotnumber + smc_elem_aa->dte_addr);
+      return (slotnumber + static_cast<slot_number_t>(smc_elem_aa->dte_addr));
     }
   } else if (slot_type == slot_type_unknown) {
     return -1;
@@ -77,31 +85,39 @@ slot_number_t GetBareosSlotNumberByElementAddress(
     slot_number_t element_addr)
 {
   if (slot_type == slot_type_storage) {
-    if ((element_addr < smc_elem_aa->se_addr) ||
-        (element_addr > smc_elem_aa->se_addr + smc_elem_aa->se_count - 1)) {
+    if (element_addr < static_cast<slot_number_t>(smc_elem_aa->se_addr) ||
+        (element_addr > static_cast<slot_number_t>(smc_elem_aa->se_addr) +
+                            static_cast<slot_number_t>(smc_elem_aa->se_count) -
+                            1)) {
       return -1;
     } else {
-      return (element_addr - smc_elem_aa->se_addr +
+      return (element_addr - static_cast<slot_number_t>(smc_elem_aa->se_addr) +
               1);  // slots count start from 1
     }
   } else if (slot_type == slot_type_import) {
-    if ((element_addr < smc_elem_aa->iee_addr) ||
-        (element_addr > smc_elem_aa->iee_addr + smc_elem_aa->iee_count - 1)) {
+    if ((element_addr < static_cast<slot_number_t>(smc_elem_aa->iee_addr)) ||
+        (element_addr >
+         static_cast<slot_number_t>(smc_elem_aa->iee_addr) +
+             static_cast<slot_number_t>(smc_elem_aa->iee_count - 1))) {
       return -1;
     } else {
       return (element_addr +
-              smc_elem_aa->se_count  // i/e slots follow after normal slots
-              - smc_elem_aa->iee_addr + 1);  // slots count start from 1
+              static_cast<slot_number_t>(
+                  smc_elem_aa->se_count)  // i/e slots follow after normal slots
+              - static_cast<slot_number_t>(smc_elem_aa->iee_addr) +
+              1);  // slots count start from 1
     }
   } else if (slot_type == slot_type_drive) {
-    if ((element_addr < smc_elem_aa->dte_addr) ||
-        (element_addr > smc_elem_aa->dte_addr + smc_elem_aa->dte_count - 1)) {
+    if ((element_addr < static_cast<slot_number_t>(smc_elem_aa->dte_addr)) ||
+        (element_addr > static_cast<slot_number_t>(smc_elem_aa->dte_addr) +
+                            static_cast<slot_number_t>(smc_elem_aa->dte_count) -
+                            1)) {
       return -1;
     } else {
-      return (element_addr - smc_elem_aa->dte_addr);
+      return (element_addr - static_cast<slot_number_t>(smc_elem_aa->dte_addr));
     }
   } else if (slot_type == slot_type_picker) {
-    return (element_addr - smc_elem_aa->mte_addr);
+    return (element_addr - static_cast<slot_number_t>(smc_elem_aa->mte_addr));
 
   } else if (slot_type == slot_type_unknown) {
     return -1;
