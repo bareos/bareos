95708d597478a7b7b3fa505481378865851552b2
Frank Ueberschar <frank.ueberschar@bareos.com>
2019-06-19 12:31:28 +0200

    tests: added backtrace_test

---
+++ core/src/tests/test_backtrace.cc
@@ -0,0 +1,34 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2019-2019 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include "gtest/gtest.h"
+#include "include/bareos.h"
+#include "lib/backtrace.cc"
+
+#include <string>
+#include <iostream>
+
+TEST(Backtrace, test_backtrace)
+{
+  std::vector<BacktraceInfo> bt(Backtrace(0, 1));
+  ASSERT_EQ(bt.size(), 1);
+  EXPECT_NE(bt[0].function_call_.find("Backtrace"), std::string::npos);
+}
