c3084203cb9bd0d2ef84feee83db5773fbce4660
Frank Ueberschar <frank.ueberschar@bareos.com>
2019-07-04 15:45:11 +0200

    bnet: swapped the logic for IsBnetDumpEnabled()
    - the if branch contains now the behaviour when bnet dump is enabled

--- core/src/lib/cram_md5.cc
+++ core/src/lib/cram_md5.cc
@@ -67,22 +67,22 @@ bool CramMd5Handshake::CramMd5Challenge()
   Mmsg(chal, "<%u.%u@%s>", (uint32_t)random(), (uint32_t)time(NULL),
        host.c_str());
 
-  if (!bs_->IsBnetDumpEnabled()) {
-    Dmsg2(debuglevel_, "send: auth cram-md5 %s ssl=%d\n", chal.c_str(),
-          local_tls_policy_);
-
-    if (!bs_->fsend("auth cram-md5 %s ssl=%d\n", chal.c_str(),
-                    local_tls_policy_)) {
-      Dmsg1(debuglevel_, "Bnet send challenge comm error. ERR=%s\n",
-            bs_->bstrerror());
-      return false;
-    }
-  } else {  // network dump enabled
+  if (bs_->IsBnetDumpEnabled()) {
     Dmsg2(debuglevel_, "send: auth cram-md5 %s ssl=%d qualified-name=%s\n",
           chal.c_str(), local_tls_policy_, own_qualified_name_.c_str());
 
     if (!bs_->fsend("auth cram-md5 %s ssl=%d qualified-name=%s\n", chal.c_str(),
                     local_tls_policy_, own_qualified_name_.c_str())) {
+      Dmsg1(debuglevel_, "Bnet send challenge comm error. ERR=%s\n",
+            bs_->bstrerror());
+      return false;
+    }
+  } else {  // network dump disabled
+    Dmsg2(debuglevel_, "send: auth cram-md5 %s ssl=%d\n", chal.c_str(),
+          local_tls_policy_);
+
+    if (!bs_->fsend("auth cram-md5 %s ssl=%d\n", chal.c_str(),
+                    local_tls_policy_)) {
       Dmsg1(debuglevel_, "Bnet send challenge comm error. ERR=%s\n",
             bs_->bstrerror());
       return false;
@@ -135,20 +135,7 @@ bool CramMd5Handshake::CramMd5Response()
 
   Dmsg1(100, "cram-get received: %s", bs_->msg);
   chal.check_size(bs_->message_length);
-  if (!bs_->IsBnetDumpEnabled()) {
-    if (sscanf(bs_->msg, "auth cram-md5c %s ssl=%d", chal.c_str(),
-               &remote_tls_policy_) == 2) {
-      compatible_ = true;
-    } else if (sscanf(bs_->msg, "auth cram-md5 %s ssl=%d", chal.c_str(),
-                      &remote_tls_policy_) != 2) {
-      if (sscanf(bs_->msg, "auth cram-md5 %s\n", chal.c_str()) != 1) {
-        Dmsg1(debuglevel_, "Cannot scan challenge: %s", bs_->msg);
-        bs_->fsend(_("1999 Authorization failed.\n"));
-        Bmicrosleep(bs_->sleep_time_after_authentication_error, 0);
-        return false;
-      }
-    }
-  } else {  // network dump enabled
+  if (bs_->IsBnetDumpEnabled()) {
     std::vector<char> destination_qualified_name(256);
     if (sscanf(bs_->msg, "auth cram-md5c %s ssl=%d qualified-name=%s",
                chal.c_str(), &remote_tls_policy_,
@@ -164,8 +151,20 @@ bool CramMd5Handshake::CramMd5Response()
         return false;
       }
     }
-    bs_->SetBnetDumpDestinationQualifiedName(
-        destination_qualified_name.data());
+    bs_->SetBnetDumpDestinationQualifiedName(destination_qualified_name.data());
+  } else {  // network dump disabled
+    if (sscanf(bs_->msg, "auth cram-md5c %s ssl=%d", chal.c_str(),
+               &remote_tls_policy_) == 2) {
+      compatible_ = true;
+    } else if (sscanf(bs_->msg, "auth cram-md5 %s ssl=%d", chal.c_str(),
+                      &remote_tls_policy_) != 2) {
+      if (sscanf(bs_->msg, "auth cram-md5 %s\n", chal.c_str()) != 1) {
+        Dmsg1(debuglevel_, "Cannot scan challenge: %s", bs_->msg);
+        bs_->fsend(_("1999 Authorization failed.\n"));
+        Bmicrosleep(bs_->sleep_time_after_authentication_error, 0);
+        return false;
+      }
+    }
   }
 
   hmac_md5((uint8_t*)chal.c_str(), strlen(chal.c_str()), (uint8_t*)password_,
