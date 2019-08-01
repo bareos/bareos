000ed52fd970e5a04e971327b329eea81b1e09e9
root <root@localhost.localdomain>
2019-08-01 15:08:48 +0200

    fixup seg viol

--- core/src/lib/res.cc
+++ core/src/lib/res.cc
@@ -747,7 +747,7 @@ void ConfigurationParser::StoreAlistStr(LEX* lc,
     }
 
     list->append(strdup(lc->str));
-    Dmsg4(100, "BR debug: list.size %s, content0 %s", list->size(), list->first());
+    Dmsg4(100, "BR debug: list.size %s", list->size());
   }
   ScanToEol(lc);
   SetBit(index, (*item->allocated_resource)->item_present_);
@@ -795,7 +795,7 @@ void ConfigurationParser::StoreAlistDir(LEX* lc,
     }
 
     list->append(strdup(lc->str));
-    Dmsg4(100, "BR debug: list.size %s, content0 %s", list->size(), list->first());
+    Dmsg4(100, "BR debug: list.size %s", list->size());
   }
   ScanToEol(lc);
   SetBit(index, (*item->allocated_resource)->item_present_);
