82728e4074367fd7418d31b87e08ead3ebb7cdb7
Frank Bergkemper <frank.bergkemper@bareos.com>
2019-08-26 14:02:56 +0200

    webui: Correct @copyright and @link

--- webui/module/Restore/src/Restore/Form/RestoreForm.php
+++ webui/module/Restore/src/Restore/Form/RestoreForm.php
@@ -4,8 +4,8 @@
 *
 * bareos-webui - Bareos Web-Frontend
 *
-* @link      https://github.com/bareos/bareos-webui for the canonical source repository
-* @copyright Copyright (c) 2013-2019 dass-IT GmbH (http://www.dass-it.de/)
+* @link      https://github.com/bareos/bareos for the canonical source repository
+* @copyright Copyright (c) 2013-2019 Bareos GmbH & Co. KG (http://www.bareos.org/)
 * @license   GNU Affero General Public License (http://www.gnu.org/licenses/)
 *
 * This program is free software: you can redistribute it and/or modify
--- webui/packaging/obs/bareos-webui.dsc
+++ webui/packaging/obs/bareos-webui.dsc
@@ -5,7 +5,7 @@ Architecture: all
 Version: 15.2.1
 Maintainer: Bareos Packager <packager@bareos.com>
 Standards-Version: 3.9.1
-Vcs-Browser: https://github.com/bareos/bareos-webui.git
+Vcs-Browser: https://github.com/bareos/bareos.git
 Vcs-Git: git://github.com/bareos/bareos-webui.git
 Build-Depends: debhelper (>= 7.0.50~), apache2, apache2-dev | apache2-prefork-dev, cmake, bareos-common
 DEBTRANSFORM-RELEASE: 1
