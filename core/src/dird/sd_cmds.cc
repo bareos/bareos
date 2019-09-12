bareos/bareos

commit c3a587f46644971c12412c9c75370ca0a7de1c05

NDMP: merged NDMP multidrive support
This commit merges the current NDMP multidrive support
branch into bareos 18.

core/src/dird/sd_cmds.cc
@@ -383,21 +383,21 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
          /*
           * Scanning -- require only valid slot
           */
-         vl->Slot = atoi(field1);
-         if (vl->Slot <= 0) {
+         vl->bareos_slot_number = atoi(field1);
+         if (vl->bareos_slot_number <= 0) {
             ua->ErrorMsg(_("Invalid Slot number: %s\n"), sd->msg);
             free(vl);
             continue;
          }
 
-         vl->Type = slot_type_normal;
+         vl->slot_type = slot_type_storage;
          if (strlen(field2) > 0) {
-            vl->Content = slot_content_full;
+            vl->slot_status = slot_status_full;
             vl->VolName = bstrdup(field2);
          } else {
-            vl->Content = slot_content_empty;
+            vl->slot_status = slot_status_empty;
          }
-         vl->Index = INDEX_SLOT_OFFSET + vl->Slot;
+         vl->element_address = INDEX_SLOT_OFFSET + vl->bareos_slot_number;
       } else if (!listall) {
          /*
           * Not scanning and not listall.
@@ -407,7 +407,7 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
             continue;
          }
 
-         if (!IsAnInteger(field1) || (vl->Slot = atoi(field1)) <= 0) {
+         if (!IsAnInteger(field1) || (vl->bareos_slot_number = atoi(field1)) <= 0) {
             ua->ErrorMsg(_("Invalid Slot number: %s\n"), field1);
             free(vl);
             continue;
@@ -419,10 +419,10 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
             continue;
          }
 
-         vl->Type = slot_type_normal;
-         vl->Content = slot_content_full;
+         vl->slot_type = slot_type_storage;
+         vl->slot_status = slot_status_full;
          vl->VolName = bstrdup(field2);
-         vl->Index = INDEX_SLOT_OFFSET + vl->Slot;
+         vl->element_address = INDEX_SLOT_OFFSET + vl->bareos_slot_number;
       } else {
          /*
           * Listall.
@@ -433,17 +433,17 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
 
          switch (*field1) {
          case 'D':
-            vl->Type = slot_type_drive;
+            vl->slot_type = slot_type_drive;
             break;
          case 'S':
-            vl->Type = slot_type_normal;
+            vl->slot_type = slot_type_storage;
             break;
          case 'I':
-            vl->Type = slot_type_import;
-            vl->Flags |= (can_import | can_export | by_oper);
+            vl->slot_type = slot_type_import;
+            vl->flags |= (can_import | can_export | by_oper);
             break;
          default:
-            vl->Type = slot_type_unknown;
+            vl->slot_type = slot_type_unknown;
             break;
          }
 
@@ -451,39 +451,39 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
           * For drives the Slot is the actual drive number.
           * For any other type its the actual slot number.
           */
-         switch (vl->Type) {
+         switch (vl->slot_type) {
          case slot_type_drive:
-            if (!IsAnInteger(field2) || (vl->Slot = atoi(field2)) < 0) {
+            if (!IsAnInteger(field2) || (vl->bareos_slot_number = atoi(field2)) < 0) {
                ua->ErrorMsg(_("Invalid Drive number: %s\n"), field2);
                free(vl);
                continue;
             }
-            vl->Index = INDEX_DRIVE_OFFSET + vl->Slot;
-            if (vl->Index >= INDEX_MAX_DRIVES) {
+            vl->element_address = INDEX_DRIVE_OFFSET + vl->bareos_slot_number;
+            if (vl->element_address >= INDEX_MAX_DRIVES) {
                ua->ErrorMsg(_("Drive number %hd greater then INDEX_MAX_DRIVES(%hd) please increase define\n"),
-                             vl->Slot, INDEX_MAX_DRIVES);
+                             vl->bareos_slot_number, INDEX_MAX_DRIVES);
                free(vl);
                continue;
             }
             break;
          default:
-            if (!IsAnInteger(field2) || (vl->Slot = atoi(field2)) <= 0) {
+            if (!IsAnInteger(field2) || (vl->bareos_slot_number = atoi(field2)) <= 0) {
                ua->ErrorMsg(_("Invalid Slot number: %s\n"), field2);
                free(vl);
                continue;
             }
-            vl->Index = INDEX_SLOT_OFFSET + vl->Slot;
+            vl->element_address = INDEX_SLOT_OFFSET + vl->bareos_slot_number;
             break;
          }
 
          switch (*field3) {
          case 'E':
-            vl->Content = slot_content_empty;
+            vl->slot_status = slot_status_empty;
             break;
          case 'F':
-            vl->Content = slot_content_full;
-            switch (vl->Type) {
-            case slot_type_normal:
+            vl->slot_status = slot_status_full;
+            switch (vl->slot_status) {
+            case slot_type_storage:
             case slot_type_import:
                if (field4) {
                   vl->VolName = bstrdup(field4);
@@ -491,7 +491,7 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
                break;
             case slot_type_drive:
                if (field4) {
-                  vl->Loaded = atoi(field4);
+                  vl->currently_loaded_slot_number = atoi(field4);
                }
                if (field5) {
                   vl->VolName = bstrdup(field5);
@@ -502,17 +502,17 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
             }
             break;
          default:
-            vl->Content = slot_content_unknown;
+            vl->slot_status = slot_status_unknown;
             break;
          }
       }
 
       if (vl->VolName) {
          Dmsg6(100, "Add index = %hd slot=%hd loaded=%hd type=%hd content=%hd Vol=%s to SD list.\n",
-               vl->Index, vl->Slot, vl->Loaded, vl->Type, vl->Content, NPRT(vl->VolName));
+               vl->element_address, vl->bareos_slot_number, vl->currently_loaded_slot_number, vl->slot_type, vl->slot_type, NPRT(vl->VolName));
       } else {
          Dmsg5(100, "Add index = %hd slot=%hd loaded=%hd type=%hd content=%hd Vol=NULL to SD list.\n",
-               vl->Index, vl->Slot, vl->Loaded, vl->Type, vl->Content);
+               vl->element_address, vl->bareos_slot_number, vl->currently_loaded_slot_number, vl->slot_type, vl->slot_type);
       }
 
       vol_list->binary_insert(vl, StorageCompareVolListEntry);

core/src/dird/CMakeLists.txt
@@ -30,6 +30,7 @@ set(DIRD_OBJECTS_SRCS admin.cc archive.cc authenticate.cc authenticate_console.c
    ndmp_dma_generic.cc
    ndmp_dma_restore_common.cc ndmp_dma_restore_NDMP_BAREOS.cc ndmp_dma_restore_NDMP_NATIVE.cc
    ndmp_fhdb_common.cc ndmp_fhdb_helpers.cc
+   ndmp_slot2elemaddr.cc
    ndmp_fhdb_mem.cc ndmp_fhdb_lmdb.cc ndmp_ndmmedia_db_helpers.cc
    newvol.cc next_vol.cc quota.cc socket_server.cc recycle.cc restore.cc
    run_conf.cc sd_cmds.cc scheduler.cc stats.cc storage.cc ua.cc ua_acl.cc ua_audit.cc

core/src/dird/backup.cc
@@ -45,6 +45,7 @@
 #include "dird/msgchan.h"
 #include "dird/quota.h"
 #include "dird/sd_cmds.h"
+#include "ndmp/smc.h"
 #include "dird/storage.h"
 
 #include "cats/sql.h"

core/src/dird/dird.h
@@ -109,16 +109,16 @@ typedef enum {
 typedef enum {
    slot_type_unknown,             /**< Unknown slot type */
    slot_type_drive,               /**< Drive slot */
-   slot_type_normal,              /**< Normal slot */
+   slot_type_storage,             /**< Storage slot */
    slot_type_import,              /**< Import/export slot */
    slot_type_picker               /**< Robotics */
-} slot_type;
+} slot_type_t;
 
 typedef enum {
-   slot_content_unknown,          /**< Slot content is unknown */
-   slot_content_empty,            /**< Slot is empty */
-   slot_content_full              /**< Slot is full */
-} slot_content;
+   slot_status_unknown,          /**< Slot content is unknown */
+   slot_status_empty,            /**< Slot is empty */
+   slot_status_full              /**< Slot is full */
+} slot_status_t;
 
 enum s_mapping_type {
    LOGICAL_TO_PHYSICAL,
@@ -130,12 +130,12 @@ enum s_mapping_type {
  */
 struct vol_list_t {
    dlink link;                    /**< Link for list */
-   slot_number_t Index;           /**< Unique index */
-   slot_flags_t Flags;            /**< Slot specific flags see e_slot_flag enum */
-   slot_type Type;                /**< See slot_type_* */
-   slot_content Content;          /**< See slot_content_* */
-   slot_number_t Slot;            /**< Drive number when slot_type_drive or actual slot number */
-   slot_number_t Loaded;          /**< Volume loaded in drive when slot_type_drive */
+   slot_number_t element_address ; /**< scsi element address */
+   slot_flags_t flags;            /**< Slot specific flags see e_slot_flag enum */
+   slot_type_t slot_type;                /**< See slot_type_* */
+   slot_status_t slot_status;          /**< See slot_status_* */
+   slot_number_t bareos_slot_number; /**< Drive number when slot_type_drive or actual slot number */
+   slot_number_t currently_loaded_slot_number; /**< Volume loaded in drive when slot_type_drive */
    char *VolName;                 /**< Actual Volume Name */
 };
 
@@ -151,19 +151,54 @@ struct changer_vol_list_t {
  */
 struct storage_mapping_t {
    dlink link;                   /**< Link for list */
-   slot_type Type;               /**< See slot_type_* */
-   slot_number_t Index;          /**< Unique index */
+   slot_type_t slot_type;               /**< See slot_type_* */
+   slot_number_t element_address;/**< scsi element address */
    slot_number_t Slot;           /**< Drive number when slot_type_drive or actual slot number */
 };
+
+
+
+#if HAVE_NDMP
+/**
+ * same as smc_element_address_assignment
+ * from ndmp/smc.h
+ * TODO: check if original definition can be used
+ */
+struct smc_element_address_assignment {
+
+  unsigned  mte_addr;   /* media transport element */
+  unsigned  mte_count;
+
+  unsigned  se_addr;    /* storage element */
+  unsigned  se_count;
+
+  unsigned  iee_addr; /* import/export element */
+  unsigned  iee_count;
+
+  unsigned  dte_addr; /* data transfer element */
+  unsigned  dte_count;
+
+};
+
+struct ndmp_deviceinfo_t {
+  std::string device;
+  std::string model;
+  JobId_t JobIdUsingDevice;
+};
+#endif
+
 
 struct runtime_storage_status_t {
    int32_t NumConcurrentJobs;     /**< Number of concurrent jobs running */
    int32_t NumConcurrentReadJobs; /**< Number of jobs reading */
    drive_number_t drives;         /**< Number of drives in autochanger */
    slot_number_t slots;           /**< Number of slots in autochanger */
-   dlist *storage_mappings;       /**< Mappings from logical to physical storage address */
-   changer_vol_list_t *vol_list;  /**< Cached content of autochanger */
    pthread_mutex_t changer_lock;  /**< Any access to the autochanger is controlled by this lock */
+   unsigned char smc_ident[32];   /**< smc ident info = changer name */
+   smc_element_address_assignment storage_mapping;/**< smc element assignment */
+   changer_vol_list_t *vol_list;  /**< Cached content of autochanger */
+   std::list<ndmp_deviceinfo_t> *ndmp_deviceinfo; /**< NDMP device info for devices in this Storage */
+   pthread_mutex_t ndmp_deviceinfo_lock;  /**< Any access to the list devices is controlled by this lock */
 };
 
 struct runtime_client_status_t {

core/src/dird/dird_conf.cc
@@ -2486,6 +2486,12 @@ static bool UpdateResourcePointer(int type, ResourceItem *items)
 
             Emsg1(M_ERROR_TERM, 0, _("pthread_mutex_init: ERR=%s\n"), be.bstrerror(status));
          }
+         if ((status = pthread_mutex_init(&res->res_store.rss->ndmp_deviceinfo_lock, NULL)) != 0) {
+            BErrNo be;
+
+            Emsg1(M_ERROR_TERM, 0, _("pthread_mutex_init: ERR=%s\n"), be.bstrerror(status));
+         }
+
       }
       break;
    case R_JOBDEFS:
@@ -4094,7 +4100,6 @@ static void FreeResource(CommonResourceHeader *sres, int type)
       if (res->res_store.ndmp_changer_device) { free(res->res_store.ndmp_changer_device); }
       if (res->res_store.device) { delete res->res_store.device; }
       if (res->res_store.rss) {
-        if (res->res_store.rss->storage_mappings) { delete res->res_store.rss->storage_mappings; }
         if (res->res_store.rss->vol_list) {
           if (res->res_store.rss->vol_list->contents) {
             vol_list_t *vl;
@@ -4108,6 +4113,7 @@ static void FreeResource(CommonResourceHeader *sres, int type)
           free(res->res_store.rss->vol_list);
         }
         pthread_mutex_destroy(&res->res_store.rss->changer_lock);
+        pthread_mutex_destroy(&res->res_store.rss->ndmp_deviceinfo_lock);
         free(res->res_store.rss);
       }
       if (res->res_store.tls_cert_.allowed_certificate_common_names_) {

core/src/dird/ndmp_dma_backup_NDMP_BAREOS.cc
@@ -201,11 +201,7 @@ bool DoNdmpBackup(JobControlRecord *jcr)
    bool retval = false;
    int NdmpLoglevel;
 
-   if (jcr->res.client->ndmp_loglevel > me->ndmp_loglevel) {
-      NdmpLoglevel = jcr->res.client->ndmp_loglevel;
-   } else {
-      NdmpLoglevel = me->ndmp_loglevel;
-   }
+   NdmpLoglevel = std::max(jcr->res.client->ndmp_loglevel, me->ndmp_loglevel);
 
    /*
     * Print Job Start message

commit 56939d39d7c17b13fed788a6ec49518ac186edff

core: make native_get_vol_list() retrieve loaded volumes again
Fixes #1111 import/export and status slots commands don't see tapes loaded in drives anymore

This patch fixes a regression introduced by c3a587f4
Previously volumes loaded in a tape drive were not visible to the director
which lead to several issues. This patch restores the old behaviour.

core/src/dird/sd_cmds.cc
@@ -482,7 +482,7 @@ dlist *native_get_vol_list(UaContext *ua, StorageResource *store, bool listall, 
             break;
          case 'F':
             vl->slot_status = slot_status_full;
-            switch (vl->slot_type) {
+            switch (vl->slot_status) {
             case slot_type_storage:
             case slot_type_import:
                if (field4) {