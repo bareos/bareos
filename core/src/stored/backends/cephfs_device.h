a339f96203d2d59a222df30786040af7416f9f01
Philipp Storz <philipp.storz@bareos.com>
2019-10-22 13:30:36 +0200

    cephfs_device.h: added overrides

--- core/src/stored/backends/cephfs_device.h
+++ core/src/stored/backends/cephfs_device.h
@@ -2,7 +2,7 @@
    BAREOS® - Backup Archiving REcovery Open Sourced
 
    Copyright (C) 2014-2014 Planets Communications B.V.
-   Copyright (C) 2014-2014 Bareos GmbH & Co. KG
+   Copyright (C) 2014-2019 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -47,13 +47,15 @@ class cephfs_device : public Device {
   /*
    * Interface from Device
    */
-  int d_close(int);
-  int d_open(const char* pathname, int flags, int mode);
-  int d_ioctl(int fd, ioctl_req_t request, char* mt = NULL);
-  boffset_t d_lseek(DeviceControlRecord* dcr, boffset_t offset, int whence);
-  ssize_t d_read(int fd, void* buffer, size_t count);
-  ssize_t d_write(int fd, const void* buffer, size_t count);
-  bool d_truncate(DeviceControlRecord* dcr);
+  int d_close(int) override;
+  int d_open(const char* pathname, int flags, int mode) override;
+  int d_ioctl(int fd, ioctl_req_t request, char* mt = NULL) override;
+  boffset_t d_lseek(DeviceControlRecord* dcr,
+                    boffset_t offset,
+                    int whence) override;
+  ssize_t d_read(int fd, void* buffer, size_t count) override;
+  ssize_t d_write(int fd, const void* buffer, size_t count) override;
+  bool d_truncate(DeviceControlRecord* dcr) override;
 };
 
 } /* namespace storagedaemon */
