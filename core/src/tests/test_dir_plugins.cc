142dcc5e1a489c82aeec5daf34fa827ea42343c6
Philipp Storz <philipp.storz@bareos.com>
2019-10-26 00:19:43 +0200

    test_xx_plugins: ignore unused-variable warning

--- core/src/tests/test_dir_plugins.cc
+++ core/src/tests/test_dir_plugins.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2007-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2016 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2019 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -56,8 +56,11 @@ TEST(dir, dir_plugins)
   InitMsg(NULL, NULL);
 
   OSDependentInit();
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+  char* cwd = getcwd(plugin_dir, sizeof(plugin_dir) - 1);
+#pragma GCC diagnostic pop
 
-  getcwd(plugin_dir, sizeof(plugin_dir) - 1);
   LoadDirPlugins(plugin_dir, NULL);
 
   jcr1->JobId = 111;
