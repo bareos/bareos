2663a9d370c45ea99e77bf0d4b80eba325edab66
Philipp Storz <philipp.storz@bareos.com>
2019-10-28 13:21:04 +0100

    ua_dotcmds.cc: fix class-memaccess warnings

--- core/src/dird/ua_dotcmds.cc
+++ core/src/dird/ua_dotcmds.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2002-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2018 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2019 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -636,8 +636,6 @@ bool DotBvfsGetJobidsCmd(UaContext* ua, const char* cmd)
   if (jr.JobLevel == L_BASE) {
     jobids.add(edit_int64(jr.JobId, ed1));
   } else {
-    FileSetDbRecord fs;
-
     /*
      * If we have the "all" option, we do a search on all defined fileset for
      * this client
@@ -657,7 +655,7 @@ bool DotBvfsGetJobidsCmd(UaContext* ua, const char* cmd)
      * Foreach different FileSet, we build a restore jobid list
      */
     for (int i = 0; i < ids.num_ids; i++) {
-      memset(&fs, 0, sizeof(fs));
+      FileSetDbRecord fs;
 
       /*
        * Lookup the FileSet.
