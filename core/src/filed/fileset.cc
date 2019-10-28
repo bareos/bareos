a344e227c54c0d52b55037a3cfddfaf1e85cd920
Philipp Storz <philipp.storz@bareos.com>
2019-10-28 12:49:30 +0100

    fileset.cc: fix class-memaccess warnings

--- core/src/filed/fileset.cc
+++ core/src/filed/fileset.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2016 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2019 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -79,7 +79,9 @@ bool InitFileset(JobControlRecord* jcr)
   ff = jcr->ff;
   if (ff->fileset) { return false; }
   fileset = (findFILESET*)malloc(sizeof(findFILESET));
-  memset(fileset, 0, sizeof(findFILESET));
+  static const findFILESET emptyfindFILESET{};
+  *fileset = emptyfindFILESET;
+
   ff->fileset = fileset;
   fileset->state = state_none;
   fileset->include_list.init(1, true);
