c39d2cc67885e18420e5de790c1718938909afbf
Philipp Storz <philipp.storz@bareos.com>
2019-10-29 17:44:42 +0100

    lib: IsNameValid(): disallow leading and trailing spaces
    Names with leading and trailing space are now illegal.
    Also added a unit test that checks that IsNameValid() works
    as expected.

---
+++ core/src/tests/test_is_name_valid.cc
@@ -0,0 +1,64 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2019-2019 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation, which is
+   listed in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include "gtest/gtest.h"
+#include "include/bareos.h"
+#include "lib/edit.h"
+
+POOLMEM* msg = GetPoolMemory(PM_FNAME);
+
+TEST(is_name_valid, is_name_valid)
+{
+  EXPECT_EQ(true, IsNameValid("STRING_CONTAINING_ALLOWED_CHARS :.-_/", msg));
+  EXPECT_EQ(true, IsNameValid("A name_has_space_at_second_place"));
+  EXPECT_EQ(true, IsNameValid("name_has_space_as_second_last Z"));
+  EXPECT_EQ(
+      true,
+      IsNameValid(
+          "STRING_WITH_MAX_ALLOWED_LENGTH......................................"
+          "...........................................................",
+          msg));
+
+
+  EXPECT_EQ(false, IsNameValid("illegalch@racter", msg));
+  EXPECT_STREQ("Illegal character \"@\" in name.\n", msg);
+
+  EXPECT_EQ(false, IsNameValid("", msg));
+  EXPECT_STREQ("Name must be at least one character long.\n", msg);
+
+  EXPECT_EQ(false, IsNameValid(nullptr, msg));
+  EXPECT_STREQ("Empty name not allowed.\n", msg);
+
+  EXPECT_EQ(false, IsNameValid(" name_starts_with_space", msg));
+  EXPECT_STREQ("Name cannot start with space.\n", msg);
+
+  EXPECT_EQ(false, IsNameValid("name_ends_with_space ", msg));
+  EXPECT_STREQ("Name cannot end with space.\n", msg);
+
+  EXPECT_EQ(
+      false,
+      IsNameValid(
+          "STRING_WITH_MAX_ALLOWED_LENGTH_PLUS_ONE............................."
+          "............................................................",
+          msg));
+  EXPECT_STREQ("Name too long.\n", msg);
+}
+
