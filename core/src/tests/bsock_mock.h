1675b76c4dd9e026bca36b3fecf6b48076a02c82
Philipp Storz <philipp.storz@bareos.com>
2019-11-04 13:01:33 +0100

    bsock_mock.h: avoid warnings with clang

--- core/src/tests/bsock_mock.h
+++ core/src/tests/bsock_mock.h
@@ -37,9 +37,14 @@
 
 #include <algorithm>
 
+#ifdef __GNUC__
+#ifndef __clang__
 /* ignore the suggest-override warnings caused by MOCK_METHODx */
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsuggest-override"
+#endif
+#endif
+
 class BareosSocketMock : public BareosSocket {
  public:
   BareosSocketMock() : BareosSocket() {}
@@ -85,8 +90,11 @@ class BareosSocketMock : public BareosSocket {
                     utime_t,
                     int*));
 };
+#ifdef __GNUC__
+#ifndef __clang__
 #pragma GCC diagnostic pop
-
+#endif
+#endif
 /* define a gmock action that fills bsock->msg so we can recv() a message */
 ACTION_P2(BareosSocket_Recv, bsock, msg)
 {
