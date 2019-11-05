73c01230c245af816707c9d29a243e0a25613f6a
Philipp Storz <philipp.storz@bareos.com>
2019-11-05 10:54:40 +0100

    label.cc: limit size written to ProgVersion

--- core/src/stored/label.cc
+++ core/src/stored/label.cc
@@ -586,7 +586,7 @@ void CreateVolumeLabel(Device* dev, const char* VolName, const char* PoolName)
   }
   bstrncpy(dev->VolHdr.LabelProg, my_name, sizeof(dev->VolHdr.LabelProg));
   snprintf(dev->VolHdr.ProgVersion, sizeof(dev->VolHdr.ProgVersion),
-           "Ver. %s %s", VERSION, BDATE);
+           "Ver. %.32s %.12s", VERSION, BDATE);
   snprintf(dev->VolHdr.ProgDate, sizeof(dev->VolHdr.ProgDate), "Build %s %s",
            __DATE__, __TIME__);
   dev->SetLabeled(); /* set has Bareos label */
