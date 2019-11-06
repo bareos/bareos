e07b91ab1970ecdd2920b3a9939cd9880bc7547d
Frank Ueberschar <frank.ueberschar@bareos.com>
2019-11-06 10:37:05 +0100

    filed: refactore member initializers

--- core/src/filed/jcr_private.h
+++ core/src/filed/jcr_private.h
@@ -35,55 +35,53 @@ class BareosAccurateFilelist;
 
 /* clang-format off */
 struct CryptoContext {
-  bool pki_sign = false;               /**< Enable PKI Signatures? */
-  bool pki_encrypt = false;            /**< Enable PKI Encryption? */
-  DIGEST* digest = nullptr;            /**< Last file's digest context */
-  X509_KEYPAIR* pki_keypair = nullptr; /**< Encryption key pair */
-  alist* pki_signers = nullptr;        /**< Trusted Signers */
-  alist* pki_recipients = nullptr;     /**< Trusted Recipients */
-  CRYPTO_SESSION* pki_session = nullptr; /**< PKE Public Keys + Symmetric Session Keys */
-  POOLMEM* crypto_buf = nullptr;       /**< Encryption/Decryption buffer */
-  POOLMEM* pki_session_encoded = nullptr; /**< Cached DER-encoded copy of pki_session */
-  int32_t pki_session_encoded_size = 0; /**< Size of DER-encoded pki_session */
+  bool pki_sign{};                /**< Enable PKI Signatures? */
+  bool pki_encrypt{};             /**< Enable PKI Encryption? */
+  DIGEST* digest{};               /**< Last file's digest context */
+  X509_KEYPAIR* pki_keypair{};    /**< Encryption key pair */
+  alist* pki_signers{};           /**< Trusted Signers */
+  alist* pki_recipients{};        /**< Trusted Recipients */
+  CRYPTO_SESSION* pki_session{};  /**< PKE Public Keys + Symmetric Session Keys */
+  POOLMEM* crypto_buf{};          /**< Encryption/Decryption buffer */
+  POOLMEM* pki_session_encoded{}; /**< Cached DER-encoded copy of pki_session */
+  int32_t pki_session_encoded_size{}; /**< Size of DER-encoded pki_session */
 };
 
 struct JobControlRecordPrivate {
-  uint32_t num_files_examined = 0; /**< Files examined this job */
-  POOLMEM* last_fname = nullptr;   /**< Last file saved/verified */
-  POOLMEM* job_metadata = nullptr; /**< VSS job metadata */
-  acl_data_t* acl_data = nullptr;  /**< ACLs for backup/restore */
-  xattr_data_t* xattr_data =
-      nullptr;                   /**< Extended Attributes for backup/restore */
-  int32_t last_type = 0;         /**< Type of last file saved/verified */
-  bool incremental = false;      /**< Set if incremental for SINCE */
-  utime_t mtime = 0;             /**< Begin time for SINCE */
-  int listing = 0;               /**< Job listing in estimate */
-  int32_t Ticket = 0;            /**< Ticket */
-  char* big_buf = nullptr;       /**< I/O buffer */
-  int32_t replace = 0;           /**< Replace options */
-  FindFilesPacket* ff = nullptr; /**< Find Files packet */
-  char PrevJob[MAX_NAME_LENGTH]{
-      0}; /**< Previous job name assiciated with since time */
-  uint32_t ExpectedFiles = 0; /**< Expected restore files */
-  uint32_t StartFile = 0;
-  uint32_t EndFile = 0;
-  uint32_t StartBlock = 0;
-  uint32_t EndBlock = 0;
-  pthread_t heartbeat_id = 0;                 /**< Id of heartbeat thread */
-  volatile bool hb_started = false;           /**< Heartbeat running */
+  uint32_t num_files_examined{};  /**< Files examined this job */
+  POOLMEM* last_fname{};          /**< Last file saved/verified */
+  POOLMEM* job_metadata{};        /**< VSS job metadata */
+  acl_data_t* acl_data{};         /**< ACLs for backup/restore */
+  xattr_data_t* xattr_data{};     /**< Extended Attributes for backup/restore */
+  int32_t last_type{};            /**< Type of last file saved/verified */
+  bool incremental{};             /**< Set if incremental for SINCE */
+  utime_t mtime{};                /**< Begin time for SINCE */
+  int listing{};                  /**< Job listing in estimate */
+  int32_t Ticket{};               /**< Ticket */
+  char* big_buf{};                /**< I/O buffer */
+  int32_t replace{};              /**< Replace options */
+  FindFilesPacket* ff{};          /**< Find Files packet */
+  char PrevJob[MAX_NAME_LENGTH]{};/**< Previous job name assiciated with since time */
+  uint32_t ExpectedFiles{};       /**< Expected restore files */
+  uint32_t StartFile{};
+  uint32_t EndFile{};
+  uint32_t StartBlock{};
+  uint32_t EndBlock{};
+  pthread_t heartbeat_id{};       /**< Id of heartbeat thread */
+  volatile bool hb_started{};     /**< Heartbeat running */
   std::shared_ptr<BareosSocket> hb_bsock;     /**< Duped SD socket */
   std::shared_ptr<BareosSocket> hb_dir_bsock; /**< Duped DIR socket */
-  alist* RunScripts = nullptr; /**< Commands to run before and after job */
-  CryptoContext crypto;        /**< Crypto ctx */
-  filedaemon::DirectorResource* director = nullptr; /**< Director resource */
-  bool enable_vss = false;                          /**< VSS used by FD */
-  bool got_metadata = false;  /**< Set when found job_metadata */
-  bool multi_restore = false; /**< Dir can do multiple storage restore */
-  filedaemon::BareosAccurateFilelist* file_list = nullptr; /**< Previous file list (accurate mode) */
-  uint64_t base_size = 0; /**< Compute space saved with base job */
-  filedaemon::save_pkt* plugin_sp = nullptr; /**< Plugin save packet */
+  alist* RunScripts{};            /**< Commands to run before and after job */
+  CryptoContext crypto;           /**< Crypto ctx */
+  filedaemon::DirectorResource* director{}; /**< Director resource */
+  bool enable_vss{};              /**< VSS used by FD */
+  bool got_metadata{};            /**< Set when found job_metadata */
+  bool multi_restore{};           /**< Dir can do multiple storage restore */
+  filedaemon::BareosAccurateFilelist* file_list{}; /**< Previous file list (accurate mode) */
+  uint64_t base_size{};           /**< Compute space saved with base job */
+  filedaemon::save_pkt* plugin_sp{}; /**< Plugin save packet */
 #ifdef HAVE_WIN32
-  VSSClient* pVSSClient = nullptr; /**< VSS Client Instance */
+  VSSClient* pVSSClient{};        /**< VSS Client Instance */
 #endif
 };
 /* clang-format on */
