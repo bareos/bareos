ac3ed8032ba259843c240ade8b24d948ef7488a6
Frank Ueberschar <frank.ueberschar@bareos.com>
2019-11-14 15:57:22 +0100

    dird: move CheckCatalogue from dird.cc to separate file

---
+++ core/src/dird/check_catalogue.h
@@ -0,0 +1,39 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
+   Copyright (C) 2011-2016 Planets Communications B.V.
+   Copyright (C) 2019-2019 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+#ifndef BAREOS_SRC_DIRD_CHECK_CATALOGUE_H_
+#define BAREOS_SRC_DIRD_CHECK_CATALOGUE_H_
+
+namespace directordaemon {
+
+typedef enum
+{
+  CHECK_CONNECTION, /* Check catalog connection */
+  UPDATE_CATALOG,   /* Ensure that catalog is ok with conf */
+  UPDATE_AND_FIX    /* Ensure that catalog is ok, and fix old jobs */
+} cat_op;
+
+bool CheckCatalog(cat_op mode);
+
+}  // namespace directordaemon
+
+#endif  // BAREOS_SRC_DIRD_CHECK_CATALOGUE_H_
