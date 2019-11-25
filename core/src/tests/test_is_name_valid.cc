0f561e0cbe4c7e303a8f79183026c08731448a47
Philipp Storz <philipp.storz@bareos.com>
2019-11-25 16:43:22 +0100

    tests: is_name_valid

--- core/src/tests/test_is_name_valid.cc
+++ core/src/tests/test_is_name_valid.cc
@@ -19,8 +19,16 @@
    02110-1301, USA.
 */
 
+#if defined(HAVE_MINGW)
+#define _S_IFDIR S_IFDIR
+#define _stat stat
+#include "minwindef.h"
+#include "include/bareos.h"
+#include "gtest/gtest.h"
+#else
 #include "gtest/gtest.h"
 #include "include/bareos.h"
+#endif
 #include "lib/edit.h"
 
 POOLMEM* msg = GetPoolMemory(PM_FNAME);
