4b077e8800972be0ed8a58d4e020af5c561759ea
Philipp Storz <philipp.storz@bareos.com>
2020-01-06 17:03:49 +0100

    bareos-fd-percona.py: add bareos header

--- core/src/plugins/filed/bareos-fd-percona.py
+++ core/src/plugins/filed/bareos-fd-percona.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Bareos-fd-local-fileset a simple example for a python Bareos FD Plugin using BareosFdPluginLocalFileset
-# The plugin argument 'filename' is used to read all files listed in that file and add it to the fileset
-# License: AGPLv3
+# Copyright (C) 2015-2020 Bareos GmbH & Co. KG
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of version three of the GNU Affero General Public
+# License as published by the Free Software Foundation, which is
+# listed in the file LICENSE.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
 
 # Provided by the Bareos FD Python plugin interface
 from bareosfd import *
