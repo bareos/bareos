72b4dd2a6c4c3db2e12f20d00e272d7223b48535
Joerg Steffens <joerg.steffens@bareos.com>
2020-01-15 17:53:17 +0100

    pam_exec_add_bareos_user.py: reformated by black

--- misc/bareos_pam_integration/pam_exec_add_bareos_user.py
+++ misc/bareos_pam_integration/pam_exec_add_bareos_user.py
@@ -13,7 +13,7 @@ import bareos.bsock
 import bareos.exceptions
 import logging
 import os
-from   pprint import pformat
+from pprint import pformat
 import random
 import string
 import sys
@@ -24,25 +24,33 @@ def check_requirements(director):
     logger = logging.getLogger()
     logger.debug("checking requirements: start")
     try:
-        result = director.call('.users')
+        result = director.call(".users")
     except (bareos.exceptions.Error) as e:
-        logger.error('The .users command is required, but not available: {}'.format(str(e)))
+        logger.error(
+            "The .users command is required, but not available: {}".format(str(e))
+        )
         sys.exit(1)
     try:
-        result = director.call('version')
+        result = director.call("version")
     except (bareos.exceptions.Error) as e:
-        logger.error('The version command is required, but not available: {}'.format(str(e)))
+        logger.error(
+            "The version command is required, but not available: {}".format(str(e))
+        )
         sys.exit(1)
-    version = result['version']['version']
+    version = result["version"]["version"]
     if version < min_version:
-        logger.error('Bareos has version {}. However Bareos >= {} is required.'.format(version, min_version))
+        logger.error(
+            "Bareos has version {}. However Bareos >= {} is required.".format(
+                version, min_version
+            )
+        )
         sys.exit(1)
     logger.debug("checking requirements: finish")
 
 
 def get_user_names(director):
-    result = director.call('.users')['users']
-    users = [i['name'] for i in result]
+    result = director.call(".users")["users"]
+    users = [i["name"] for i in result]
     return users
 
 
@@ -54,44 +62,53 @@ def add_user(director, username, profile):
 
     result = director.call(
         'configure add user="{username}" profile="{profile}"'.format(
-            username = username,
-            profile = profile
+            username=username, profile=profile
         )
     )
-    
+
     try:
-        if result['configure']['add']['name'] != username:
-            logger.error('Failed to create user {}.'.format(username))
-            #logger.debug(str(result))
+        if result["configure"]["add"]["name"] != username:
+            logger.error("Failed to create user {}.".format(username))
+            # logger.debug(str(result))
             return False
     except KeyError:
-        logger.debug('result: {}'.format(pformat(result)))
+        logger.debug("result: {}".format(pformat(result)))
         errormessage = pformat(result)
         try:
-            errormessage = ''.join(result['error']['data']['messages']['error'])
+            errormessage = "".join(result["error"]["data"]["messages"]["error"])
         except KeyError:
             pass
-        print('Failed to add user {}:\n'.format(username))
-        print('{}'.format(errormessage))
+        print("Failed to add user {}:\n".format(username))
+        print("{}".format(errormessage))
         return False
-    
+
     return True
 
 
 def getArguments():
-    argparser = argparse.ArgumentParser(description='Add a PAM user to Bareos Director.')
-    argparser.add_argument('-d', '--debug', action='store_true', help="enable debugging output")
+    argparser = argparse.ArgumentParser(
+        description="Add a PAM user to Bareos Director."
+    )
+    argparser.add_argument(
+        "-d", "--debug", action="store_true", help="enable debugging output"
+    )
     bareos.bsock.DirectorConsole.argparser_add_default_command_line_arguments(argparser)
-    argparser.add_argument('--username', help="Name of the user to add. Default: content of ENV(PAM_USER)")
-    argparser.add_argument('--profile', default='webui-admin',
-                        help="Bareos Profile for the newly generated user")
+    argparser.add_argument(
+        "--username", help="Name of the user to add. Default: content of ENV(PAM_USER)"
+    )
+    argparser.add_argument(
+        "--profile",
+        default="webui-admin",
+        help="Bareos Profile for the newly generated user",
+    )
     args = argparser.parse_args()
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(
-        format='%(levelname)s %(module)s.%(funcName)s: %(message)s', level=logging.INFO)
+        format="%(levelname)s %(module)s.%(funcName)s: %(message)s", level=logging.INFO
+    )
     logger = logging.getLogger()
 
     args = getArguments()
@@ -99,7 +116,7 @@ if __name__ == '__main__':
         logger.setLevel(logging.DEBUG)
 
     bareos_args = bareos.bsock.DirectorConsole.argparser_get_bareos_parameter(args)
-    logger.debug('options: %s' % (bareos_args))
+    logger.debug("options: %s" % (bareos_args))
 
     try:
         director = bareos.bsock.DirectorConsoleJson(**bareos_args)
@@ -110,19 +127,19 @@ if __name__ == '__main__':
 
     check_requirements(director)
 
-    username = os.getenv('PAM_USER', args.username)
-    profile = getattr(args, 'profile', 'webui-admin')
+    username = os.getenv("PAM_USER", args.username)
+    profile = getattr(args, "profile", "webui-admin")
 
     if username is None:
-        logger.error('Failed: username not given.')
+        logger.error("Failed: username not given.")
         sys.exit(1)
-        
+
     if does_user_exists(director, username):
-        print('Skipped. User {} already exists.'.format(username))
+        print("Skipped. User {} already exists.".format(username))
         sys.exit(0)
-    
+
     if not add_user(director, username, profile):
-        logger.error('Failed to add user {}.'.format(username))
+        logger.error("Failed to add user {}.".format(username))
         sys.exit(1)
-        
-    print('Added user {} (with profile {}) to Bareos.'.format(username, profile))
+
+    print("Added user {} (with profile {}) to Bareos.".format(username, profile))
