3264abbc7aa93d30d7ec78fca553714da110cf42
Frank Ueberschar <frank.ueberschar@bareos.com>
2020-02-06 11:49:11 +0100

    tests: add messages unit test and test-config

---
+++ core/src/tests/messages.cc
@@ -0,0 +1,58 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2020-2020 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#if defined(HAVE_MINGW)
+#include "include/bareos.h"
+#include "gtest/gtest.h"
+#else
+#include "gtest/gtest.h"
+#include "include/bareos.h"
+#endif
+
+#include "dird/dird_conf.h"
+#include "dird/dird_globals.h"
+#include "lib/messages_resource.h"
+#include "lib/parse_conf.h"
+
+using directordaemon::InitDirConfig;
+using directordaemon::my_config;
+
+TEST(messages, send_message_to_all_configured_destinations)
+{
+  std::string path_to_config_file =
+      std::string(RELATIVE_PROJECT_SOURCE_DIR "/configs/messages");
+
+  std::unique_ptr<ConfigurationParser> p{
+      InitDirConfig(path_to_config_file.c_str(), M_ERROR_TERM)};
+
+  directordaemon::my_config = p.get();
+  ASSERT_TRUE(directordaemon::my_config->ParseConfig());
+
+  MessagesResource* messages =
+      dynamic_cast<MessagesResource*>(directordaemon::my_config->GetResWithName(
+          directordaemon::R_MSGS, "Standard"));
+
+  ASSERT_NE(messages, nullptr);
+
+  InitMsg(NULL, messages); /* initialize message handler */
+
+  DispatchMessage(nullptr, M_ERROR, 0, "This is an error message");
+}
