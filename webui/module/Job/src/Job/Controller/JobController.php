ffa12fa6a3dbf0b6cd3059745ae77cba626677a6
Frank Bergkemper <frank.bergkemper@bareos.com>
2020-02-27 14:44:31 +0100

    webui: fix typo

--- webui/module/Job/src/Job/Controller/JobController.php
+++ webui/module/Job/src/Job/Controller/JobController.php
@@ -5,7 +5,7 @@
  * bareos-webui - Bareos Web-Frontend
  *
  * @link      https://github.com/bareos/bareos for the canonical source repository
- * @copyright Copyright (c) 2013-2019 Bareos GmbH & Co. KG (http://www.bareos.org/)
+ * @copyright Copyright (c) 2013-2020 Bareos GmbH & Co. KG (http://www.bareos.org/)
  * @license   GNU Affero General Public License (http://www.gnu.org/licenses/)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -77,8 +77,8 @@ class JobController extends AbstractActionController
     }
 
     $period = $this->params()->fromQuery('period') ? $this->params()->fromQuery('period') : '7';
-    $status = $this->params()->fromQuery('status') ? $this->params()->fromQUery('status') : 'all';
-    $jobname = $this->params()->fromQuery('jobname') ? $this->params()->fromQUery('jobname') : 'all';
+    $status = $this->params()->fromQuery('status') ? $this->params()->fromQuery('status') : 'all';
+    $jobname = $this->params()->fromQuery('jobname') ? $this->params()->fromQuery('jobname') : 'all';
 
     try {
       $this->bsock = $this->getServiceLocator()->get('director');
