ca41c8de2de21a1bb5962b945a8e1b2398ce5c0d
Joerg Steffens <joerg.steffens@bareos.com>
2020-04-14 10:29:46 +0200

    python-bareos: adapted to be published at PyPI.
    (cherry picked from commit 5475451f5eb819c86db30c5209092d5a43aafba2)

--- python-bareos/setup.py
+++ python-bareos/setup.py
@@ -9,7 +9,7 @@ def get_version():
 
     try:
         with open(
-            os.path.join(base_dir, "python-bareos", "VERSION.txt")
+            os.path.join(base_dir, "bareos", "VERSION.txt")
         ) as version_file:
             __version__ = version_file.read().strip()
     except IOError:
@@ -24,24 +24,28 @@ def get_version():
 setup(
     name="python-bareos",
     version=get_version(),
+    license="AGPLv3",
     author="Bareos Team",
     author_email="packager@bareos.com",
     packages=find_packages(),
     scripts=["bin/bconsole.py", "bin/bconsole-json.py", "bin/bareos-fd-connect.py"],
     package_data={"bareos": ["VERSION.txt"]},
-    url="https://github.com/bareos/python-bareos/",
+    url="https://github.com/bareos/bareos/",
     # What does your project relate to?
     keywords="bareos",
-    description="Network socket connection to the Bareos backup system.",
+    description="Client library and tools for Bareos console access.",
     long_description=open("README.rst").read(),
+    long_description_content_type='text/x-rst',
     # Python 2.6 is used by RHEL/Centos 6.
     # When RHEL/Centos 6 is no longer supported (End of 2020),
     # Python 2.6 will no longer be supported by python-bareos.
     python_requires=">=2.6",
-    install_requires=[
-        #'hmac',
-        #'socket',
-        "python-dateutil"
-    ],
     extras_require={"TLS-PSK": ["sslpsk"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: System :: Archiving :: Backup",
+    ],
 )
