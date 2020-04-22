5a47b6d6ac8f22fbba8bdccb9d789cf0ca11a53a
Frank Bergkemper <frank.bergkemper@bareos.com>
2020-04-22 15:12:41 +0200

    dird: add JobIds used during restore to auditing

--- core/src/dird/ua_restore.cc
+++ core/src/dird/ua_restore.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2002-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2019 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2020 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -1134,7 +1134,11 @@ static bool BuildDirectoryTree(UaContext* ua, RestoreContext* rx)
     }
   }
 
-  ua->InfoMsg(_("\nBuilding directory tree for JobId(s) %s ...  "), rx->JobIds);
+  ua->InfoMsg(
+      _("\nBuilding directory tree for JobId(s) %s ...  "), rx->JobIds);
+
+  ua->LogAuditEventInfoMsg(
+      _("Building directory tree for JobId(s) %s"), rx->JobIds);
 
   if (!ua->db->GetFileList(ua->jcr, rx->JobIds, false /* do not use md5 */,
                            true /* get delta */, InsertTreeHandler,
