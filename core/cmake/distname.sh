f3ba48e25f00c465ce4ed6caeaa64dbfa724fd38
Andreas Pachler <apachler@paan-systems.com>
2020-04-26 16:14:52 +0200

    Added support for Manjaro Linux
    Also changed the lsb_release call for DISTVER to get the release number not the complete release description. I think this makes more sense for most distributions.

--- core/cmake/distname.sh
+++ core/cmake/distname.sh
@@ -29,7 +29,7 @@ else
          MandrivaLinux)
             DISTNAME=mandrake
             ;;
-         Arch|archlinux)
+         Arch|archlinux|ManjaroLinux)
             DISTNAME=archlinux
             ;;
          LinuxMint)
@@ -54,7 +54,7 @@ else
       #
       if test "x$DISTNAME" != "x"
       then
-         DISTVER=`lsb_release -d -s | sed -e 's/"//g'`
+         DISTVER=`lsb_release -r -s | sed -e 's/"//g'`
       fi
    fi
 
