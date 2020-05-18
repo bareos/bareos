b0ef356f65877c9fc3b0e40c9365683f6333a377
Philipp Storz <philipp.storz@bareos.com>
2020-05-18 15:09:42 +0200

    python plugins: use central c api definition generator

--- core/src/plugins/scripts/create_api_includes.sh
+++ core/src/plugins/scripts/create_api_includes.sh
@@ -2,34 +2,40 @@
 IFS='
 '
 
+PREFIX=$1
+if [ -z "$PREFIX" ]; then
+  echo "API prefix required. Bareosfd, Bareossd or Bareosdir"
+  exit 1
+fi
+
 exec >capi_1.inc
 
 echo "/* C API functions */"
 NUM=0
-for i in $(cat bareosfd_api_funcs.txt); do
+for i in $(cat api_definition.txt); do
   retval=$(echo $i | sed 's/ .*//g')
   funcname=$(echo $i | cut -b 5- | sed s/\(.*//g)
   prot=$(echo $i | sed s/.*\(//g | sed 's/);//g')
 echo "
 /* static $i */
-#define Bareosfd_${funcname}_NUM $NUM
-#define Bareosfd_${funcname}_RETURN $retval
-#define Bareosfd_${funcname}_PROTO (${prot})"
+#define ${PREFIX}_${funcname}_NUM $NUM
+#define ${PREFIX}_${funcname}_RETURN $retval
+#define ${PREFIX}_${funcname}_PROTO (${prot})"
 
 ((NUM=NUM+1))
 done
 echo "
 /*Total Number of C API function pointers */
-#define Bareosfd_API_pointers $NUM"
+#define ${PREFIX}_API_pointers $NUM"
 
 exec >capi_2.inc
 
 NUM=0
-for i in $(cat bareosfd_api_funcs.txt); do
+for i in $(cat api_definition.txt); do
   retval=$(echo $i | sed 's/ .*//g')
   funcname=$(echo $i | cut -b 5- | sed s/\(.*//g)
   prot=$(echo $i | sed s/.*\(//g | sed 's/);//g')
-  echo "#define Bareosfd_${funcname} (*(Bareosfd_${funcname}_RETURN(*)Bareosfd_${funcname}_PROTO) Bareosfd_API[Bareosfd_${funcname}_NUM])
+  echo "#define ${PREFIX}_${funcname} (*(${PREFIX}_${funcname}_RETURN(*)${PREFIX}_${funcname}_PROTO) ${PREFIX}_API[${PREFIX}_${funcname}_NUM])
 "
 ((NUM=NUM+1))
 done
@@ -37,10 +43,10 @@ done
 exec >capi_3.inc
 
 NUM=0
-for i in $(cat bareosfd_api_funcs.txt); do
+for i in $(cat api_definition.txt); do
   retval=$(echo $i | sed 's/ .*//g')
   funcname=$(echo $i | cut -b 5- | sed s/\(.*//g)
   prot=$(echo $i | sed s/.*\(//g | sed 's/);//g')
-  echo " Bareosfd_API[Bareosfd_${funcname}_NUM] = (void*)${funcname};"
+  echo " ${PREFIX}_API[${PREFIX}_${funcname}_NUM] = (void*)${funcname};"
 ((NUM=NUM+1))
 done
