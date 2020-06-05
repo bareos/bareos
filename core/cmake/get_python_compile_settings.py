ca3fa588e207f2e1f9de2b691fa04a7d7449d557
Philipp Storz <philipp.storz@bareos.com>
2020-06-05 22:24:35 +0200

    get_python_compile_settings.py : comment string changed

--- core/cmake/get_python_compile_settings.py
+++ core/cmake/get_python_compile_settings.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-   BAREOS(R) - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2020-2020 Bareos GmbH & Co. KG
+#   BAREOS(R) - Backup Archiving REcovery Open Sourced
+#
+#   Copyright (C) 2020-2020 Bareos GmbH & Co. KG
+#
+#   This program is Free Software; you can redistribute it and/or
+#   modify it under the terms of version three of the GNU Affero General Public
+#   License as published by the Free Software Foundation, which is
+#   listed in the file LICENSE.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#   Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#   02110-1301, USA.
 
-   This program is Free Software; you can redistribute it and/or
-   modify it under the terms of version three of the GNU Affero General Public
-   License as published by the Free Software Foundation, which is
-   listed in the file LICENSE.
+#  This program extracts the build configuration from the python interpreter.
+#  The output is consumed by cmake where appropriate variables are set.
+#  This is required to build python modules without setuptools.
 
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-   Affero General Public License for more details.
-
-   You should have received a copy of the GNU Affero General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301, USA.
-"""
-""" This program extracts the  build configuration from the python interpreter.
-    The output is consumed by cmake where appropriate variables are set.
-    This is required to build python modules without setuptools.
-"""
 import sys
 import sysconfig
 for var in ("CC", "BLDSHARED"):
