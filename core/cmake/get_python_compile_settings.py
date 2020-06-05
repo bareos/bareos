1795911531d276c0b3f8205a45828c433ee78c4e
Philipp Storz <philipp.storz@bareos.com>
2020-06-05 17:51:17 +0200

    cmake: build without setup tools works

--- core/cmake/get_python_compile_settings.py
+++ core/cmake/get_python_compile_settings.py
@@ -8,6 +8,13 @@ for var in ("CC", "BLDSHARED"):
     print ("message(STATUS \"Python{}_{}_FLAGS\ is\  {}\")" . format(sys.version_info.major, var, sysconfig.get_config_var(var).split(' ',1)[1]))
     print ("set(Python{}_{}_FLAGS \"{}\")" . format(sys.version_info.major, var, sysconfig.get_config_var(var).split(' ',1)[1]))
 
-for var in ("CFLAGS","CCSHARED","INCLUDEPY","LDFLAGS","EXT_SUFFIX"):
+for var in ("CFLAGS","CCSHARED","INCLUDEPY","LDFLAGS"):
     print ("message(STATUS \"Python{}_{}\ is\  {}\")" . format(sys.version_info.major, var, sysconfig.get_config_var(var)))
     print ("set(Python{}_{} \"{}\")" . format(sys.version_info.major, var, sysconfig.get_config_var(var)))
+
+for var in ("EXT_SUFFIX",):
+    value = sysconfig.get_config_var(var)
+    if value is None:
+        value = ''
+    print ("message(STATUS \"Python{}_{}\ is\  {}\")" . format(sys.version_info.major, var, value))
+    print ("set(Python{}_{} \"{}\")" . format(sys.version_info.major, var, value))
