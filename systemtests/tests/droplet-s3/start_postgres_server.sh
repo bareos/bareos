166f2aeaa54fc63a37d795a10123169e2a52cf46
Frank Ueberschar <frank.ueberschar@bareos.com>
2020-06-11 12:27:21 +0200

    systemtests: remove redundant information from backup-bareos-droplet-s3-test

--- systemtests/tests/droplet-s3/start_postgres_server.sh
+++ systemtests/tests/droplet-s3/start_postgres_server.sh
@@ -4,7 +4,7 @@ working="working"
 
 . environment
 
-killall -SIGTERM postgres
+killall -SIGTERM postgres > /dev/null 2>&1
 
 database_data_dir="$working"/pgsql/data
 
@@ -12,17 +12,19 @@ if [ -d "$database_data_dir" ]; then
   rm -rf "$database_data_dir"
 fi
 
-mkdir -p "$database_data_dir"
-chown postgres "$database_data_dir"
+mkdir -p "$database_data_dir" || exit 1
+chown postgres "$database_data_dir" || exit 2
 
 sudo --login --user=postgres bash << EOF
-initdb -D "$database_data_dir"
+initdb -D "$database_data_dir" || exit 3
 postgres -D "$database_data_dir" > /dev/null 2>&1 &
 
 while ! psql -l > /dev/null 2>&1; do
   sleep 0.1
 done
 
-createuser --superuser root
-
+createuser --superuser root || exit 4
 EOF
+
+exit 0
+
