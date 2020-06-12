429c7cf5fe836bfbd3575210c58127b39d3bb156
Philipp Storz <philipp.storz@bareos.com>
2020-06-12 19:39:08 +0200

    pyplug-fd-postgres test: can be run as root user
    When running as user root, the database needs to run as user
    postgresql as pg_ctl cannot be started as root.

--- systemtests/tests/pyplug-fd-postgres/database/setup_local_db.sh
+++ systemtests/tests/pyplug-fd-postgres/database/setup_local_db.sh
@@ -2,7 +2,11 @@
 
 local_db_stop_server() {
   echo "stop db server"
-  pg_ctl --silent --pgdata=data stop
+  if [ $UID -eq 0 ]; then
+    su postgres -c "pg_ctl --pgdata=data stop"
+  else
+    pg_ctl --silent --pgdata=data stop
+  fi
   tries=10
   while psql --host="$1" --list > /dev/null 2>&1; do
     echo " -- $tries -- "
@@ -16,15 +20,20 @@ local_db_stop_server() {
 
 local_db_prepare_files() {
   echo "Prepare files"
-  rm --recursive --force tmp data log
+  rm --recursive --force tmp data log wal_archive
   mkdir tmp data log wal_archive
-  LANG= pg_ctl --silent --pgdata=data --log=log/postgres.log initdb
-
+  if [ $UID -eq 0 ]; then
+    chown postgres  tmp data log wal_archive
+    LANG= su postgres -c "pg_ctl --pgdata=data --log=log/postgres.log initdb"
+  else
+    LANG= pg_ctl --silent --pgdata=data --log=log/postgres.log initdb
+  fi
   sed -i.bak "s@#listen_addresses.*@listen_addresses = ''@g" data/postgresql.conf
   sed -i.bak "s@#unix_socket_directories.*@unix_socket_directories = \'$1\'@g" data/postgresql.conf
 
   {
-  # for online backups we need wal_archiving
+  # fo
+r online backups we need wal_archiving
     echo "wal_level = archive"
     echo "archive_mode = on"
     echo "archive_command = 'cp %p ../wal_archive'"
@@ -34,31 +43,39 @@ local_db_prepare_files() {
 
 local_db_start_server() {
   echo "start db server"
-  pg_ctl --silent --pgdata=data --log=log/logfile start
+  if [ $UID -eq 0 ]; then
+    su postgres -c "pg_ctl -w --pgdata=data --log=log/logfile start"
+  else
+    pg_ctl --silent --pgdata=data --log=log/logfile start
+  fi
+#  tries=60
+#  while ! psql --host="$1" --list > /dev/null 2>&1; do
+#    [ $((tries-=1)) -eq 0 ] && {
+#      echo "Could not start postgres server"
+#      return 1
+#    }
+#    sleep 1
+#  done
 
-  tries=60
-  while ! psql --host="$1" --list > /dev/null 2>&1; do
-    [ $((tries-=1)) -eq 0 ] && {
-      echo "Could not start postgres server"
-      return 1
-    }
-    sleep 1
-  done
-
-  tries=60
-  while ! echo "select pg_is_in_recovery()" | psql --host="$1" postgres | grep -q -e "^ f$" ; do
-    [ $((tries-=1)) -eq 0 ] && {
-      echo "Could not start postgres server (still recovering)"
-      return 1
-    }
-    sleep 1
-  done
+#  tries=60
+#  while ! echo "select pg_is_in_recovery()" | psql --host="$1" postgres | grep -q -e "^ f$" ; do
+#    [ $((tries-=1)) -eq 0 ] && {
+#      echo "Could not start postgres server (still recovering)"
+#      return 1
+#    }
+#    sleep 1
+#  done
 
   return 0
 }
 
 local_db_create_superuser_role() {
-  echo "CREATE ROLE root WITH SUPERUSER CREATEDB CREATEROLE REPLICATION LOGIN" |psql -h "$1" postgres
+  if [ $UID -eq 0 ]; then
+    su postgres -c "createuser -s --host=$1 root"
+    su postgres -c "createdb   --host=$1 root"
+  else
+    echo "CREATE ROLE root WITH SUPERUSER CREATEDB CREATEROLE REPLICATION LOGIN" |psql -h "$1" postgres
+  fi
 }
 
 setup_local_db() {
@@ -66,9 +83,11 @@ setup_local_db() {
   local_db_prepare_files "$1"
   if ! local_db_start_server "$1"; then return 1; fi
   local_db_create_superuser_role "$1"
-
-  echo stop server with "pg_ctl --pgdata=data stop"
-
+  if [ $UID -eq 0 ]; then
+    echo stop server with "pg_ctl --pgdata=data stop"
+  else
+    echo stop server with "su postgres -c ' pg_ctl--pgdata=data stop'"
+  fi
   return 0
 }
 
