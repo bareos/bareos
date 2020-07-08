c6e8690dc89b378802fc2233b8d2ebb1639e11b0
Frank Ueberschar <frank.ueberschar@bareos.com>
2020-07-08 11:39:46 +0200

    stored: add compile definition for sd droplet device

--- core/src/stored/dev.cc
+++ core/src/stored/dev.cc
@@ -88,8 +88,10 @@
 #ifdef HAVE_GFAPI
 #include "backends/gfapi_device.h"
 #endif
+#ifdef HAVE_BAREOSSD_DROPLET_DEVICE
 #include "backends/chunked_device.h"
 #include "backends/droplet_device.h"
+#endif
 #ifdef HAVE_RADOS
 #include "backends/rados_device.h"
 #endif
