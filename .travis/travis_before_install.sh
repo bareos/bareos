bc0e7851c11ef6124cfac8e12357fb45b097b164
Joerg Steffens <joerg.steffens@bareos.com>
2020-08-27 17:08:27 +0200

    travis: fix the travis checks
    The Travis checks did fail, because the Bareos project directory structure have changed.
    The Bareos WebUI is now build and packaged in the same step as the Bareos core.
    
    Also disables the tests for MySQL and Sqlite, as these are deprecated.
    
    Debian build now honors the flag
    DEB_BUILD_OPTIONS="nocheck"
    to skip tests.

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+set -e
+set -u
+
 sudo apt-get -qq update
 # qt5 should be used. Remove qt4-dev to avoid confusion.
 sudo apt-get remove libqt4-dev
-cd core
 dpkg-checkbuilddeps 2> /tmp/dpkg-builddeps || true
-if [ $BUILD_WEBUI ]; then
+if [ "${BUILD_WEBUI:-}" ]; then
     sudo -H pip install --upgrade pip 'urllib3>=1.22'
     sudo -H pip install sauceclient selenium
-    cd ../webui
-    dpkg-checkbuilddeps 2>> /tmp/dpkg-builddeps || true
 fi
 cat /tmp/dpkg-builddeps
 sed -e "s/^.*:.*:\s//" -e "s/\s([^)]*)//g" -e "s/|/ /g" -e "s/ /\n/g" /tmp/dpkg-builddeps > /tmp/build_depends
 sudo apt-get -q --assume-yes install fakeroot
 cat /tmp/build_depends | while read pkg; do
-echo "installing $pkg"
-sudo apt-get -q --assume-yes install $pkg
+  echo "installing $pkg"
+  sudo apt-get -q --assume-yes install $pkg || true
 done
-true
