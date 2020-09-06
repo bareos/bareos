a2ba18329d776d86b6d93fa43207a2a3a14ef64d
Philipp Storz <philipp.storz@bareos.com>
2020-09-06 22:25:13 +0200

    edit_test.cc: keep only two well-named tests

--- core/src/tests/test_edit.cc
+++ core/src/tests/test_edit.cc
@@ -28,7 +28,7 @@
 
 #include "lib/edit.h"
 
-TEST(edit, format_1k)
+TEST(edit, convert_number_to_siunits)
 {
   ASSERT_STREQ(SizeAsSiPrefixFormat(1).c_str(), "1");
   ASSERT_STREQ(SizeAsSiPrefixFormat(1024).c_str(), "1 k");
@@ -46,115 +46,116 @@ TEST(edit, format_1k)
 }
 
 // kibibyte
-TEST(edit, k)
+TEST(edit, convert_siunits_to_numbers)
 {
-  char str[] = "1 k";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1024);
-}
+  {
+    char str[] = "1 k";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1024);
+  }
 
 
-// kilobyte
-TEST(edit, kb)
-{
-  char str[] = "1 kb";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000);
-}
-TEST(edit, kB)
-{
-  char str[] = "1 KB";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000);
-}
+  // kilobyte
+  {
+    char str[] = "1 kb";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000);
+  }
+  {
+    char str[] = "1 KB";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000);
+  }
 
-// mebibyte
-TEST(edit, m)
-{
-  char str[] = "1 m";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1024 * 1024);
-}
+  // mebibyte
+  {
+    char str[] = "1 m";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1024 * 1024);
+  }
 
-// megabyte
-TEST(edit, mb)
-{
-  char str[] = "1 mb";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000 * 1000);
-}
+  // megabyte
+  {
+    char str[] = "1 mb";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000 * 1000);
+  }
 
 
-// gibibyte
-TEST(edit, g)
-{
-  char str[] = "1 g";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1024 * 1024 * 1024);
-}
-// gigabyte
-TEST(edit, gb)
-{
-  char str[] = "1 gb";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000 * 1000 * 1000);
-}
+  // gibibyte
+  {
+    char str[] = "1 g";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1024 * 1024 * 1024);
+  }
+  // gigabyte
+  {
+    char str[] = "1 gb";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000 * 1000 * 1000);
+  }
 
 
-// tebibyte
-TEST(edit, t)
-{
-  char str[] = "1 t";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1099511627776);
-}
-// terabyte
-TEST(edit, tb)
-{
-  char str[] = "1 tb";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000000000000);
-}
+  // tebibyte
+  {
+    char str[] = "1 t";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1099511627776);
+  }
+  // terabyte
+  {
+    char str[] = "1 tb";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000000000000);
+  }
 
-// pebibyte
-TEST(edit, p)
-{
-  char str[] = "1 p";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1125899906842624);
-}
-// petabyte
-TEST(edit, pb)
-{
-  char str[] = "1 pb";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000000000000000);
-}
+  // pebibyte
+  {
+    char str[] = "1 p";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1125899906842624);
+  }
+  // petabyte
+  {
+    char str[] = "1 pb";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000000000000000);
+  }
 
-// exbibyte
-TEST(edit, e)
-{
-  char str[] = "1 e";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1152921504606846976);
-}
-// exabyte
-TEST(edit, eb)
-{
-  char str[] = "1 eb";
-  uint64_t retvalue = 0;
-  size_to_uint64(str, &retvalue);
-  ASSERT_EQ(retvalue, 1000000000000000000);
+  // exbibyte
+  {
+    char str[] = "1 e";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1152921504606846976);
+  }
+  // exabyte
+  {
+    char str[] = "1 eb";
+    uint64_t retvalue = 0;
+    size_to_uint64(str, &retvalue);
+    ASSERT_EQ(retvalue, 1000000000000000000);
+  }
+  // size_to_uint64 only checks for first modifier so the following does not
+  // work
+  /*
+    {
+      char str[] = "1 e 1 p 1 t 1 g 1 m 1 k 1";
+      uint64_t retvalue = 0;
+      size_to_uint64(str, &retvalue);
+      ASSERT_EQ(retvalue, 1152921504606846976 + 1125899906842624 + 1099511627776
+    + 1073741824 + 1048576 + 1024 + 1);
+    }
+  */
 }
