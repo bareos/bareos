f4ba8b447e17742fcbdb07f02df6155cd149f4be
Frank Ueberschar <frank.ueberschar@bareos.com>
2020-09-24 10:54:30 +0200

    systemtests: start minio separately for each test
    - each test using minio starts minio with a unique port number
      and a dedicated name

--- systemtests/scripts/stop_minio.sh
+++ systemtests/scripts/stop_minio.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+set -e
+set -u
+
 . environment
+
+minio_alias=$1-minio
 
 echo "$0: stopping minio server"
 
 tries=0
-while pidof minio > /dev/null; do
-  kill -SIGTERM "$(pidof minio)"
+while pidof "$minio_alias" > /dev/null; do
+  if ! pkill -f -SIGTERM "$minio_alias" ; then
+    break
+  fi
   sleep 0.1
   (( tries++ )) && [ $tries == '100' ] \
     && { echo "$0: could not stop minio server"; exit 1; }
 done
 
-if ! pidof minio; then
+if ! pidof "$minio_alias"; then
   exit 0
 fi
 
