189374e7e22fe1dfc61369f74b236e8a20e0b883
Andreas Rogge <andreas.rogge@bareos.com>
2020-12-09 18:15:12 +0100

    vmware: check VDDK bug triggers in wrapper script
    The VDDK after 6.5 has at least two issues that can cause a crash,
    this patch tests for the circumstances that trigger the bug and
    will lead to a crash, telling the user how to work around the issue.
    
    The patch also removes the setting of LD_LIBRARY_PATH as it is not
    required anymore.

--- core/src/vmware/vadp_dumper/bareos_vadp_dumper_wrapper.sh
+++ core/src/vmware/vadp_dumper/bareos_vadp_dumper_wrapper.sh
@@ -18,5 +18,32 @@
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #   02110-1301, USA.
 
-export LD_LIBRARY_PATH=/usr/lib/vmware-vix-disklib/lib64
+cancel=0
+
+if [ ! -d /sys/class/scsi_disk/ ]; then
+  cat <<EOT >&2
+  Your system does not have /sys/class/scsi_disk/ which is required for
+  VADP dumper due to a bug in VMware's VDDK. Please load the sd_mod
+  kernel module and make sure /sys/class/scsi_disk/ exists.
+  You can do this temporarily by running "modprobe sd_mod" as root.
+  Please refer to your OS documentation how to persist this change across reboots.
+EOT
+  cancel=1
+fi
+
+if [ ! -r /etc/mtab ]; then
+  cat <<EOT >&2
+  Your system does not have /etc/mtab which is required for VADP dumper
+  due to a bug in VMware's VDDK. Please create /etc/mtab as a symlink to
+  /proc/self/mounts.
+  This can be done by running "cd /etc; ln -s ../proc/self/mounts mtab"
+  as root.
+EOT
+  cancel=1
+fi
+
+if [ "$cancel" -eq 1 ]; then
+  exit 2
+fi
+
 exec bareos_vadp_dumper "$@"
