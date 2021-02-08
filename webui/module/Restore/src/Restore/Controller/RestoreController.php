872cfa50a703dd69a4013f90c4c935152338564a
Frank Bergkemper <f.bergkemper@gmail.com>
2021-02-08 10:35:25 +0100

    Revert "webui: change updateBvfsCache trigger condition in restore controller"
    This reverts commit 9967eb01f7297735d2e838dce6569ce8273a0c5c and
    corrects the comparison operators.

--- webui/module/Restore/src/Restore/Controller/RestoreController.php
+++ webui/module/Restore/src/Restore/Controller/RestoreController.php
@@ -550,9 +550,10 @@ class RestoreController extends AbstractActionController
     $this->setRestoreParams();
     $this->getJobIds();
 
-    if($this->restore_params['id'] !== null && $this->restore_params['id'] !== "#") {
+    if($this->restore_params['id'] === null || $this->restore_params['id'] === "#") {
       $this->updateBvfsCache();
     }
+
     $this->getDirectories();
     $this->getFiles();
 
