bareos/bareos

commit 84a44ca6cc4d3bd4fcbe428c6433722cae251d35

check-sources: apply whitespace plugin
* strip trailing spaces
* strip more than one trailing newline
* convert to unix line-endings where possible

systemtests/tests/config-dump/testrunner
@@ -23,7 +23,7 @@ start_bareos_dir()
     if ! ${rscripts}/bareos-ctl-dir status >/dev/null; then
         set_error "Director does not run anymore. This should not happen."
         exit 1
-    fi    
+    fi
 }
 
 remove_bconsole_commands_from_output()
@@ -65,7 +65,7 @@ dump_config()
     fi
 
     "${BAREOS_DIRECTOR_BINARY}" -c "${configfile}" -xc > $tmp/bareos-dir-xc-${ext}.conf
-    
+
     start_bareos_dir -c "${configfile}"
 
 cat <<END_OF_DATA >$tmp/bconcmds
@@ -86,14 +86,14 @@ quit
 END_OF_DATA
 
     run_bconsole
-    
+
     remove_bconsole_commands_from_output "$tmp/bareos-dir-show-${ext}.conf"
     remove_bconsole_commands_from_output "$tmp/bareos-dir-show-verbose-${ext}.conf"
     remove_bconsole_commands_from_output "$tmp/bareos-dir-show-${ext}.json"
     remove_bconsole_commands_from_output "$tmp/bareos-dir-show-verbose-${ext}.json"
-    
+
     stop_bareos > $output 2>&1
-    
+
     if ${rscripts}/bareos-ctl-dir status >/dev/null; then
         set_error "Director is not stopped."
         exit 1

systemtests/tests/client-initiated/testrunner
@@ -100,4 +100,3 @@ check_for_zombie_jobs storage=File
 check_two_logs
 check_restore_diff "${BackupDirectory}"
 end_test
-

systemtests/tests/copy-remote-bscan/testrunner
@@ -141,7 +141,7 @@ wait
 messages
 quit
 END_OF_DATA
-                                                                                
+
 run_bconsole "$tmp/bconcmds2"
 
 check_for_zombie_jobs storage=File

systemtests/tests/multiplied-device/testrunner
@@ -71,4 +71,3 @@ if test $? -ne 0 ; then
 fi
 
 end_test
-

systemtests/tests/reload-add-client/testrunner
@@ -50,4 +50,3 @@ stop_director
 ############################################
 end_test
 ############################################
-

systemtests/tests/reload-add-duplicate-job/testrunner
@@ -50,4 +50,3 @@ stop_director
 ############################################
 end_test
 ############################################
-

systemtests/tests/reload-add-empty-job/testrunner
@@ -46,4 +46,3 @@ stop_director
 ############################################
 end_test
 ############################################
-

systemtests/tests/reload-add-second-director/testrunner
@@ -50,4 +50,3 @@ stop_director
 ############################################
 end_test
 ############################################
-

commit 9745ef9656c0013d60a2a6e3a5d804931a8661b3

systemtest config-dump: ignore whitespaces in config files
Workaround for a problem introduced by 84a44ca6cc4d3bd4fcbe428c6433722cae251d35.
This test has been whitespace sensitive, but that commit removed all trailing whitespaces.
As a workaround for this problem, we use
diff --ignore-blank-lines --ignore-space-changes
instead of a diff without parameter.
The parameters of diff and sed have been tested to work on Linux and on FreeBSD.

systemtests/CMakeLists.txt
@@ -589,7 +589,6 @@ set(SYSTEM_TESTS_DISABLED # initially empty
 # add tests also here that are unreliable they are excluded when running tests
 # during build in jenkins
 set(SYSTEM_TESTS_BROKEN
-    config-dump
     dbcopy-mysql-postgresql
     py2plug-fd-postgres
     py3plug-fd-local-fileset

systemtests/tests/config-dump/testrunner
@@ -30,7 +30,7 @@ remove_bconsole_commands_from_output()
 {
     FILE="$1"
     # remove first and last line from file
-    sed -i -e '1d' -e '$ d' ${FILE}
+    sed -i'.bak' -e '1d' -e '$ d' ${FILE}
 }
 
 strip_config()
@@ -63,6 +63,9 @@ dump_config()
         set_error "Director config file \"${configfile}\" does not exist."
         exit 1
     fi
+
+    DESC="dump_config ${configfile} ext=${ext}"
+    print_debug "*** start $DESC"
 
     "${BAREOS_DIRECTOR_BINARY}" -c "${configfile}" -xc > $tmp/bareos-dir-xc-${ext}.conf
 
@@ -98,6 +101,8 @@ END_OF_DATA
         set_error "Director is not stopped."
         exit 1
     fi
+
+    print_debug "*** end   $DESC"
 }
 
 diff_files()
@@ -107,7 +112,9 @@ diff_files()
     base1="$(basename "$file1" .conf)"
     base2="$(basename "$file2" .conf)"
     difffile="${tmp}/${base1}_${base2}.diff"
-    DIFF_CMD='diff "${file1}" "${file2}"'
+    # --ignore-space-change could be replaced by --ignore-trailing-space,
+    # however, the FreeBSD version of diff does not support this option.
+    DIFF_CMD='diff --ignore-blank-lines --ignore-space-change "${file1}" "${file2}"'
     if ! eval "$DIFF_CMD" > "${difffile}"; then
         echo "Differences found. Output of:"
         eval "echo $DIFF_CMD"