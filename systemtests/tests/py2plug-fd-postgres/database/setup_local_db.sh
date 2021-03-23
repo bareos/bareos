254817ec15d9eaf2df06fdc1b3be30b98ed19ca9
Frank Ueberschar <frank.ueberschar@bareos.com>
2021-03-23 16:01:07 +0100

    pyplug-fd-postgres: cancel job when using python > 3.7
    Co-authored-by: Jörg Steffens <joergsteffens@users.noreply.github.com>

--- systemtests/tests/py2plug-fd-postgres/database/setup_local_db.sh
+++ systemtests/tests/py2plug-fd-postgres/database/setup_local_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #   BAREOS® - Backup Archiving REcovery Open Sourced
 #
-#   Copyright (C) 2020-2020 Bareos GmbH & Co. KG
+#   Copyright (C) 2020-2021 Bareos GmbH & Co. KG
 #
 #   This program is Free Software; you can redistribute it and/or
 #   modify it under the terms of version three of the GNU Affero General Public
@@ -97,7 +97,7 @@ local_db_create_superuser_role() {
 }
 
 setup_local_db() {
-  local_db_stop_server "$1"
+  local_db_stop_server "$1" || true
   local_db_prepare_files "$1"
   if ! local_db_start_server "$1"; then return 1; fi
   local_db_create_superuser_role "$1"
