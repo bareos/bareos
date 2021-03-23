f0772b5ac9006e21093956eb38f5a51cfcaff110
Joerg Steffens <joerg.steffens@bareos.com>
2021-03-23 15:21:22 +0100

    bareos-triggerjob: code formatting

--- contrib/misc/triggerjob/bareos-triggerjob.py
+++ contrib/misc/triggerjob/bareos-triggerjob.py
@@ -7,38 +7,42 @@ import logging
 import sys
 
 
-
 def get_job_names(director):
-    result = director.call('.jobs')['jobs']
-    jobs = [job['name'] for job in result]
+    result = director.call(".jobs")["jobs"]
+    jobs = [job["name"] for job in result]
     return jobs
 
 
 def get_connected_clients(director):
-    result = director.call('status director')['client-connection']
-    clients = [client['name'] for client in result]
+    result = director.call("status director")["client-connection"]
+    clients = [client["name"] for client in result]
     return clients
 
 
 def trigger(director, jobnames, clients, hours):
     for client in clients:
-        jobname = 'backup-{}'.format(client)
+        jobname = "backup-{}".format(client)
         if not jobname in jobnames:
-            print('{} skipped, no matching job ({}) found'.format(client, jobname))
+            print("{} skipped, no matching job ({}) found".format(client, jobname))
         else:
             jobs = director.call(
-                ('list jobs client={} hours={}').format(client, hours))['jobs']
+                ("list jobs client={} hours={}").format(client, hours)
+            )["jobs"]
             if jobs:
                 job = director.call(
-                    ('list jobs client={} hours={} last').format(client, hours))['jobs'][0]
-                jobinfo = '{starttime}: jobid={jobid}, level={level}, status={jobstatus}'.format(
-                    **job)
-                print('{}: skipped, recent backups available ({})'.format(
-                    jobname, jobinfo))
+                    ("list jobs client={} hours={} last").format(client, hours)
+                )["jobs"][0]
+                jobinfo = "{starttime}: jobid={jobid}, level={level}, status={jobstatus}".format(
+                    **job
+                )
+                print(
+                    "{}: skipped, recent backups available ({})".format(
+                        jobname, jobinfo
+                    )
+                )
             else:
-                jobid = director.call('run {} yes'.format(jobname))[
-                    'run']['jobid']
-                print('{}: backup triggered, jobid={}'.format(jobname, jobid))
+                jobid = director.call("run {} yes".format(jobname))["run"]["jobid"]
+                print("{}: backup triggered, jobid={}".format(jobname, jobid))
 
 
 def getArguments():
@@ -52,18 +56,22 @@ def getArguments():
     Note: bareos-triggerjob requires a connection between director and client. Therefore, activate Client Initiated Connections (https://docs.bareos.org/TasksAndConcepts/NetworkSetup.html#client-initiated-connection) to automatically establish a connection whenever possible. Otherwise no jobs will be started.
 
     """
-    
-    argparser = argparse.ArgumentParser(description=u"Trigger Bareos jobs.", epilog=epilog)
-    argparser.add_argument('-d', '--debug', action='store_true',
-                        help="enable debugging output")
+
+    argparser = argparse.ArgumentParser(
+        description=u"Trigger Bareos jobs.", epilog=epilog
+    )
+    argparser.add_argument(
+        "-d", "--debug", action="store_true", help="enable debugging output"
+    )
     bareos.bsock.DirectorConsole.argparser_add_default_command_line_arguments(argparser)
     args = argparser.parse_args()
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(
-        format='%(levelname)s %(module)s.%(funcName)s: %(message)s', level=logging.INFO)
+        format="%(levelname)s %(module)s.%(funcName)s: %(message)s", level=logging.INFO
+    )
     logger = logging.getLogger()
 
     args = getArguments()
@@ -71,7 +79,7 @@ if __name__ == '__main__':
         logger.setLevel(logging.DEBUG)
 
     try:
-        options = ['address', 'port', 'dirname', 'name']
+        options = ["address", "port", "dirname", "name"]
         parameter = {}
         for i in options:
             if hasattr(args, i) and getattr(args, i) != None:
@@ -79,9 +87,9 @@ if __name__ == '__main__':
                 parameter[i] = getattr(args, i)
             else:
                 logger.debug('%s: ""' % (i))
-        logger.debug('options: %s' % (parameter))
+        logger.debug("options: %s" % (parameter))
         password = bareos.bsock.Password(args.password)
-        parameter['password'] = password
+        parameter["password"] = password
         director = bareos.bsock.DirectorConsoleJson(**parameter)
     except RuntimeError as e:
         print(str(e))
