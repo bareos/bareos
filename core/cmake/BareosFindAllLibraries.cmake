c4f2cb9b5e410abd64f9939e8c263f2636762f8a
Frank Ueberschar <frank.ueberschar@bareos.com>
2021-03-26 11:10:40 +0100

    systemtests: disable some tests if python > 3.7

--- core/cmake/BareosFindAllLibraries.cmake
+++ core/cmake/BareosFindAllLibraries.cmake
@@ -1,6 +1,6 @@
 #   BAREOS® - Backup Archiving REcovery Open Sourced
 #
-#   Copyright (C) 2017-2020 Bareos GmbH & Co. KG
+#   Copyright (C) 2017-2021 Bareos GmbH & Co. KG
 #
 #   This program is Free Software; you can redistribute it and/or
 #   modify it under the terms of version three of the GNU Affero General Public
@@ -39,6 +39,16 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
 else()
   find_package(Python2 COMPONENTS Interpreter Development)
   find_package(Python3 COMPONENTS Interpreter Development)
+
+  set(Python3_VERSION_MAJOR
+      ${Python3_VERSION_MAJOR}
+      PARENT_SCOPE
+  )
+
+  set(Python3_VERSION_MINOR
+      ${Python3_VERSION_MINOR}
+      PARENT_SCOPE
+  )
 
   if(${Python2_FOUND} OR ${Python3_FOUND})
     set(HAVE_PYTHON 1)
