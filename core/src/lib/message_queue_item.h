4b1d38f5dc25896a59201473a6a04c809842cea0
Philipp Storz <philipp.storz@bareos.com>
2021-03-30 15:16:46 +0200

    message.cc: fix memleak

--- core/src/lib/message_queue_item.h
+++ core/src/lib/message_queue_item.h
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2019-2019 Bareos GmbH & Co. KG
+   Copyright (C) 2019-2021 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -35,7 +35,7 @@ class MessageQueueItem {
   dlink link_;
   int type_ = 0;
   utime_t mtime_ = {0};
-  std::string msg_;
+  std::string* msg_{nullptr};
 };
 
 
