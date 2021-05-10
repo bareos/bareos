4457ed6a79ebcd836f876dadd00faa08ce4fc445
Andreas Rogge <andreas.rogge@bareos.com>
2021-05-10 21:07:10 +0200

    lib: refactor alist.h to inline all methods
    To improve readability all methods have been moved into the class
    declaration itself which will imply the class templating and inlining.

--- core/src/lib/alist.h
+++ core/src/lib/alist.h
@@ -101,187 +101,129 @@ template <typename T> class alist {
   int num_grow = 0;
   int cur_item = 0;
   bool own_items = false;
-  void GrowList(void);
+  /* Private grow list function. Used to insure that
+   *   at least one more "slot" is available.
+   */
+  void GrowList(void)
+  {
+    if (items == NULL) {
+      if (num_grow == 0) { num_grow = 1; /* default if not initialized */ }
+      items = (T*)malloc(num_grow * sizeof(T));
+      max_items = num_grow;
+    } else if (num_items == max_items) {
+      max_items += num_grow;
+      items = (T*)realloc(items, max_items * sizeof(T));
+    }
+  }
 
  public:
   // Ueb disable non pointer initialization
-  alist(int num = 1, bool own = true);
-  ~alist();
-  void init(int num = 1, bool own = true);
-  void append(T item);
-  void prepend(T item);
-  T remove(int index);
-  T get(int index);
-  bool empty() const;
-  T prev();
-  T next();
-  T first();
-  T last();
-  T operator[](int index) const;
-  int current() const { return cur_item; }
-  int size() const;
-  void destroy();
-  void grow(int num);
+  alist(int num = 1, bool own = true) { init(num, own); }
+  ~alist() { destroy(); }
 
-  std::list<std::string> to_std_list_string();
+  /* This allows us to do explicit initialization,
+   *   allowing us to mix C++ classes inside malloc'ed
+   *   C structures. Define before called in constructor.
+   */
+  void init(int num = 1, bool own = true)
+  {
+    items = nullptr;
+    num_items = 0;
+    max_items = 0;
+    num_grow = num;
+    own_items = own;
+    cur_item = 0;
+  }
+  void append(T item)
+  {
+    GrowList();
+    items[num_items++] = item;
+  }
+  void prepend(T item)
+  {
+    GrowList();
+    if (num_items == 0) {
+      items[num_items++] = item;
+      return;
+    }
+    for (int i = num_items; i > 0; i--) { items[i] = items[i - 1]; }
+    items[0] = item;
+    num_items++;
+  }
+  T remove(int index)
+  {
+    T item;
+    if (index < 0 || index >= num_items) { return NULL; }
+    item = items[index];
+    num_items--;
+    for (int i = index; i < num_items; i++) { items[i] = items[i + 1]; }
+    return item;
+  }
+  T get(int index)
+  {
+    if (index < 0 || index >= num_items) { return NULL; }
+    return items[index];
+  }
+  bool empty() const { return num_items == 0; }
+  T prev()
+  {
+    if (cur_item <= 1) {
+      return NULL;
+    } else {
+      return items[--cur_item];
+    }
+  }
+  T next()
+  {
+    if (cur_item >= num_items) {
+      return NULL;
+    } else {
+      return items[cur_item++];
+    }
+  }
+  T first()
+  {
+    cur_item = 1;
+    if (num_items == 0) {
+      return NULL;
+    } else {
+      return items[0];
+    }
+  }
+  T last()
+  {
+    if (num_items == 0) {
+      return NULL;
+    } else {
+      cur_item = num_items;
+      return items[num_items - 1];
+    }
+  }
+  T operator[](int index) const
+  {
+    if (index < 0 || index >= num_items) { return nullptr; }
+    return items[index];
+  }
+  int current() const { return cur_item; }
+  int size() const { return num_items; }
+  void destroy()
+  {
+    if (items) {
+      if (own_items) {
+        for (int i = 0; i < num_items; i++) {
+          free((void*)(items[i]));
+          items[i] = NULL;
+        }
+      }
+      free(items);
+      items = NULL;
+    }
+  }
+  void grow(int num) { num_grow = num; }
 
   // Use it as a stack, pushing and popping from the end
   void push(T item) { append(item); }
   T pop() { return remove(num_items - 1); }
 };
-
-// Define index operator []
-template <typename T> inline T alist<T>::operator[](int index) const
-{
-  if (index < 0 || index >= num_items) { return nullptr; }
-  return items[index];
-}
-
-template <typename T> inline bool alist<T>::empty() const
-{
-  return num_items == 0;
-}
-
-/* This allows us to do explicit initialization,
- *   allowing us to mix C++ classes inside malloc'ed
- *   C structures. Define before called in constructor.
- */
-template <typename T> inline void alist<T>::init(int num, bool own)
-{
-  items = nullptr;
-  num_items = 0;
-  max_items = 0;
-  num_grow = num;
-  own_items = own;
-  cur_item = 0;
-}
-
-template <typename T> inline alist<T>::alist(int num, bool own)
-{
-  init(num, own);
-}
-
-template <typename T> inline alist<T>::~alist() { destroy(); }
-
-template <typename T> inline int alist<T>::size() const
-{
-  /* Check for null pointer, which allows test
-   *  on size to succeed even if nothing put in
-   *  alist.
-   */
-  return num_items;
-}
-
-/* How much to grow by each time */
-template <typename T> inline void alist<T>::grow(int num) { num_grow = num; }
-
-/* Private grow list function. Used to insure that
- *   at least one more "slot" is available.
- */
-template <typename T> void alist<T>::GrowList()
-{
-  if (items == NULL) {
-    if (num_grow == 0) { num_grow = 1; /* default if not initialized */ }
-    items = (T*)malloc(num_grow * sizeof(T));
-    max_items = num_grow;
-  } else if (num_items == max_items) {
-    max_items += num_grow;
-    items = (T*)realloc(items, max_items * sizeof(T));
-  }
-}
-
-template <typename T> T alist<T>::first()
-{
-  cur_item = 1;
-  if (num_items == 0) {
-    return NULL;
-  } else {
-    return items[0];
-  }
-}
-
-template <typename T> T alist<T>::last()
-{
-  if (num_items == 0) {
-    return NULL;
-  } else {
-    cur_item = num_items;
-    return items[num_items - 1];
-  }
-}
-
-template <typename T> T alist<T>::next()
-{
-  if (cur_item >= num_items) {
-    return NULL;
-  } else {
-    return items[cur_item++];
-  }
-}
-
-template <typename T> T alist<T>::prev()
-{
-  if (cur_item <= 1) {
-    return NULL;
-  } else {
-    return items[--cur_item];
-  }
-}
-
-// prepend an item to the list -- i.e. add to beginning
-template <typename T> void alist<T>::prepend(T item)
-{
-  GrowList();
-  if (num_items == 0) {
-    items[num_items++] = item;
-    return;
-  }
-  for (int i = num_items; i > 0; i--) { items[i] = items[i - 1]; }
-  items[0] = item;
-  num_items++;
-}
-
-
-// Append an item to the list
-template <typename T> void alist<T>::append(T item)
-{
-  GrowList();
-  items[num_items++] = item;
-}
-
-template <typename T>
-/* Remove an item from the list */
-T alist<T>::remove(int index)
-{
-  T item;
-  if (index < 0 || index >= num_items) { return NULL; }
-  item = items[index];
-  num_items--;
-  for (int i = index; i < num_items; i++) { items[i] = items[i + 1]; }
-  return item;
-}
-
-
-/* Get the index item -- we should probably allow real indexing here */
-template <typename T> T alist<T>::get(int index)
-{
-  if (index < 0 || index >= num_items) { return NULL; }
-  return items[index];
-}
-
-/* Destroy the list and its contents */
-template <typename T> void alist<T>::destroy()
-{
-  if (items) {
-    if (own_items) {
-      for (int i = 0; i < num_items; i++) {
-        free((void*)(items[i]));
-        items[i] = NULL;
-      }
-    }
-    free(items);
-    items = NULL;
-  }
-}
 
 #endif  // BAREOS_LIB_ALIST_H_
