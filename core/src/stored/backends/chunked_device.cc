a0c7e940a40a20cfda8ace02df53d2648f5d00c3
Andreas Rogge <andreas.rogge@bareos.com>
2021-08-18 12:16:28 +0200

    stored: improve chunked device debug messages

--- core/src/stored/backends/chunked_device.cc
+++ core/src/stored/backends/chunked_device.cc
@@ -703,7 +703,7 @@ ssize_t ChunkedDevice::ReadChunked(int fd, void* buffer, size_t count)
 
       bytes_left
           = MIN((ssize_t)count, (current_chunk_->buflen - wanted_offset));
-      Dmsg2(200, "Reading %d bytes at offset %d from chunk buffer\n",
+      Dmsg2(200, "Reading complete %d byte read-request from chunk offset %d\n",
             bytes_left, wanted_offset);
 
       if (bytes_left < 0) {
@@ -735,8 +735,10 @@ ssize_t ChunkedDevice::ReadChunked(int fd, void* buffer, size_t count)
                            (ssize_t)(current_chunk_->buflen - wanted_offset));
 
           if (bytes_left > 0) {
-            Dmsg2(200, "Reading %d bytes at offset %d from chunk buffer\n",
-                  bytes_left, wanted_offset);
+            Dmsg2(200,
+                  "Reading %d bytes of %d byte read-request from end of chunk "
+                  "at offset %d\n",
+                  bytes_left, count, wanted_offset);
 
             memcpy(((char*)buffer + offset),
                    current_chunk_->buffer + wanted_offset, bytes_left);
@@ -771,8 +773,9 @@ ssize_t ChunkedDevice::ReadChunked(int fd, void* buffer, size_t count)
                            (ssize_t)(current_chunk_->buflen));
 
           if (bytes_left > 0) {
-            Dmsg2(200, "Reading %d bytes at offset %d from chunk buffer\n",
-                  bytes_left, 0);
+            Dmsg2(200,
+                  "Reading %d bytes of %d byte read-request from next chunk\n",
+                  bytes_left, count);
 
             memcpy(((char*)buffer + offset), current_chunk_->buffer,
                    bytes_left);
@@ -832,8 +835,8 @@ ssize_t ChunkedDevice::WriteChunked(int fd, const void* buffer, size_t count)
         && current_chunk_->end_offset >= (boffset_t)((offset_ + count) - 1)) {
       wanted_offset = (offset_ % current_chunk_->chunk_size);
 
-      Dmsg2(200, "Writing %d bytes at offset %d in chunk buffer\n", count,
-            wanted_offset);
+      Dmsg2(200, "Writing complete %d byte write-request to chunk offset %d\n",
+            count, wanted_offset);
 
       memcpy(current_chunk_->buffer + wanted_offset, buffer, count);
 
@@ -864,8 +867,10 @@ ssize_t ChunkedDevice::WriteChunked(int fd, const void* buffer, size_t count)
                               + 1));
 
           if (bytes_left > 0) {
-            Dmsg2(200, "Writing %d bytes at offset %d in chunk buffer\n",
-                  bytes_left, wanted_offset);
+            Dmsg2(200,
+                  "Writing %d bytes of %d byte write-request to end of chunk "
+                  "at offset %d\n",
+                  bytes_left, count, wanted_offset);
 
             memcpy(current_chunk_->buffer + wanted_offset,
                    ((char*)buffer + offset), bytes_left);
@@ -894,8 +899,9 @@ ssize_t ChunkedDevice::WriteChunked(int fd, const void* buffer, size_t count)
                                     - current_chunk_->start_offset)
                                    + 1));
         if (bytes_left > 0) {
-          Dmsg2(200, "Writing %d bytes at offset %d in chunk buffer\n",
-                bytes_left, 0);
+          Dmsg2(200,
+                "Writing %d bytes of %d byte write-request to next chunk\n",
+                bytes_left, count);
 
           memcpy(current_chunk_->buffer, ((char*)buffer + offset), bytes_left);
           current_chunk_->buflen = bytes_left;
