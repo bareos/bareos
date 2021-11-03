f5f9f3f1080419b684b8d140e18432702cad7333
Alaa Eddine Elamri <alaeddine.elamri@bareos.com>
2021-11-03 11:10:25 +0100

    bench: created custom target for benchmarks
    it runs all the benchmarks available and creates a json file containing the results for each
    apply changes from bareos-check-sources

--- core/cmake/BareosFindAllLibraries.cmake
+++ core/cmake/BareosFindAllLibraries.cmake
@@ -189,7 +189,6 @@ bareosfindlibrary("dl")
 bareosfindlibrary("acl")
 if(NOT ${CMAKE_CXX_COMPILER_ID} MATCHES SunPro)
   find_package(GTest 1.8 CONFIG)
-  find_package(benchmark 1.5 CONFIG)
 endif()
 
 if(${HAVE_CAP})
