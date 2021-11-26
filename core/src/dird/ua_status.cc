39ac44db8cbfee3766dd163ce4cf1032d034129f
Philipp Storz <philipp.storz@bareos.com>
2021-11-26 16:00:50 +0100

    ua_stauts: update copyright date

--- core/src/dird/ua_status.cc
+++ core/src/dird/ua_status.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2001-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2019 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2021 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
