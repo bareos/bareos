72ca475b8f79795e26b07089993c25baf75c069a
Joerg Steffens <joerg.steffens@bareos.com>
2021-12-20 19:04:25 +0100

    contrib: renamed bareos_option_example
    Renamed options-plugin-sample to bareos_option_example to satisfy PEP8.
    Adapted for Bareos >= 20.

---
+++ contrib/fd-plugins/bareos_option_example/BareosFdPluginFileInteract.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+# Bareos python class as stub for option plugin
+# handle_backup_file gets called for each file in the backup
+# 
+# (c) Bareos GmbH & Co. KG
+# AGPL v.3
+# Author: Maik Aussendorf
+
+import bareosfd
+from BareosFdPluginBaseclass import BareosFdPluginBaseclass
+
+class BareosFdPluginFileInteract(BareosFdPluginBaseclass):
+
+    def __init__(self, plugindef):
+        super(BareosFdPluginFileInteract, self).__init__(plugindef)
+        bareosfd.RegisterEvents([bareosfd.bEventHandleBackupFile])
+
+    def handle_backup_file(self, savepkt):
+        bareosfd.DebugMessage(100, "handle_backup_file called with " + str(savepkt) + "\n");
+        bareosfd.DebugMessage(100, "fname: " + savepkt.fname + " Type: " + str(savepkt.type) + "\n");
+        if (savepkt.type == bFileType['FT_REG']):
+            bareosfd.DebugMessage(100, "regular file, do something now...\n");
+            # Add your stuff here.
+
+        return bareosfd.bRC_OK
--- contrib/fd-plugins/bareos_option_example/__init__.py
+++ contrib/fd-plugins/bareos_option_example/__init__.py
@@ -4,23 +4,22 @@
 # The plugin argument 'filename' is used to read all files listed in that file and add it to the fileset
 
 # Provided by the Bareos FD Python plugin interface
-from bareosfd import *
-from bareos_fd_consts import *
+import bareosfd
 
 # This module contains the wrapper functions called by the Bareos-FD, the functions call the corresponding
 # methods from your plugin class
-from BareosFdWrapper import *
+import BareosFdWrapper
 
 # This module contains the used plugin class
-from BareosFdPluginFileInteract import *
+from BareosFdPluginFileInteract import BareosFdPluginFileInteract
 
-def load_bareos_plugin(context, plugindef):
+def load_bareos_plugin(plugindef):
     '''
     This function is called by the Bareos-FD to load the plugin
     We use it to intantiate the plugin class
     '''
     # BareosFdWrapper.bareos_fd_plugin_object is the module attribute that holds the plugin class object
-    BareosFdWrapper.bareos_fd_plugin_object = BareosFdPluginFileInteract (context, plugindef);
-    return bRCs['bRC_OK'];
+    BareosFdWrapper.bareos_fd_plugin_object = BareosFdPluginFileInteract(plugindef);
+    return bareosfd.bRC_OK
 
 # the rest is done in the Plugin module
