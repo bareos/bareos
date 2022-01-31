a85f7f2dcb0bfbc95223caa66d018b33a43cefac
Andreas Rogge <andreas.rogge@bareos.com>
2022-01-31 13:54:24 +0100

    lib: fix off-by-one error in edit_pthread()

--- core/src/lib/edit.cc
+++ core/src/lib/edit.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2002-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2022 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -340,7 +340,7 @@ char* edit_pthread(pthread_t val, char* buf, int buf_len)
 
   bstrncpy(buf, "0x", buf_len);
   for (i = sizeof(val); i; --i) {
-    Bsnprintf(mybuf, sizeof(mybuf), "%02x", (unsigned)(ptc[i]));
+    Bsnprintf(mybuf, sizeof(mybuf), "%02x", (unsigned)(ptc[i - 1]));
     bstrncat(buf, mybuf, buf_len);
   }
 
