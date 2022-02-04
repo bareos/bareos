8a5c629dfef85c90f4651434d343fbc23647e935
Frank Bergkemper <frank.bergkemper@bareos.com>
2022-02-04 12:36:34 +0100

    webui: add timelineAction to client controller
    (cherry picked from commit 36cd37a0e9cc9f3a8e913f779a122208723a2bf4)

--- webui/module/Client/src/Client/Controller/ClientController.php
+++ webui/module/Client/src/Client/Controller/ClientController.php
@@ -5,7 +5,7 @@
  * bareos-webui - Bareos Web-Frontend
  *
  * @link      https://github.com/bareos/bareos for the canonical source repository
- * @copyright Copyright (c) 2013-2020 Bareos GmbH & Co. KG (http://www.bareos.org/)
+ * @copyright Copyright (c) 2013-2022 Bareos GmbH & Co. KG (http://www.bareos.org/)
  * @license   GNU Affero General Public License (http://www.gnu.org/licenses/)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -256,6 +256,44 @@ class ClientController extends AbstractActionController
         'result' => $result
       )
     );
+  }
+
+  public function timelineAction()
+  {
+    $this->RequestURIPlugin()->setRequestURI();
+
+    if(!$this->SessionTimeoutPlugin()->isValid()) {
+      return $this->redirect()->toRoute(
+        'auth',
+        array(
+          'action' => 'login'
+        ),
+        array(
+          'query' => array(
+            'req' => $this->RequestURIPlugin()->getRequestURI(),
+            'dird' => $_SESSION['bareos']['director']
+          )
+        )
+      );
+    }
+
+    $module_config = $this->getServiceLocator()->get('ModuleManager')->getModule('Application')->getConfig();
+    $invalid_commands = $this->CommandACLPlugin()->getInvalidCommands(
+      $module_config['console_commands']['Job']['mandatory']
+    );
+    if(count($invalid_commands) > 0) {
+      $this->acl_alert = true;
+      return new ViewModel(
+        array(
+          'acl_alert' => $this->acl_alert,
+          'invalid_commands' => implode(",", $invalid_commands)
+        )
+      );
+    }
+
+    $this->bsock = $this->getServiceLocator()->get('director');
+
+    return new ViewModel();
   }
 
   public function getDataAction()
