45704dddd748b8e337335f2dcceb0821a6440573
Alaa Eddine Elamri <alaeddine.elamri@bareos.com>
2022-03-18 12:19:27 +0100

    dird: refactored multicolumn prompts and added new tests

--- core/src/tests/multicolumn_prompts.cc
+++ core/src/tests/multicolumn_prompts.cc
@@ -41,11 +41,70 @@ class MulticolumPrompts : public ::testing::Test {
 
   void TearDown() override { FreeUaContext(ua); }
 
+  void PopulateUaWithPrompts(UaContext* ua, const char** list)
+  {
+    StartPrompt(ua, "start");
+    for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
+  }
+
+  int window_width{80};
+  int lines_threshold{20};
   JobControlRecord jcr{};
   UaContext* ua{};
 };
 
-TEST_F(MulticolumPrompts, FormatForAnyNumberOfLinesAndStandardWidth)
+TEST_F(MulticolumPrompts, ReturnsNothingOnAnEmptyList)
+{
+  const char* list[] = {nullptr};
+  PopulateUaWithPrompts(ua, list);
+
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
+  EXPECT_STREQ(
+      output.c_str(),
+      "");
+  /* clang-format on */
+}
+
+TEST_F(MulticolumPrompts, ReturnsSingleElementWhenOnlyOnePromptIsAvailable)
+{
+  const char* list[] = {_("bareos1"), nullptr};
+  PopulateUaWithPrompts(ua, list);
+
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
+  EXPECT_STREQ(
+      output.c_str(),
+      "1: bareos1\n");
+  /* clang-format on */
+}
+
+TEST_F(MulticolumPrompts, Formatting10Elements_StandardWidthNoThreshold)
+{
+  const char* list[] = {_("bareos1"), _("bareos2"),  _("bareos3"), _("bareos4"),
+                        _("bareos5"), _("bareos6"),  _("bareos7"), _("bareos8"),
+                        _("bareos9"), _("bareos10"), nullptr};
+
+  PopulateUaWithPrompts(ua, list);
+
+  lines_threshold = 0;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
+  EXPECT_STREQ(
+      output.c_str(),
+      " 1: bareos1   3: bareos3   5: bareos5   7: bareos7   9: bareos9  \n"
+      " 2: bareos2   4: bareos4   6: bareos6   8: bareos8  10: bareos10 \n"
+     );
+  /* clang-format on */
+}
+
+TEST_F(MulticolumPrompts, Formatting15Elements_StandardWidthNoThreshold)
 {
   const char* list[] = {
       _("bareos1"),  _("bareos2"),  _("bareos3"),  _("bareos4"),  _("bareos5"),
@@ -54,58 +113,87 @@ TEST_F(MulticolumPrompts, FormatForAnyNumberOfLinesAndStandardWidth)
 
       nullptr};
 
-  StartPrompt(ua, "start");
-  for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
+  PopulateUaWithPrompts(ua, list);
 
-  std::string output = FormatMulticolumnPrompts(ua, 80, 1);
-
+  lines_threshold = 0;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+  /* clang-format off */
   EXPECT_STREQ(
       output.c_str(),
       " 1: bareos1   4: bareos4   7: bareos7  10: bareos10 13: bareos13 \n"
       " 2: bareos2   5: bareos5   8: bareos8  11: bareos11 14: bareos14 \n"
-      " 3: bareos3   6: bareos6   9: bareos9  12: bareos12 15: bareos15 ");
+      " 3: bareos3   6: bareos6   9: bareos9  12: bareos12 15: bareos15 \n");
+  /* clang-format on */
 }
 
-TEST_F(MulticolumPrompts, FormatForMoreThan10LinesAndStandardWidth)
+TEST_F(MulticolumPrompts, Formatting16Elements_StandardWidthNoThreshold)
 {
-  const char* list[]
-      = {_("List last 20 Jobs run"),
-         _("List Jobs where a given File is saved"),
-         _("Enter list of comma separated JobIds to select"),
-         _("Enter SQL list command"),
-         _("Select the most recent backup for a client"),
-         _("Select backup for a client before a specified time"),
-         _("Enter a list of files to restore"),
-         _("Enter a list of files to restore before a specified time"),
-         _("Find the JobIds of the most recent backup for a client"),
-         _("Find the JobIds for a backup for a client before a specified time"),
-         _("Enter a list of directories to restore for found JobIds"),
-         _("Select full restore to a specified Job date"),
-         _("Cancel"),
-         nullptr};
-  StartPrompt(ua, "start");
-  for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
+  const char* list[] = {
+      _("bareos1"),  _("bareos2"),  _("bareos3"),  _("bareos4"),  _("bareos5"),
+      _("bareos6"),  _("bareos7"),  _("bareos8"),  _("bareos9"),  _("bareos10"),
+      _("bareos11"), _("bareos12"), _("bareos13"), _("bareos14"), _("bareos15"),
+      _("bareos16"), nullptr};
 
-  std::string output = FormatMulticolumnPrompts(ua, 80, 20);
+  PopulateUaWithPrompts(ua, list);
 
+  lines_threshold = 0;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
   EXPECT_STREQ(
       output.c_str(),
-      " 1: List last 20 Jobs run\n"
-      " 2: List Jobs where a given File is saved\n"
-      " 3: Enter list of comma separated JobIds to select\n"
-      " 4: Enter SQL list command\n"
-      " 5: Select the most recent backup for a client\n"
-      " 6: Select backup for a client before a specified time\n"
-      " 7: Enter a list of files to restore\n"
-      " 8: Enter a list of files to restore before a specified time\n"
-      " 9: Find the JobIds of the most recent backup for a client\n"
-      "10: Find the JobIds for a backup for a client before a specified time\n"
-      "11: Enter a list of directories to restore for found JobIds\n"
-      "12: Select full restore to a specified Job date\n"
-      "13: Cancel\n");
+      " 1: bareos1   4: bareos4   7: bareos7  10: bareos10 13: bareos13 16: bareos16 \n"
+      " 2: bareos2   5: bareos5   8: bareos8  11: bareos11 14: bareos14 \n"
+      " 3: bareos3   6: bareos6   9: bareos9  12: bareos12 15: bareos15 \n");
+  /* clang-format on */
 }
 
-TEST_F(MulticolumPrompts, FormatForAnyNumberOfLinesAndHugeWidth)
+TEST_F(MulticolumPrompts, Formatting21Elements_StandardWidthNoThreshold)
+{
+  const char* list[] = {
+      _("bareos1"),  _("bareos2"),  _("bareos3"),  _("bareos4"),  _("bareos5"),
+      _("bareos6"),  _("bareos7"),  _("bareos8"),  _("bareos9"),  _("bareos10"),
+      _("bareos11"), _("bareos12"), _("bareos13"), _("bareos14"), _("bareos15"),
+      _("bareos16"), _("bareos17"), _("bareos18"), _("bareos19"), _("bareos20"),
+      _("bareos21"), nullptr};
+
+  PopulateUaWithPrompts(ua, list);
+
+  lines_threshold = 0;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
+  EXPECT_STREQ(
+      output.c_str(),
+      " 1: bareos1   5: bareos5   9: bareos9  13: bareos13 17: bareos17 21: bareos21 \n"
+      " 2: bareos2   6: bareos6  10: bareos10 14: bareos14 18: bareos18 \n"
+      " 3: bareos3   7: bareos7  11: bareos11 15: bareos15 19: bareos19 \n"
+      " 4: bareos4   8: bareos8  12: bareos12 16: bareos16 20: bareos20 \n");
+  /* clang-format on */
+}
+
+TEST_F(MulticolumPrompts,
+       NoMulticolumnformattingWhenNumberOfElementsLessThanThreshold)
+{
+  const char* list[] = {_("List last 20 Jobs run"), _("Cancel"), nullptr};
+
+  PopulateUaWithPrompts(ua, list);
+
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
+  EXPECT_STREQ(
+      output.c_str(),
+      "1: List last 20 Jobs run\n"
+      "2: Cancel\n");
+  /* clang-format on */
+}
+
+TEST_F(MulticolumPrompts, FormatsForVeryLargeWidth)
 {
   const char* list[]
       = {_("List last 20 Jobs run"),
@@ -122,11 +210,15 @@ TEST_F(MulticolumPrompts, FormatForAnyNumberOfLinesAndHugeWidth)
          _("Select full restore to a specified Job date"),
          _("Cancel"),
          nullptr};
-  StartPrompt(ua, "start");
-  for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
 
-  std::string output = FormatMulticolumnPrompts(ua, 5000, 1);
+  PopulateUaWithPrompts(ua, list);
 
+  window_width = 5000;
+  lines_threshold = 10;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
   EXPECT_STREQ(
       output.c_str(),
       " 1: List last 20 Jobs run                                             "
@@ -141,10 +233,11 @@ TEST_F(MulticolumPrompts, FormatForAnyNumberOfLinesAndHugeWidth)
       "10: Find the JobIds for a backup for a client before a specified time "
       "11: Enter a list of directories to restore for found JobIds           "
       "12: Select full restore to a specified Job date                       "
-      "13: Cancel\n");
+      "13: Cancel                                                            \n");
+  /* clang-format on */
 }
 
-TEST_F(MulticolumPrompts, FormatForMoreThan10LinesAndSmallerThanStandardWidth)
+TEST_F(MulticolumPrompts, Format15Elements_SmallWidth10LineThreshold)
 {
   const char* list[] = {
       _("bareos1"),  _("bareos2"),  _("bareos3"),  _("bareos4"),  _("bareos5"),
@@ -153,44 +246,83 @@ TEST_F(MulticolumPrompts, FormatForMoreThan10LinesAndSmallerThanStandardWidth)
 
       nullptr};
 
-  StartPrompt(ua, "start");
-  for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
+  PopulateUaWithPrompts(ua, list);
 
-  std::string output = FormatMulticolumnPrompts(ua, 60, 10);
+  window_width = 60;
+  lines_threshold = 10;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
 
+  /* clang-format off */
   EXPECT_STREQ(output.c_str(),
-               " 1: bareos1   5: bareos5   9: bareos9  13: bareos13\n"
-               " 2: bareos2   6: bareos6  10: bareos10 14: bareos14\n"
-               " 3: bareos3   7: bareos7  11: bareos11 15: bareos15\n"
-               " 4: bareos4   8: bareos8  12: bareos12\n");
+               " 1: bareos1   5: bareos5   9: bareos9  13: bareos13 \n"
+               " 2: bareos2   6: bareos6  10: bareos10 14: bareos14 \n"
+               " 3: bareos3   7: bareos7  11: bareos11 15: bareos15 \n"
+               " 4: bareos4   8: bareos8  12: bareos12 \n");
+  /* clang-format on */
 }
 
-TEST_F(MulticolumPrompts, FormatPromptsContainingSpacesAndRegularPrompts)
+TEST_F(MulticolumPrompts, Formatting_NoWidth)
+{
+  const char* list[]
+      = {_("bareos1"), _("bareos2"),  _("bareos3"),  _("bareos4"),
+         _("bareos5"), _("bareos6"),  _("bareos7"),  _("bareos8"),
+         _("bareos9"), _("bareos10"), _("bareos11"), nullptr};
+
+  PopulateUaWithPrompts(ua, list);
+
+  window_width = 0;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
+
+  /* clang-format off */
+  EXPECT_STREQ(output.c_str(),
+               " 1: bareos1\n"
+               " 2: bareos2\n"
+               " 3: bareos3\n"
+               " 4: bareos4\n"
+               " 5: bareos5\n"
+               " 6: bareos6\n"
+               " 7: bareos7\n"
+               " 8: bareos8\n"
+               " 9: bareos9\n"
+               "10: bareos10\n"
+               "11: bareos11\n");
+  /* clang-format on */
+}
+
+TEST_F(MulticolumPrompts,
+       FormatPromptsContainingSpacesAndRegularPrompts_StandartWidthNoThreshold)
 {
   const char* list[] = {_(""), _("Listsaved"), _("Cancel"), nullptr};
 
-  StartPrompt(ua, "start");
-  for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
+  PopulateUaWithPrompts(ua, list);
 
-  std::string output = FormatMulticolumnPrompts(ua, 80, 1);
+  lines_threshold = 0;
+  std::string output
+      = FormatMulticolumnPrompts(ua, window_width, lines_threshold);
 
+  /* clang-format off */
   EXPECT_STREQ(output.c_str(),
                "1:           "
                "2: Listsaved "
-               "3: Cancel\n");
+               "3: Cancel    \n");
+  /* clang-format on */
 }
 
-TEST_F(MulticolumPrompts, FormatPromptsContainingSpaces)
+TEST_F(MulticolumPrompts,
+       FormatPromptsContainingOnlySpacesPrompts_StandartWidthNoThreshold)
 {
   const char* list[] = {_(""), _(" "), _("  "), nullptr};
 
-  StartPrompt(ua, "start");
-  for (int i = 0; list[i]; ++i) { AddPrompt(ua, list[i]); }
+  PopulateUaWithPrompts(ua, list);
 
   std::string output = FormatMulticolumnPrompts(ua, 80, 20);
 
+  /* clang-format off */
   EXPECT_STREQ(output.c_str(),
                "1: \n"
                "2:  \n"
                "3:   \n");
+  /* clang-format on */
 }
