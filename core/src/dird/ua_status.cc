247e1102c75555717c03ad92c55eadf030a0ae4f
Philipp Storz <philipp.storz@bareos.com>
2022-03-24 14:10:20 +0100

    ua_status.cc: remove redundant comments
    (cherry picked from commit 94b4cd7bcc8c0c157f7ad99c3207693c780ab809)

--- core/src/dird/ua_status.cc
+++ core/src/dird/ua_status.cc
@@ -757,13 +757,10 @@ static void DoDirectorStatus(UaContext* ua)
 {
   ListDirStatusHeader(ua);
 
-  // List scheduled Jobs
   ListScheduledJobs(ua);
 
-  // List running jobs
   ListRunningJobs(ua);
 
-  // List terminated jobs
   ListTerminatedJobs(ua);
 
   ListConnectedClients(ua);
