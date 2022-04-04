b5c2ad55095da0483c87ed134a625fbc4e9cb0bd
Philipp Storz <philipp.storz@bareos.com>
2022-04-04 16:30:20 +0200

    config: make ResHeadContainer a class

--- core/src/lib/parse_conf.h
+++ core/src/lib/parse_conf.h
@@ -419,7 +419,8 @@ class ConfigurationParser {
   void SetResourceDefaultsParserPass2(ResourceItem* item);
 };
 
-struct ResHeadContainer {
+class ResHeadContainer {
+ public:
   BareosResource** res_head_ = nullptr;
   ConfigurationParser* config_ = nullptr;
 
