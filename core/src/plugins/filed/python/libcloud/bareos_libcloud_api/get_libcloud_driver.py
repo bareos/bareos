ae36762ef56ac24c8e0f9a42586029f1cb3554c4
Matthias Kneer <mkneer@plusline.net>
2022-04-27 13:44:36 +0200

    libcloud plugin: libcloud storage provider config
    Right now S3 is hardcoded within the provider type
    for the apache libcloud plugin. Any other value, in
    the config_file parameter for "provider" will not
    be respected.
    
    With this change the option will be actually used.

--- core/src/plugins/filed/python/libcloud/bareos_libcloud_api/get_libcloud_driver.py
+++ core/src/plugins/filed/python/libcloud/bareos_libcloud_api/get_libcloud_driver.py
@@ -34,6 +34,11 @@ options = {
 def get_driver(options):
     driver_opt = dict(options)
 
+    try:
+        provider_opt = driver_opt.get("provider")
+    except KeyError:
+        provider_opt = "S3"
+
     # remove unknown options
     for opt in (
         "buckets_exclude",
@@ -50,7 +55,7 @@ def get_driver(options):
 
     driver = None
 
-    provider = getattr(libcloud.storage.types.Provider, "S3")
+    provider = getattr(libcloud.storage.types.Provider, provider_opt)
     driver = libcloud.storage.providers.get_driver(provider)(**driver_opt)
 
     try:
