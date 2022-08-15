11653173ab7dfedc11ee518988eb94dc9a6afa0f
Bruno Friedmann <bruno.friedmann@bareos.com>
2022-08-15 12:10:03 +0000

    Update copyright year
    Signed-off-by: Bruno Friedmann <bruno.friedmann@bareos.com>

--- systemtests/python-modules/bareos_unittest/json.py
+++ systemtests/python-modules/bareos_unittest/json.py
@@ -2,7 +2,7 @@
 #
 #   BAREOS - Backup Archiving REcovery Open Sourced
 #
-#   Copyright (C) 2019-2021 Bareos GmbH & Co. KG
+#   Copyright (C) 2019-2022 Bareos GmbH & Co. KG
 #
 #   This program is Free Software; you can redistribute it and/or
 #   modify it under the terms of version three of the GNU Affero General Public
