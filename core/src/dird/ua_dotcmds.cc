b83bab1b52508111ea11aa02dd9708fc4cd9a183
Andreas Rogge <andreas.rogge@bareos.com>
2022-10-17 08:43:38 +0200

    dird: avoid crash in .jobstatus
    Previously if .jobstatus was called before another command in the same
    UA had already opened the catalog, it would simply crash.
    This patch reorders OpenClientDB() before the FillQuery() to avoid the
    crash.
    
    (cherry picked from commit b501728558c245a930eb6cb41fe1d008786f1b00)

--- core/src/dird/ua_dotcmds.cc
+++ core/src/dird/ua_dotcmds.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2002-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2022 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -937,10 +937,10 @@ bool DotJobstatusCmd(UaContext* ua, const char* cmd)
     }
   }
 
+  if (!OpenClientDb(ua)) { return false; }
+
   ua->db->FillQuery(select, BareosDb::SQL_QUERY::get_jobstatus_details,
                     where.c_str());
-
-  if (!OpenClientDb(ua)) { return false; }
 
   ua->send->ArrayStart("jobstatus");
   retval = ua->db->ListSqlQuery(ua->jcr, select.c_str(), ua->send, HORZ_LIST,
