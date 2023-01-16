9d9fcf4c23e2d72926e86af8c40f1ec4519d1018
Andreas Rogge <andreas.rogge@bareos.com>
2023-01-16 17:17:48 +0100

    stored: add new backend autochanger_test_device
    Duplicate the file backend and pretend it is a tape. This is meant for
    testing purposes only and will allow developers to run
    autochanger-related tests without the need for an autochanger or a full
    emulator (like mhvtl).

---
+++ core/src/stored/backends/autochanger_test_device.cc
@@ -0,0 +1,30 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2023-2023 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include "include/bareos.h"
+#include "stored/sd_backends.h"
+#include "autochanger_test_device.h"
+
+namespace storagedaemon {
+
+REGISTER_SD_BACKEND(autochanger_test, autochanger_test_device);
+
+} /* namespace storagedaemon  */
