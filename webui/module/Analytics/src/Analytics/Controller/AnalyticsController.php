4fd9c9123fdc7bd132e2c97b47f8618b24894d20
Frank Bergkemper <frank.bergkemper@bareos.com>
2023-01-19 10:42:58 +0100

    [WIP] introduce tree charts of configuration resources

--- webui/module/Analytics/src/Analytics/Controller/AnalyticsController.php
+++ webui/module/Analytics/src/Analytics/Controller/AnalyticsController.php
@@ -5,7 +5,7 @@
  * bareos-webui - Bareos Web-Frontend
  *
  * @link      https://github.com/bareos/bareos for the canonical source repository
- * @copyright Copyright (c) 2013-2022 Bareos GmbH & Co. KG (http://www.bareos.org/)
+ * @copyright Copyright (c) 2013-2023 Bareos GmbH & Co. KG (http://www.bareos.org/)
  * @license   GNU Affero General Public License (http://www.gnu.org/licenses/)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -35,6 +35,43 @@ class AnalyticsController extends AbstractActionController
   protected $analyticsModel = null;
 
   public function indexAction()
+  {
+    $this->RequestURIPlugin()->setRequestURI();
+
+    if(!$this->SessionTimeoutPlugin()->isValid()) {
+      return $this->redirect()->toRoute(
+        'auth',
+        array(
+          'action' => 'login'
+        ),
+        array(
+          'query' => array(
+            'req' => $this->RequestURIPlugin()->getRequestURI(),
+            'dird' => $_SESSION['bareos']['director']
+          )
+        )
+      );
+    }
+
+    $module_config = $this->getServiceLocator()->get('ModuleManager')->getModule('Application')->getConfig();
+    $invalid_commands = $this->CommandACLPlugin()->getInvalidCommands(
+      $module_config['console_commands']['Analytics']['mandatory']
+    );
+
+    if(count($invalid_commands) > 0) {
+      $this->acl_alert = true;
+      return new ViewModel(
+        array(
+          'acl_alert' => $this->acl_alert,
+          'invalid_commands' => implode(",", $invalid_commands)
+        )
+      );
+    }
+
+    return new ViewModel();
+  }
+
+  public function configurationAction()
   {
     $this->RequestURIPlugin()->setRequestURI();
 
@@ -108,6 +145,16 @@ class AnalyticsController extends AbstractActionController
       try {
         $this->bsock = $this->getServiceLocator()->get('director');
         $result = $this->getAnalyticsModel()->getOverallJobTotals($this->bsock);
+        $this->bsock->disconnect();
+      }
+      catch(Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+    elseif($data == "config-tree-dir-fd-job") {
+      try {
+        $this->bsock = $this->getServiceLocator()->get('director');
+        $result = $this->getAnalyticsModel()->getConfigTree($this->bsock);
         $this->bsock->disconnect();
       }
       catch(Exception $e) {
--- webui/module/Analytics/src/Analytics/Model/AnalyticsModel.php
+++ webui/module/Analytics/src/Analytics/Model/AnalyticsModel.php
@@ -5,7 +5,7 @@
  * bareos-webui - Bareos Web-Frontend
  *
  * @link      https://github.com/bareos/bareos for the canonical source repository
- * @copyright Copyright (c) 2013-2022 Bareos GmbH & Co. KG (http://www.bareos.org/)
+ * @copyright Copyright (c) 2013-2023 Bareos GmbH & Co. KG (http://www.bareos.org/)
  * @license   GNU Affero General Public License (http://www.gnu.org/licenses/)
  *
  * This program is free software: you can redistribute it and/or modify
@@ -49,6 +49,87 @@ class AnalyticsModel
          $jobtotals = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
          $result = $jobtotals['result']['jobtotals'];
          return $result;
+      }
+      else {
+         throw new \Exception('Missing argument.');
+      }
+   }
+
+   public function getConfigTree(&$bsock=null)
+   {
+      if(isset($bsock)) {
+
+         // note: has no result in api 2 on a restricted named console
+         /*
+         $cmd = 'show directors';
+         $result = $bsock->send_command($cmd, 2);
+         $directors = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+         $directors_cache = $directors['result']['directors'];
+         */
+
+         $cmd = 'show clients';
+         $result = $bsock->send_command($cmd, 2);
+         $clients = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+         $clients_cache = $clients['result']['clients'];
+
+         $cmd = 'show jobs';
+         $result = $bsock->send_command($cmd, 2);
+         $jobs = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+         $jobs_cache = $jobs['result']['jobs'];
+
+         $cmd = 'show jobdefs';
+         $result = $bsock->send_command($cmd, 2);
+         $jobdefs = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+         $jobdefs_cache = $jobdefs['result']['jobdefs'];
+
+         /*
+         $cmd = 'show fileset';
+         $result = $bsock->send_command($cmd, 2);
+         $filesets = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+         $filesets_cache = $filesets['result']['filesets'];
+         */
+
+         $config_tree = array('name' => 'DIR', 'children' => array());
+
+         foreach ($clients_cache as $client_key=>$client) {
+
+            $client_children = array();
+
+            foreach ($jobs_cache as $job_key=>$job) {
+
+               $job_children = array();
+
+               if (isset($job["client"]) && $job["client"] == $client_key) {
+                  if (isset($job["fileset"])) {
+                     array_push($job_children, array('name' => $job['fileset']));
+                  } else {
+                     foreach ($jobdefs_cache as $jobdefs_key=>$jobdefs) {
+                        if ($job["jobdefs"] == $jobdefs_key) {
+                           array_push($job_children, array('name' => $jobdefs['fileset']));
+                        }
+                     }
+                  }
+                  array_push($client_children, array('name' => $job_key, 'children' => $job_children));
+               }
+
+               if (!isset($job['client'])) {
+                  foreach ($jobdefs_cache as $jobdefs_key=>$jobdefs) {
+                     if (isset($job['jobdefs']) && $job['jobdefs'] == $jobdefs_key && $client_key == $jobdefs['client']) {
+                        if (isset($job['fileset'])) {
+                           array_push($job_children, array('name' => $job['fileset']));
+                        } else {
+                           array_push($job_children, array('name' => $jobdefs['fileset']));
+                        }
+                        array_push($client_children, array('name' => $job_key, 'children' => $job_children));
+                     }
+                  }
+               }
+            }
+
+            array_push($config_tree["children"], array("name" => $client_key, "children" => $client_children));
+         }
+
+         return $config_tree;
       }
       else {
          throw new \Exception('Missing argument.');
