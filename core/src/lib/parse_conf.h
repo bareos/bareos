cfadaa9c9aae7bcf77589d6d8a9dc11656010d22
Alaa Eddine Elamri <alaeddine.elamri@bareos.com>
2023-01-20 10:31:07 +0100

    lib: make ResLocker non-copiable and non-moveable

--- core/src/lib/parse_conf.h
+++ core/src/lib/parse_conf.h
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2010 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2022 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -501,6 +501,10 @@ class ResLocker {
     config_res_->b_LockRes(__FILE__, __LINE__);
   }
   ~ResLocker() { config_res_->b_UnlockRes(__FILE__, __LINE__); }
+  ResLocker(const ResLocker&) = delete;
+  ResLocker& operator=(const ResLocker&) = delete;
+  ResLocker(ResLocker&&) = delete;
+  ResLocker& operator=(ResLocker&&) = delete;
 };
 
 #endif  // BAREOS_LIB_PARSE_CONF_H_
