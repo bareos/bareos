bareos/bareos

commit 6c265ec52d1968b0c5f5e573833fe260e48ee30a

replace volatile device pointers with actual variable

core/src/stored/vol_mgr.cc
@@ -368,10 +368,9 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
                                       const char* VolumeName)
 {
   VolumeReservationItem *vol, *nvol;
-  Device* volatile dev = dcr->dev;
 
   if (JobCanceled(dcr->jcr)) { return NULL; }
-  ASSERT(dev != NULL);
+  ASSERT(dcr->dev != NULL);
 
   Dmsg2(debuglevel, "enter reserve_volume=%s drive=%s\n", VolumeName,
         dcr->dev->print_name());
@@ -382,7 +381,7 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
     Mmsg(dcr->jcr->errmsg,
          _("Could not reserve volume \"%s\" for append, because it is read by "
            "another Job.\n"),
-         dev->VolHdr.VolumeName);
+         dcr->dev->VolHdr.VolumeName);
     return NULL;
   }
 
@@ -397,10 +396,10 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
    * First, remove any old volume attached to this device as it is no longer
    * used.
    */
-  if (dev->vol) {
-    vol = dev->vol;
+  if (dcr->dev->vol) {
+    vol = dcr->dev->vol;
     Dmsg4(debuglevel, "Vol attached=%s, newvol=%s volinuse=%d on %s\n",
-          vol->vol_name, VolumeName, vol->IsInUse(), dev->print_name());
+          vol->vol_name, VolumeName, vol->IsInUse(), dcr->dev->print_name());
     /*
      * Make sure we don't remove the current volume we are inserting
      * because it was probably inserted by another job, or it
@@ -422,11 +421,11 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
             vol->vol_name);
 
       // If old Volume is still mounted, must unload it
-      if (bstrcmp(vol->vol_name, dev->VolHdr.VolumeName)) {
+      if (bstrcmp(vol->vol_name, dcr->dev->VolHdr.VolumeName)) {
         Dmsg0(50, "SetUnload\n");
-        dev->SetUnload(); /* have to unload current volume */
+        dcr->dev->SetUnload(); /* have to unload current volume */
       }
-      FreeVolume(dev); /* Release old volume entry */
+      FreeVolume(dcr->dev); /* Release old volume entry */
 
       if (debug_level >= debuglevel) { DebugListVolumes("reserve_vol free"); }
     }
@@ -439,11 +438,12 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
    * See if this is a request for reading a file type device which can be
    * accesses by multiple readers at once without disturbing each other.
    */
-  if (me->filedevice_concurrent_read && !dcr->IsWriting() && dev->IsFile()) {
+  if (me->filedevice_concurrent_read && !dcr->IsWriting()
+      && dcr->dev->IsFile()) {
     nvol->SetJobid(dcr->jcr->JobId);
     nvol->SetReading();
     vol = nvol;
-    dev->vol = vol;
+    dcr->dev->vol = vol;
 
     /*
      * Read volumes on file based devices are not inserted into the write volume
@@ -458,7 +458,7 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
 
   if (vol != nvol) {
     Dmsg2(debuglevel, "Found vol=%s dev-same=%d\n", vol->vol_name,
-          dev == vol->dev);
+          dcr->dev == vol->dev);
 
     /*
      * At this point, a Volume with this name already is in the list,
@@ -473,7 +473,7 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
     FreeVolItem(nvol);
 
     if (vol->dev) {
-      Dmsg2(debuglevel, "dev=%s vol->dev=%s\n", dev->print_name(),
+      Dmsg2(debuglevel, "dev=%s vol->dev=%s\n", dcr->dev->print_name(),
             vol->dev->print_name());
     }
 
@@ -481,35 +481,35 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
      * Check if we are trying to use the Volume on a different drive dev is our
      * device vol->dev is where the Volume we want is
      */
-    if (dev != vol->dev) {
+    if (dcr->dev != vol->dev) {
       // Caller wants to switch Volume to another device
 
       // should be different devices with different names
-      if (bstrcmp(dev->print_name(), vol->dev->print_name())) {
+      if (bstrcmp(dcr->dev->print_name(), vol->dev->print_name())) {
         // names are same
         Dmsg1(100, "device pointers are different but have same name %s\n",
-              dev->print_name());
+              dcr->dev->print_name());
       }
 
       if (!vol->dev->IsBusy() && !vol->IsSwapping()) {
         slot_number_t slot;
 
         Dmsg3(debuglevel, "==== Swap vol=%s from dev=%s to %s\n", VolumeName,
-              vol->dev->print_name(), dev->print_name());
-        FreeVolume(dev); /* free any volume attached to our drive */
-        Dmsg1(50, "SetUnload dev=%s\n", dev->print_name());
-        dev->SetUnload();      /* Unload any volume that is on our drive */
+              vol->dev->print_name(), dcr->dev->print_name());
+        FreeVolume(dcr->dev); /* free any volume attached to our drive */
+        Dmsg1(50, "SetUnload dev=%s\n", dcr->dev->print_name());
+        dcr->dev->SetUnload(); /* Unload any volume that is on our drive */
         dcr->SetDev(vol->dev); /* temp point to other dev */
         slot = GetAutochangerLoadedSlot(dcr); /* get slot on other drive */
-        dcr->SetDev(dev);                     /* restore dev */
+        dcr->SetDev(dcr->dev);                /* restore dev */
         vol->SetSlotNumber(slot);             /* save slot */
         vol->dev->SetUnload();                /* unload the other drive */
         vol->SetSwapping();                   /* swap from other drive */
-        dev->swap_dev = vol->dev;             /* remember to get this vol */
-        dev->SetLoad();                       /* then reload on our drive */
+        dcr->dev->swap_dev = vol->dev;        /* remember to get this vol */
+        dcr->dev->SetLoad();                  /* then reload on our drive */
         vol->dev->vol = NULL; /* remove volume from other drive */
-        vol->dev = dev;       /* point the Volume at our drive */
-        dev->vol = vol;       /* point our drive at the Volume */
+        vol->dev = dcr->dev;  /* point the Volume at our drive */
+        dcr->dev->vol = vol;  /* point our drive at the Volume */
       } else {
         Jmsg7(dcr->jcr, M_WARNING, 0,
               "Need volume from other drive, but swap not possible. "
@@ -517,13 +517,14 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
               "vol=%s from dev=%s to %s\n",
               vol->dev->CanRead(), vol->dev->num_writers,
               vol->dev->NumReserved(), vol->IsSwapping(), VolumeName,
-              vol->dev->print_name(), dev->print_name());
-        if (vol->IsSwapping() && dev->swap_dev) {
+              vol->dev->print_name(), dcr->dev->print_name());
+        if (vol->IsSwapping() && dcr->dev->swap_dev) {
           Dmsg3(debuglevel, "Swap failed vol=%s from=%s to dev=%s\n",
-                vol->vol_name, dev->swap_dev->print_name(), dev->print_name());
+                vol->vol_name, dcr->dev->swap_dev->print_name(),
+                dcr->dev->print_name());
         } else {
           Dmsg3(debuglevel, "Swap failed vol=%s from=%p to dev=%s\n",
-                vol->vol_name, dev->swap_dev, dev->print_name());
+                vol->vol_name, dcr->dev->swap_dev, dcr->dev->print_name());
         }
 
         if (debug_level >= debuglevel) { DebugListVolumes("failed swap"); }
@@ -532,10 +533,10 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
         goto get_out;
       }
     } else {
-      dev->vol = vol;
+      dcr->dev->vol = vol;
     }
   } else {
-    dev->vol = vol; /* point to newly inserted volume */
+    dcr->dev->vol = vol; /* point to newly inserted volume */
   }
 
 get_out:

core/src/stored/ansi_label.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2005-2009 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2022 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -57,7 +57,6 @@ static bool SameLabelNames(char* bareos_name, char* ansi_name);
  */
 int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
 {
-  Device* volatile dev = dcr->dev;
   JobControlRecord* jcr = dcr->jcr;
   char label[80]; /* tape label */
   int status, i;
@@ -69,36 +68,36 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
    * If tape read the following EOF mark, on disk do not read.
    */
   Dmsg0(100, "Read ansi label.\n");
-  if (!dev->IsTape()) { return VOL_OK; }
+  if (!dcr->dev->IsTape()) { return VOL_OK; }
 
-  dev->label_type = B_BAREOS_LABEL; /* assume Bareos label */
+  dcr->dev->label_type = B_BAREOS_LABEL; /* assume Bareos label */
 
   // Read a maximum of 5 records VOL1, HDR1, ... HDR4
   for (i = 0; i < 6; i++) {
     do {
-      status = dev->read(label, sizeof(label));
+      status = dcr->dev->read(label, sizeof(label));
     } while (status == -1 && errno == EINTR);
 
     if (status < 0) {
       BErrNo be;
-      dev->clrerror(-1);
+      dcr->dev->clrerror(-1);
       Dmsg1(100, "Read device got: ERR=%s\n", be.bstrerror());
       Mmsg2(jcr->errmsg, _("Read error on device %s in ANSI label. ERR=%s\n"),
-            dev->archive_device_string, be.bstrerror());
-      Jmsg(jcr, M_ERROR, 0, "%s", dev->errmsg);
-      dev->VolCatInfo.VolCatErrors++;
+            dcr->dev->archive_device_string, be.bstrerror());
+      Jmsg(jcr, M_ERROR, 0, "%s", dcr->dev->errmsg);
+      dcr->dev->VolCatInfo.VolCatErrors++;
       return VOL_IO_ERROR;
     }
 
     if (status == 0) {
-      if (dev->AtEof()) {
-        dev->SetEot(); /* second eof, set eot bit */
+      if (dcr->dev->AtEof()) {
+        dcr->dev->SetEot(); /* second eof, set eot bit */
         Dmsg0(100, "EOM on ANSI label\n");
         Mmsg0(jcr->errmsg,
               _("Insane! End of tape while reading ANSI label.\n"));
         return VOL_LABEL_ERROR; /* at EOM this shouldn't happen */
       } else {
-        dev->SetAteof(); /* set eof state */
+        dcr->dev->SetAteof(); /* set eof state */
       }
     }
 
@@ -107,7 +106,7 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
         if (status == 80) {
           if (bstrncmp("VOL1", label, 4)) {
             ok = true;
-            dev->label_type = B_ANSI_LABEL;
+            dcr->dev->label_type = B_ANSI_LABEL;
             Dmsg0(100, "Got ANSI VOL1 label\n");
           } else {
             // Try EBCDIC
@@ -115,7 +114,7 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
             if (bstrncmp("VOL1", label, 4)) {
               ok = true;
               ;
-              dev->label_type = B_IBM_LABEL;
+              dcr->dev->label_type = B_IBM_LABEL;
               Dmsg0(100, "Found IBM label.\n");
               Dmsg0(100, "Got IBM VOL1 label\n");
             }
@@ -135,26 +134,26 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
             char* p = &label[4];
             char* q;
 
-            FreeVolume(dev);
+            FreeVolume(dcr->dev);
 
             // Store new Volume name
-            q = dev->VolHdr.VolumeName;
+            q = dcr->dev->VolHdr.VolumeName;
             for (int i = 0; *p != ' ' && i < 6; i++) { *q++ = *p++; }
             *q = 0;
             Dmsg0(100, "Call reserve_volume\n");
             //  why is this reserve_volume() needed???? KES
-            reserve_volume(dcr, dev->VolHdr.VolumeName);
-            dev = dcr->dev; /* may have changed in reserve_volume */
+            reserve_volume(dcr, dcr->dev->VolHdr.VolumeName);
+            dcr->dev = dcr->dev; /* may have changed in reserve_volume */
             Dmsg2(100, "Wanted ANSI Vol %s got %6s\n", VolName,
-                  dev->VolHdr.VolumeName);
+                  dcr->dev->VolHdr.VolumeName);
             Mmsg2(jcr->errmsg, _("Wanted ANSI Volume \"%s\" got \"%s\"\n"),
-                  VolName, dev->VolHdr.VolumeName);
+                  VolName, dcr->dev->VolHdr.VolumeName);
             return VOL_NAME_ERROR;
           }
         }
         break;
       case 1:
-        if (dev->label_type == B_IBM_LABEL) {
+        if (dcr->dev->label_type == B_IBM_LABEL) {
           EbcdicToAscii(label, label, sizeof(label));
         }
 
@@ -173,7 +172,7 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
                   &label[4]);
             Mmsg1(jcr->errmsg,
                   _("ANSI/IBM Volume \"%s\" does not belong to Bareos.\n"),
-                  dev->VolHdr.VolumeName);
+                  dcr->dev->VolHdr.VolumeName);
             return VOL_NAME_ERROR; /* Not a Bareos label */
           }
         } else {
@@ -182,14 +181,14 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
                   &label[4]);
             Mmsg1(jcr->errmsg,
                   _("ANSI/IBM Volume \"%s\" does not belong to Bareos.\n"),
-                  dev->VolHdr.VolumeName);
+                  dcr->dev->VolHdr.VolumeName);
             return VOL_NAME_ERROR; /* Not a Bareos label */
           }
         }
         Dmsg0(100, "Got HDR1 label\n");
         break;
       case 2:
-        if (dev->label_type == B_IBM_LABEL) {
+        if (dcr->dev->label_type == B_IBM_LABEL) {
           EbcdicToAscii(label, label, sizeof(label));
         }
 
@@ -207,7 +206,7 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
           return VOL_OK;
         }
 
-        if (dev->label_type == B_IBM_LABEL) {
+        if (dcr->dev->label_type == B_IBM_LABEL) {
           EbcdicToAscii(label, label, sizeof(label));
         }
 

core/src/stored/autochanger.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2002-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2022 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -123,11 +123,10 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
   slot_number_t wanted_slot;
   JobControlRecord* jcr = dcr->jcr;
   drive_number_t drive;
-  Device* volatile dev = dcr->dev;
 
-  if (!dev->AttachedToAutochanger()) {
+  if (!dcr->dev->AttachedToAutochanger()) {
     Dmsg1(100, "Device %s is not attached to an autochanger\n",
-          dev->print_name());
+          dcr->dev->print_name());
     return 0;
   }
 
@@ -138,7 +137,7 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
     return 1; /* nothing to load */
   }
 
-  drive = dev->drive_index;
+  drive = dcr->dev->drive_index;
   wanted_slot = dcr->VolCatInfo.InChanger ? dcr->VolCatInfo.Slot : 0;
   Dmsg3(100, "autoload: slot=%hd InChgr=%d Vol=%s\n", dcr->VolCatInfo.Slot,
         dcr->VolCatInfo.InChanger, dcr->getVolCatName());
@@ -162,31 +161,31 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
   changer = GetPoolMemory(PM_FNAME);
   if (!IsSlotNumberValid(wanted_slot)) {
     // Suppress info when polling
-    if (!dev->poll) {
+    if (!dcr->dev->poll) {
       Jmsg(
           jcr, M_INFO, 0,
           _("No slot defined in catalog (slot=%hd) for Volume \"%s\" on %s.\n"),
-          wanted_slot, dcr->getVolCatName(), dev->print_name());
+          wanted_slot, dcr->getVolCatName(), dcr->dev->print_name());
       Jmsg(jcr, M_INFO, 0,
            _("Cartridge change or \"update slots\" may be required.\n"));
     }
     rtn_stat = 0;
   } else if (!dcr->device_resource->changer_name) {
     // Suppress info when polling
-    if (!dev->poll) {
+    if (!dcr->dev->poll) {
       Jmsg(jcr, M_INFO, 0,
            _("No \"Changer Device\" for %s. Manual load of Volume may be "
              "required.\n"),
-           dev->print_name());
+           dcr->dev->print_name());
     }
     rtn_stat = 0;
   } else if (!dcr->device_resource->changer_command) {
     // Suppress info when polling
-    if (!dev->poll) {
+    if (!dcr->dev->poll) {
       Jmsg(jcr, M_INFO, 0,
            _("No \"Changer Command\" for %s. Manual load of Volume may be "
              "required.\n"),
-           dev->print_name());
+           dcr->dev->print_name());
     }
     rtn_stat = 0;
   } else {
@@ -218,7 +217,7 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
 
       // Load the desired volume.
       Dmsg2(100, "Doing changer load slot %hd %s\n", wanted_slot,
-            dev->print_name());
+            dcr->dev->print_name());
       Jmsg(
           jcr, M_INFO, 0,
           _("3304 Issuing autochanger \"load slot %hd, drive %hd\" command.\n"),
@@ -226,7 +225,7 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
       dcr->VolCatInfo.Slot = wanted_slot; /* slot to be loaded */
       changer = edit_device_codes(
           dcr, changer, dcr->device_resource->changer_command, "load");
-      dev->close(dcr);
+      dcr->dev->close(dcr);
       Dmsg1(200, "Run program=%s\n", changer);
       status = RunProgramFullOutput(changer, timeout, results.addr());
       if (status == 0) {
@@ -236,10 +235,10 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
             wanted_slot, drive);
         Dmsg2(100, "load slot %hd, drive %hd, status is OK.\n", wanted_slot,
               drive);
-        dev->SetSlotNumber(wanted_slot); /* set currently loaded slot */
-        if (dev->vol) {
+        dcr->dev->SetSlotNumber(wanted_slot); /* set currently loaded slot */
+        if (dcr->dev->vol) {
           // We just swapped this Volume so it cannot be swapping any more
-          dev->vol->ClearSwapping();
+          dcr->dev->vol->ClearSwapping();
         }
       } else {
         BErrNo be;
@@ -257,13 +256,13 @@ int AutoloadDevice(DeviceControlRecord* dcr, int writing, BareosSocket* dir)
              _("3992 Bad autochanger \"load slot %hd, drive %hd\": "
                "ERR=%s.\nResults=%s\n"),
              wanted_slot, drive, be.bstrerror(), results.c_str());
-        dev->SetSlotNumber(-1); /* mark unknown */
+        dcr->dev->SetSlotNumber(-1); /* mark unknown */
       }
       Dmsg2(100, "load slot %hd status=%d\n", wanted_slot, status);
       UnlockChanger(dcr);
     } else {
-      status = 0;                      /* we got what we want */
-      dev->SetSlotNumber(wanted_slot); /* set currently loaded slot */
+      status = 0;                           /* we got what we want */
+      dcr->dev->SetSlotNumber(wanted_slot); /* set currently loaded slot */
     }
 
     Dmsg1(100, "After changer, status=%d\n", status);

commit c73b1a1198b5bd40999571876043dc951149d050

stored: fix broken volume swapping
Fix a problem introduced in Bareos 22 with commit
6c265ec52 replace volatile device pointers with actual variable

Due to an oversight in a refactoring, a swap would no longer move the
volume to another device, but switch over to use the device that had the
desired volume loaded.
As that device was not reserved, but was unreserved during acquire, the
storage daemon would eventually crash in this case.

core/src/stored/vol_mgr.cc
@@ -2,7 +2,7 @@
    BAREOS® - Backup Archiving REcovery Open Sourced
 
    Copyright (C) 2000-2013 Free Software Foundation Europe e.V.
-   Copyright (C) 2015-2022 Bareos GmbH & Co. KG
+   Copyright (C) 2015-2023 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -366,26 +366,20 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
     return NULL;
   }
 
-  /*
-   * We lock the reservations system here to ensure when adding a new volume
-   * that no newly scheduled job can reserve it.
-   */
+  /* We lock the reservations system here to ensure when adding a new volume
+   * that no newly scheduled job can reserve it. */
   LockVolumes();
   if (debug_level >= debuglevel) { DebugListVolumes("begin reserve_volume"); }
 
-  /*
-   * First, remove any old volume attached to this device as it is no longer
-   * used.
-   */
+  /* First, remove any old volume attached to this device as it is no longer
+   * used. */
   if (dcr->dev->vol) {
     vol = dcr->dev->vol;
     Dmsg4(debuglevel, "Vol attached=%s, newvol=%s volinuse=%d on %s\n",
           vol->vol_name, VolumeName, vol->IsInUse(), dcr->dev->print_name());
-    /*
-     * Make sure we don't remove the current volume we are inserting
+    /* Make sure we don't remove the current volume we are inserting
      * because it was probably inserted by another job, or it
-     * is not being used and is marked as not reserved.
-     */
+     * is not being used and is marked as not reserved. */
     if (bstrcmp(vol->vol_name, VolumeName)) {
       Dmsg2(debuglevel, "=== set reserved vol=%s dev=%s\n", VolumeName,
             vol->dev->print_name());
@@ -415,10 +409,8 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
   // Create a new Volume entry
   nvol = new_vol_item(dcr, VolumeName);
 
-  /*
-   * See if this is a request for reading a file type device which can be
-   * accesses by multiple readers at once without disturbing each other.
-   */
+  /* See if this is a request for reading a file type device which can be
+   * accesses by multiple readers at once without disturbing each other. */
   if (me->filedevice_concurrent_read && !dcr->IsWriting()
       && dcr->dev->CanReadConcurrently()) {
     nvol->SetJobid(dcr->jcr->JobId);
@@ -426,10 +418,8 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
     vol = nvol;
     dcr->dev->vol = vol;
 
-    /*
-     * Read volumes on file based devices are not inserted into the write volume
-     * list.
-     */
+    /* Read volumes on file based devices are not inserted into the write volume
+     * list. */
     goto get_out;
   } else {
     // Now try to insert the new Volume
@@ -441,11 +431,9 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
     Dmsg2(debuglevel, "Found vol=%s dev-same=%d\n", vol->vol_name,
           dcr->dev == vol->dev);
 
-    /*
-     * At this point, a Volume with this name already is in the list,
+    /* At this point, a Volume with this name already is in the list,
      * so we simply release our new Volume entry. Note, this should
-     * only happen if we are moving the volume from one drive to another.
-     */
+     * only happen if we are moving the volume from one drive to another. */
     Dmsg2(debuglevel, "reserve_vol free-tmp vol=%s at %p\n", vol->vol_name,
           vol->vol_name);
 
@@ -458,10 +446,8 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
             vol->dev->print_name());
     }
 
-    /*
-     * Check if we are trying to use the Volume on a different drive dev is our
-     * device vol->dev is where the Volume we want is
-     */
+    /* Check if we are trying to use the Volume on a different drive dev is our
+     * device vol->dev is where the Volume we want is */
     if (dcr->dev != vol->dev) {
       // Caller wants to switch Volume to another device
 
@@ -480,17 +466,20 @@ VolumeReservationItem* reserve_volume(DeviceControlRecord* dcr,
         FreeVolume(dcr->dev); /* free any volume attached to our drive */
         Dmsg1(50, "SetUnload dev=%s\n", dcr->dev->print_name());
         dcr->dev->SetUnload(); /* Unload any volume that is on our drive */
-        dcr->SetDev(vol->dev); /* temp point to other dev */
-        slot = GetAutochangerLoadedSlot(dcr); /* get slot on other drive */
-        dcr->SetDev(dcr->dev);                /* restore dev */
-        vol->SetSlotNumber(slot);             /* save slot */
-        vol->dev->SetUnload();                /* unload the other drive */
-        vol->SetSwapping();                   /* swap from other drive */
-        dcr->dev->swap_dev = vol->dev;        /* remember to get this vol */
-        dcr->dev->SetLoad();                  /* then reload on our drive */
-        vol->dev->vol = NULL; /* remove volume from other drive */
-        vol->dev = dcr->dev;  /* point the Volume at our drive */
-        dcr->dev->vol = vol;  /* point our drive at the Volume */
+        {
+          auto original_dcr_dev = dcr->dev;
+          dcr->SetDev(vol->dev);                /* temp point to other dev */
+          slot = GetAutochangerLoadedSlot(dcr); /* get slot on other drive */
+          dcr->SetDev(original_dcr_dev);        /* restore dev */
+        }
+        vol->SetSlotNumber(slot);      /* save slot */
+        vol->dev->SetUnload();         /* unload the other drive */
+        vol->SetSwapping();            /* swap from other drive */
+        dcr->dev->swap_dev = vol->dev; /* remember to get this vol */
+        dcr->dev->SetLoad();           /* then reload on our drive */
+        vol->dev->vol = NULL;          /* remove volume from other drive */
+        vol->dev = dcr->dev;           /* point the Volume at our drive */
+        dcr->dev->vol = vol;           /* point our drive at the Volume */
       } else {
         Jmsg7(dcr->jcr, M_WARNING, 0,
               "Need volume from other drive, but swap not possible. "
@@ -716,12 +705,10 @@ bool VolumeUnused(DeviceControlRecord* dcr)
     return false;
   }
 
-  /*
-   * If this is a tape, we do not free the volume, rather we wait
+  /* If this is a tape, we do not free the volume, rather we wait
    * until the autoloader unloads it, or until another tape is
    * explicitly read in this drive. This allows the SD to remember
-   * where the tapes are or last were.
-   */
+   * where the tapes are or last were. */
   Dmsg4(debuglevel,
         "=== set not reserved vol=%s num_writers=%d dev_reserved=%d dev=%s\n",
         dev->vol->vol_name, dev->num_writers, dev->NumReserved(),
@@ -729,10 +716,8 @@ bool VolumeUnused(DeviceControlRecord* dcr)
   if (dev->IsTape() || dev->AttachedToAutochanger()) {
     return true;
   } else {
-    /*
-     * Note, this frees the volume reservation entry, but the file descriptor
-     * remains open with the OS.
-     */
+    /* Note, this frees the volume reservation entry, but the file descriptor
+     * remains open with the OS. */
     return FreeVolume(dev);
   }
 }
@@ -755,12 +740,10 @@ bool FreeVolume(Device* dev)
     Dmsg1(debuglevel, "=== clear in_use vol=%s\n", vol->vol_name);
     dev->vol = NULL;
 
-    /*
-     * Volume is on write volume list if one of the folling is applicable:
+    /* Volume is on write volume list if one of the folling is applicable:
      *  - The volume is written to.
      *  - Config option filedevice_concurrent_read is not on.
-     *  - The device is not of type File.
-     */
+     *  - The device is not of type File. */
     if (vol->IsWriting() || !me->filedevice_concurrent_read
         || !dev->CanReadConcurrently()) {
       vol_list->remove(vol);