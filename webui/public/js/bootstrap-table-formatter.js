b12d24b1f84fcedd953d19ba21b2f4c43bfee5d6
Joerg Steffens <joerg.steffens@bareos.com>
2023-02-02 19:01:24 +0100

    webui: re-add show update status for clients
    The server download.bareos.com offers a sorted list of current Bareos
    releases at
    https://download.bareos.com/release-info/bareos-version-info.js
    The Bareos-WebUI uses this list and add status information about the
    Bareos clients.

--- webui/public/js/bootstrap-table-formatter.js
+++ webui/public/js/bootstrap-table-formatter.js
@@ -414,6 +414,18 @@ function formatUname(value, basePath) {
    }
 }
 
+function formatUpdateStatus(value, row, index) {
+    var label = "label-default";
+    if (row.version_status == "upgrade_required") {
+        label = "label-danger";
+    } else if (row.version_status == "update_required") {
+        label = "label-warning";
+    } else if (row.version_status == "uptodate") {
+        label = "label-success";
+    }
+    return `<span class="label ${label}" id="label-fd-version" data-toggle="tooltip" data-placement="top" title="${row.version_tooltip}">${row.version}</span>`;
+}
+
 function formatScheduleName(value, basePath) {
    return '<a href="' + basePath + '/schedule/details?schedule=' + value + '">' + value + '</a>';
 }
