5222f162659b0d7ac099108740a70e418fc73b12
Sebastian Sura <sebastian.sura@bareos.com>
2023-03-08 07:35:15 +0100

    backup: add option to choose serial backup
    At the moment this is just a compile time switch.  In the future it
    may become a configuration option.

--- core/src/filed/backup.cc
+++ core/src/filed/backup.cc
@@ -173,11 +173,6 @@ bool BlastDataToStorageDaemon(JobControlRecord* jcr, crypto_cipher_t cipher)
 
   if (!CryptoSessionStart(jcr, cipher)) { return false; }
 
-  // jcr->fd_impl->ff is only used by the sending thread
-  // since it should just send the files it was given, we set
-  // incremental & accurate to false so it does not double check them
-  SetFindOptions((FindFilesPacket*)jcr->fd_impl->ff, false,
-                 jcr->fd_impl->since_time);
 
 
   StartHeartbeatMonitor(jcr);
@@ -197,33 +192,50 @@ bool BlastDataToStorageDaemon(JobControlRecord* jcr, crypto_cipher_t cipher)
     memset(jcr->fd_impl->xattr_data->u.build, 0, sizeof(xattr_build_data_t));
     jcr->fd_impl->xattr_data->u.build->content = GetPoolMemory(PM_MESSAGE);
   }
+#if 1 && !defined(SEND_SERIAL)
+  // jcr->fd_impl->ff is only used by the sending thread
+  // since it should just send the files it was given, we set
+  // incremental & accurate to false so it does not double check them
+  SetFindOptions((FindFilesPacket*)jcr->fd_impl->ff, false,
+                 jcr->fd_impl->since_time);
 
   std::optional<std::size_t> list_ok = std::nullopt;
   bool send_ok = true;
   try {
-  // buffer size chosen at random
-  // todo: is there a better buffer size maybe ?
-  auto [ins, outs] = CreateNecessaryChannels(jcr->fd_impl->ff->fileset, 50);
-  std::thread list_thread{
-      [](JobControlRecord* jcr, findFILESET* ff, bool incremental,
-	 time_t save_time,
-	 std::optional<bool (*)(JobControlRecord*, FindFilesPacket*)> chk,
-	 auto ins,
-         std::optional<std::size_t>& ok) { ok = ListFiles(jcr, ff, incremental, save_time, chk,
-							  std::move(ins)); },
-      jcr,
-      jcr->fd_impl->ff->fileset,
-      jcr->fd_impl->incremental,
-      jcr->fd_impl->since_time,
-      (jcr->accurate) ? std::optional{&AccurateCheckFile} : std::nullopt,
-      std::move(ins),
-      std::ref(list_ok)};
-  if (!SendFiles(jcr, jcr->fd_impl->ff, std::move(outs), SaveFile,
-                 PluginSave)) {
-    send_ok = false;
-  }
+	  // buffer size chosen at random
+	  // todo: is there a better buffer size maybe ?
+	  auto [ins, outs] = CreateNecessaryChannels(jcr->fd_impl->ff->fileset, 50);
+	  std::thread list_thread{
+		  [](JobControlRecord* jcr, findFILESET* ff, bool incremental,
+		     time_t save_time,
+		     std::optional<bool (*)(JobControlRecord*, FindFilesPacket*)> chk,
+		     auto ins,
+		     std::optional<std::size_t>& ok) {
+			  try {
+				  ok = ListFiles(jcr, ff, incremental, save_time, chk,
+						 std::move(ins));
+			  } catch (const std::exception& ex) {
+				  ok = std::nullopt;
+				  Dmsg1(500, "Uncaught exception! Aborting.\n Message: %s\n", ex.what());
+			  } catch (...)
+			  {
+				  ok = std::nullopt;
+				  Dmsg1(500, "Uncaught unknown exception! Aborting.\n");
+			  }
+		  },
+		  jcr,
+		  jcr->fd_impl->ff->fileset,
+		  jcr->fd_impl->incremental,
+		  jcr->fd_impl->since_time,
+		  (jcr->accurate) ? std::make_optional(&AccurateCheckFile) : std::nullopt,
+		  std::move(ins),
+		  std::ref(list_ok)};
+	  if (!SendFiles(jcr, jcr->fd_impl->ff, std::move(outs), SaveFile,
+			 PluginSave)) {
+		  send_ok = false;
+	  }
 
-  list_thread.join();
+	  list_thread.join();
   } catch (const std::exception& ex)
   {
 	  send_ok = false;
@@ -246,6 +258,22 @@ bool BlastDataToStorageDaemon(JobControlRecord* jcr, crypto_cipher_t cipher)
     ok = false; /* error */
     jcr->setJobStatusWithPriorityCheck(JS_ErrorTerminated);
   }
+#else
+  SetFindOptions((FindFilesPacket*)jcr->fd_impl->ff, jcr->fd_impl->incremental,
+                 jcr->fd_impl->since_time);
+
+  // In accurate mode, we overload the find_one check function
+  if (jcr->accurate) {
+    SetFindChangedFunction((FindFilesPacket*)jcr->fd_impl->ff,
+                           AccurateCheckFile);
+  }
+  // Subroutine SaveFile() is called for each file
+  if (!FindFiles(jcr, (FindFilesPacket*)jcr->fd_impl->ff, SaveFile,
+                 PluginSave)) {
+    ok = false; /* error */
+    jcr->setJobStatusWithPriorityCheck(JS_ErrorTerminated);
+  }
+#endif
 
   if (have_acl && jcr->fd_impl->acl_data->u.build->nr_errors > 0) {
     Jmsg(jcr, M_WARNING, 0,
