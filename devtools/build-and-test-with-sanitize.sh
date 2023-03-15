34fdfc60b4bce36adc3d8936a47493004d22f44e
Andreas Rogge <andreas.rogge@bareos.com>
2023-03-15 15:01:06 +0100

    devtools: add license-header to build script
    (cherry picked from commit 8149d56d697fcdcb89ce971cb33690ba13a37dda)

--- devtools/build-and-test-with-sanitize.sh
+++ devtools/build-and-test-with-sanitize.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#   BAREOS® - Backup Archiving REcovery Open Sourced
+#
+#   Copyright (C) 2022-2023 Bareos GmbH & Co. KG
+#
+#   This program is Free Software; you can redistribute it and/or
+#   modify it under the terms of version three of the GNU Affero General Public
+#   License as published by the Free Software Foundation and included
+#   in the file LICENSE.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#   Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#   02110-1301, USA.
+
 set -u
 set -e
 set -x
