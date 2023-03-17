e678933ba8ab741dd3728cde425feb501934c5c1
Frank Bergkemper <frank.bergkemper@bareos.com>
2023-03-17 11:56:01 +0100

    webui: add config resource graph to analytics
    Adds an experimental feature to the analytics module
    that displays a D3 force directed graph of configuration
    resources and how these are connected to each other.
    
    Currently the following configuration resources are
    a part of the graph.
    
    - client resources
    - job resources
    - jobdefs resources
    - fileset resources
    - schedule resources

--- webui/module/Analytics/src/Analytics/Model/AnalyticsModel.php
+++ webui/module/Analytics/src/Analytics/Model/AnalyticsModel.php
@@ -52,4 +52,146 @@ class AnalyticsModel
             throw new \Exception('Missing argument.');
         }
     }
+
+    public function getConfigResourceId($type, &$name)
+    {
+        # valid CSS id can only contain characters [a-zA-Z0-9-_]
+        return $type . "_" . md5($name);
+    }
+
+    public function createConfigResourceNode($type, &$resource)
+    {
+        $node = new \stdClass();
+        $node->type = $type;
+        $node->id = $this->getConfigResourceId($type, $resource["name"]);
+        $node->name = $resource["name"];
+        $node->resource = $resource;
+        return $node;
+    }
+
+
+    public function getConfigResourceGraph(&$bsock = null)
+    {
+        if (!isset($bsock)) {
+            throw new \Exception('Missing argument.');
+        }
+
+        $cmd = 'show clients';
+        $result = $bsock->send_command($cmd, 2);
+        $clients = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+        $clients_cache = $clients['result']['clients'];
+
+        $cmd = 'show jobs';
+        $result = $bsock->send_command($cmd, 2);
+        $jobs = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+        $jobs_cache = $jobs['result']['jobs'];
+
+        $cmd = 'show jobdefs';
+        $result = $bsock->send_command($cmd, 2);
+        $jobdefs = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+        $jobdefs_cache = $jobdefs['result']['jobdefs'];
+
+        $cmd = 'show fileset';
+        $result = $bsock->send_command($cmd, 2);
+        $filesets = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+        $filesets_cache = $filesets['result']['filesets'];
+
+        $cmd = 'show schedules';
+        $result = $bsock->send_command($cmd, 2);
+        $schedules = \Zend\Json\Json::decode($result, \Zend\Json\Json::TYPE_ARRAY);
+        $schedules_cache = $schedules['result']['schedules'];
+
+        $config_graph = new \stdClass();
+        $nodes = array();
+        $links = array();
+
+        foreach ($clients_cache as $client_key => $client) {
+            $node = $this->createConfigResourceNode("client", $client);
+            array_push($nodes, $node);
+            foreach ($jobs_cache as $job_key => $job) {
+                if (isset($job['client'])) {
+                    if ($client["name"] === $job["client"]) {
+                        $link = new \stdClass();
+                        $link->source = $node->id;
+                        $link->target = $this->getConfigResourceId("job", $job["name"]);
+                        array_push($links, $link);
+                    }
+                }
+            }
+        }
+        foreach ($jobs_cache as $job_key => $job) {
+            $node = $this->createConfigResourceNode("job", $job);
+            array_push($nodes, $node);
+            if (isset($job["jobdefs"])) {
+                foreach ($jobdefs_cache as $jobdefs_key => $jobdefs) {
+                    if ($job["jobdefs"] === $jobdefs["name"]) {
+                        $link = new \stdClass();
+                        $link->source = $node->id;
+                        $link->target = $this->getConfigResourceId("jobdefs", $jobdefs["name"]);
+                        array_push($links, $link);
+                    }
+                }
+            }
+            if (isset($job["schedule"])) {
+                foreach ($schedules_cache as $schedule_key => $schedule) {
+                    if ($job["schedule"] === $schedule["name"]) {
+                        $link = new \stdClass();
+                        $link->source = $node->id;
+                        $link->target = $this->getConfigResourceId("schedule", $schedule["name"]);
+                        array_push($links, $link);
+                    }
+                }
+            }
+            if (isset($job["fileset"])) {
+                foreach ($filesets_cache as $filesets_key => $fileset) {
+                    if ($job["fileset"] === $fileset["name"]) {
+                        $link = new \stdClass();
+                        $link->source = $node->id;
+                        $link->target = $this->getConfigResourceId("fileset", $fileset["name"]);
+                        array_push($links, $link);
+                    }
+                }
+            }
+        }
+
+        foreach ($filesets_cache as $fileset_key => $fileset) {
+            $node = $this->createConfigResourceNode("fileset", $fileset);
+            array_push($nodes, $node);
+        }
+
+        foreach ($jobdefs_cache as $jobdefs_key => $jobdefs) {
+            $node = $this->createConfigResourceNode("jobdefs", $jobdefs);
+            array_push($nodes, $node);
+            if (isset($jobdefs["schedule"])) {
+                foreach ($schedules_cache as $schedule_key => $schedule) {
+                    if ($jobdefs["schedule"] === $schedule["name"]) {
+                        $link = new \stdClass();
+                        $link->source = $node->id;
+                        $link->target = $this->getConfigResourceId("schedule", $schedule["name"]);
+                        array_push($links, $link);
+                    }
+                }
+            }
+            if (isset($jobdefs["fileset"])) {
+                foreach ($filesets_cache as $fileset_key => $fileset) {
+                    if ($jobdefs["fileset"] === $fileset["name"]) {
+                        $link = new \stdClass();
+                        $link->source = $node->id;
+                        $link->target = $this->getConfigResourceId("fileset", $fileset["name"]);
+                        array_push($links, $link);
+                    }
+                }
+            }
+        }
+
+        foreach ($schedules_cache as $schedule_key => $schedule) {
+            $node = $this->createConfigResourceNode("schedule", $schedule);
+            array_push($nodes, $node);
+        }
+
+        $config_graph->nodes = $nodes;
+        $config_graph->links = $links;
+
+        return $config_graph;
+    }
 }
