a4cbc560d4029d2762e41a6e3d4eeae0d3df17e0
Sebastian Sura <sebastian.sura@bareos.com>
2023-03-22 07:48:27 +0100

    sanitizer: add coredump safeguard to sanitizer builds
    This should prevent extremely huge core files

--- devtools/build-and-test-with-sanitize.sh
+++ devtools/build-and-test-with-sanitize.sh
@@ -38,6 +38,8 @@ if [ -z ${CMAKE_BUILD_PARALLEL_LEVEL+x} ]; then
   export CMAKE_BUILD_PARALLEL_LEVEL="$nproc"
 fi
 
+export ASAN_OPTIONS="disable_coredump=0:unmap_shadow_on_exit=1:abort_on_error=1"
+
 rm -rf cmake-build
 
 cmake \
