4b21e495fdfa798efa35dec1349b4a6e6c1b806c
Sebastian Sura <sebastian.sura@bareos.com>
2023-03-22 10:08:45 +0100

    find: fix using bad cached values
    We overwrote some right values with garbage ones on the first run
    which then got cached and caused even more issues.  Now we first setup
    the cache with correct values and then always use them.

--- core/src/findlib/find.cc
+++ core/src/findlib/find.cc
@@ -1076,21 +1076,27 @@ int SendFiles(JobControlRecord* jcr,
     auto [in, out] = channel::CreateBufferedChannel<stated_opened_file>(max_open_files);
     std::thread opener(PrepareFileForSending, jcr, std::move(outs), std::move(in));
 
-    // some values are not overwritten for each file inside an include block
-    // but are "cached" from one run to the next.  As we now send send them
-    // out of order (between different include blocks), we need to ensure
-    // that we restore them if needed
-    struct cached_vals { int StripPath; char* top_fname; };
+    // some values are not setup by AcceptFile / SetupFFPkt but instead
+    // get set once per include block inside SetupLastOptionBlock.
+    // Not all of these are used during the send, so we "cache" them
+    // here once and then always reuse them.
+    struct cached_vals { int StripPath; };
     // everything is set to 0
     std::vector<cached_vals> cached_values(fileset->include_list.size());
+    for (std::size_t i = 0; i < cached_values.size(); ++i) {
+	fileset->incexe = fileset->include_list.get(i);
+	SetupLastOptionBlock(ff, fileset->incexe);
+	cached_values[i].StripPath = ff->StripPath;
+    }
     while (1) {
       if (std::optional opened_file = out.get(); opened_file) {
 	auto& [file, bfd, fileset_idx] = opened_file.value();
 	fileset->incexe = fileset->include_list.get(fileset_idx);
 	char* fname = file.name.data();
-	SetupLastOptionBlock(ff, fileset->incexe);
 	ff->StripPath = cached_values[fileset_idx].StripPath;
-	ff->top_fname = cached_values[fileset_idx].top_fname;
+	// todo: what to do with top_fname ? its only used
+	// for debug messages from here on out and setting it up
+	// correctly seems wasteful
 	if (!SetupFFPkt(jcr, ff, fname, file.statp, file.delta_seq,
 			file.type, file.hfsinfo)) {
 	  Dmsg1(debuglevel, "Error: Could not setup ffpkt for file '%s'\n",
@@ -1123,8 +1129,6 @@ int SendFiles(JobControlRecord* jcr,
 	} else {
 	  CleanupLink(ff);
 	  if (ff->linked) { ff->linked->FileIndex = ff->FileIndex; }
-	  cached_values[fileset_idx].StripPath = ff->StripPath;
-	  cached_values[fileset_idx].top_fname = ff->top_fname;
 	}
 	if (JobCanceled(jcr)) { ret_val = 0; break; }
       } else {
