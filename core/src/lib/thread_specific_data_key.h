2bb7d74215bf6ae4f350de11bff04b68cf3c82b3
Bruno Friedmann <bruno.friedmann@bareos.com>
2023-03-27 13:22:11 +0200

    core: fixes for gcc-13 support
    - add explicit system_error and stdexcept
    - console_conf.cc fix std::move warning
    
    Signed-off-by: Bruno Friedmann <bruno.friedmann@bareos.com>

--- core/src/lib/thread_specific_data_key.h
+++ core/src/lib/thread_specific_data_key.h
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2019-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2019-2023 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -22,6 +22,8 @@
 #ifndef BAREOS_LIB_THREAD_SPECIFIC_DATA_KEY_H_
 #define BAREOS_LIB_THREAD_SPECIFIC_DATA_KEY_H_
 
+
+#include <system_error>
 #include "lib/berrno.h"
 
 #include <mutex>
