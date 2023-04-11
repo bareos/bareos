45ea37ca3adef924e2b2325a94989f5388a39f9b
Bruno Friedmann <bruno.friedmann@bareos.com>
2023-04-11 14:45:53 +0200

    modernize grants sql to support postgresql>15
    - grant permission on public schema with postgresql>15.
    - add views to reassign Debian dbconfig template.
    - improve readonly grant sample sql script.
    
    Signed-off-by: Bruno Friedmann <bruno.friedmann@bareos.com>

--- core/src/cats/ddl/grants/postgresql-change_owner.dbconfig-template.sql
+++ core/src/cats/ddl/grants/postgresql-change_owner.dbconfig-template.sql
@@ -8,4 +8,7 @@ CREATE OR REPLACE FUNCTION execute(text)
 SELECT execute('ALTER TABLE '|| tablename ||' OWNER TO _DBC_DBUSER_;')
     FROM pg_tables WHERE schemaname='public' AND tableowner!='_DBC_DBUSER_';
 
+SELECT execute('ALTER VIEW '|| viewname ||' OWNER TO _DBC_DBUSER_;')
+    FROM pg_views WHERE schemaname='public' AND viewowner!='_DBC_DBUSER_';
+
 DROP FUNCTION execute(text);
--- core/src/cats/ddl/grants/postgresql.sql
+++ core/src/cats/ddl/grants/postgresql.sql
@@ -1,53 +1,11 @@
 CREATE USER @DB_USER@ @DB_PASS@;
 
--- For tables
-GRANT ALL ON BaseFiles TO @DB_USER@;
-GRANT ALL ON JobMedia TO @DB_USER@;
-GRANT ALL ON File TO @DB_USER@;
-GRANT ALL ON Job TO @DB_USER@;
-GRANT ALL ON Media TO @DB_USER@;
-GRANT ALL ON Client TO @DB_USER@;
-GRANT ALL ON Pool TO @DB_USER@;
-GRANT ALL ON Fileset TO @DB_USER@;
-GRANT ALL ON Path TO @DB_USER@;
-GRANT ALL ON Counters TO @DB_USER@;
-GRANT ALL ON Version TO @DB_USER@;
-GRANT ALL ON MediaType TO @DB_USER@;
-GRANT ALL ON Storage TO @DB_USER@;
-GRANT ALL ON Device TO @DB_USER@;
-GRANT ALL ON Status TO @DB_USER@;
-GRANT ALL ON Location TO @DB_USER@;
-GRANT ALL ON LocationLog TO @DB_USER@;
-GRANT ALL ON Log TO @DB_USER@;
-GRANT ALL ON JobHisto TO @DB_USER@;
-GRANT ALL ON PathHierarchy TO @DB_USER@;
-GRANT ALL ON PathVisibility TO @DB_USER@;
-GRANT ALL ON RestoreObject TO @DB_USER@;
-GRANT ALL ON Quota TO @DB_USER@;
-GRANT ALL ON NDMPLevelMap TO @DB_USER@;
-GRANT ALL ON NDMPJobEnvironment TO @DB_USER@;
-GRANT ALL ON DeviceStats TO @DB_USER@;
-GRANT ALL ON JobStats TO @DB_USER@;
-GRANT ALL ON TapeAlerts TO @DB_USER@;
+-- For schema
+GRANT ALL ON SCHEMA public TO @DB_USER@;
 
--- For sequences ON those tables
-GRANT SELECT, UPDATE ON path_pathid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON fileset_filesetid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON pool_poolid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON client_clientid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON media_mediaid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON job_jobid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON file_fileid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON jobmedia_jobmediaid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON basefiles_baseid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON storage_storageid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON mediatype_mediatypeid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON device_deviceid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON location_locationid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON locationlog_loclogid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON log_logid_seq TO @DB_USER@;
-GRANT SELECT, UPDATE ON restoreobject_restoreobjectid_seq TO @DB_USER@;
+-- For tables & views
+GRANT ALL ON ALL TABLES IN SCHEMA public TO @DB_USER@;
 
--- For views
-GRANT ALL ON backup_unit_overview TO @DB_USER@;
-GRANT ALL ON latest_full_size_categorized TO @DB_USER@;
+-- For sequences ON for tables
+GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO @DB_USER@;
+
