f53139c8ff23a06d93ffc7f495680bf5b082b353
Sebastian Sura <sebastian.sura@bareos.com>
2023-04-17 11:36:06 +0200

    compression: rename LevelChangeResult::ERROR
    As it is it does not compile on windows since some windows
    header seems to define ERROR somewhere.

--- core/src/filed/backup.cc
+++ core/src/filed/backup.cc
@@ -175,7 +175,7 @@ static void CompressionWorker(channel::out<std::shared_ptr<compress_worker_input
     if (SetCompressionLevel(args->jcr,
 			    args->algorithm,
 			    args->level,
-			    *ctx) == LevelChangeResult::ERROR) {
+			    *ctx) == LevelChangeResult::CHANGE_ERROR) {
       chan.reset();
       continue;
     }
