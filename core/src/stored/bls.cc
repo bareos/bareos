96c78ccf449accfcef5c3aa6fba391d6c04216e1
Alaa Eddine Elamri <alaeddine.elamri@bareos.com>
2023-04-28 09:45:43 +0200

    bls: bextract: fix `-i` not correctly processing
    (cherry picked from commit e94a854be52f192e2ccb787011c3dff5d28682bc)

--- core/src/stored/bls.cc
+++ core/src/stored/bls.cc
@@ -132,8 +132,8 @@ int main(int argc, char* argv[])
           [&line, &fd](std::vector<std::string> val) {
             if ((fd = fopen(val.front().c_str(), "rb")) == nullptr) {
               BErrNo be;
-              Pmsg2(0, _("Could not open exclude file: %s, ERR=%s\n"), optarg,
-                    be.bstrerror());
+              Pmsg2(0, _("Could not open exclude file: %s, ERR=%s\n"),
+                    val.front().c_str(), be.bstrerror());
               exit(1);
             }
             while (fgets(line, sizeof(line), fd) != nullptr) {
@@ -150,11 +150,11 @@ int main(int argc, char* argv[])
   bls_app
       .add_option(
           "-i,--include-list",
-          [&line, &fd](std::vector<std::string>) {
-            if ((fd = fopen(optarg, "rb")) == nullptr) {
+          [&line, &fd](std::vector<std::string> val) {
+            if ((fd = fopen(val.front().c_str(), "rb")) == nullptr) {
               BErrNo be;
-              Pmsg2(0, _("Could not open include file: %s, ERR=%s\n"), optarg,
-                    be.bstrerror());
+              Pmsg2(0, _("Could not open include file: %s, ERR=%s\n"),
+                    val.front().c_str(), be.bstrerror());
               exit(1);
             }
             while (fgets(line, sizeof(line), fd) != nullptr) {
