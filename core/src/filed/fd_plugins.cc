75e430b0ec27c33002084d5b888b16632a6250f0
Andreas Rogge <andreas.rogge@bareos.com>
2023-06-02 16:23:01 +0200

    filed: fix off-by-one error when sizing acl buffer
    (cherry picked from commit 45d11b8b2937ea25a2a7db24f8abcfd204ae2cc2)

--- core/src/filed/fd_plugins.cc
+++ core/src/filed/fd_plugins.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2007-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2022 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -1328,7 +1328,7 @@ bacl_exit_code PluginBuildAclStreams(JobControlRecord* jcr,
       case bRC_OK:
         if (ap.content_length && ap.content) {
           acl_data->u.build->content = CheckPoolMemorySize(
-              acl_data->u.build->content, ap.content_length);
+              acl_data->u.build->content, ap.content_length + 1);
           memcpy(acl_data->u.build->content, ap.content, ap.content_length);
           acl_data->u.build->content_length = ap.content_length;
           free(ap.content);
