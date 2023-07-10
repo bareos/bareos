4d7cca5098cd3f7bfa0041bc04aba5636c140a7a
Sebastian Sura <sebastian.sura@bareos.com>
2023-07-10 09:55:58 +0200

    dedup-dev: enhance config validation

--- core/src/stored/backends/dedup/dedup_volume.cc
+++ core/src/stored/backends/dedup/dedup_volume.cc
@@ -65,6 +65,148 @@ void volume::write_current_config()
   }
 }
 
+class gap_list {
+  using interval = std::pair<std::size_t, std::size_t>;
+
+ public:
+  bool add(std::size_t start, std::size_t end)
+  {
+    if (start >= end) return true;
+
+    if (vec.size() == 0) {
+      vec.emplace_back(start, end);
+      return true;
+    }
+    auto iter
+        = std::lower_bound(vec.begin(), vec.end(), start,
+                           [](const auto& interval, const std::size_t& val) {
+                             return interval.first < val;
+                           });
+
+    if (iter == vec.end()) {
+      auto& inter = vec.front();  // we already checked that size > 1!
+
+      if (inter.first < end) {
+        // there is some overlap which we do not allow
+        return false;
+      } else if (inter.first == end) {
+        inter.first = start;
+        return true;
+      } else {
+        vec.insert(vec.begin(), interval{start, end});
+        return true;
+      }
+    }
+
+    auto next = iter + 1;
+
+    if (next == vec.end()) {
+      auto& inter = *iter;
+      if (inter.second > start) {
+        // there is some overlap which we do not allow
+        return false;
+      } else if (inter.second == start) {
+        inter.second = end;
+        return true;
+      } else {
+        vec.emplace_back(start, end);
+        return true;
+      }
+    }
+
+    bool combines_with_iter = iter->second == start;
+    bool combines_with_next = next->first == end;
+
+    if (combines_with_iter && combines_with_next) {
+      iter->second = next->second;
+      vec.erase(next);
+    } else if (combines_with_iter) {
+      iter->second = end;
+    } else if (combines_with_next) {
+      next->first = start;
+    } else {
+      vec.insert(next, interval{start, end});
+    }
+
+    return true;
+  }
+
+  bool has_gaps() const { return vec.size() > 1; }
+
+ private:
+  // vec is a sorted vector of half open intervals [first, second)
+  std::vector<interval> vec;
+};
+
+static bool validate_config(const config::loaded_config& conf)
+{
+  // at the moment we only support configurations that have
+  // at least one block and one record file.
+  // This might change in the future
+  if (conf.blockfiles.size() == 0) {
+    // error: few block files
+    return false;
+  }
+
+  gap_list block_gaps;
+  for (auto& blockfile : conf.blockfiles) {
+    if (!block_gaps.add(blockfile.start_block,
+                        blockfile.start_block + blockfile.num_blocks)) {
+      // todo: print duplicates
+      return false;
+    }
+  }
+
+  if (block_gaps.has_gaps()) {
+    // warning: gaps!
+  }
+
+  if (conf.recordfiles.size() == 0) {
+    // error: few record files
+    return false;
+  }
+
+  gap_list record_gaps;
+  for (auto& recordfile : conf.recordfiles) {
+    if (!record_gaps.add(recordfile.start_record,
+                         recordfile.start_record + recordfile.num_records)) {
+      // todo: print duplicates
+      return false;
+    }
+  }
+
+  gap_list file_indices;
+  for (auto& datafile : conf.datafiles) {
+    if (!file_indices.add(datafile.file_index, datafile.file_index + 1)) {
+      // todo: print duplicates
+      return false;
+    }
+  }
+
+  if (file_indices.has_gaps()) {
+    // warning: missing file indices
+  }
+
+
+  if (record_gaps.has_gaps()) {
+    // warning: gaps!
+  }
+
+  if (conf.info.block_header_size != sizeof(bareos_block_header)
+      || conf.info.dedup_block_header_size != sizeof(block_header)) {
+    // error: bad block header size
+    return false;
+  }
+
+  if (conf.info.record_header_size != sizeof(bareos_record_header)
+      || conf.info.dedup_record_header_size != sizeof(record_header)) {
+    // error: bad record header size
+    return false;
+  }
+
+  return true;
+}
+
 std::optional<volume_layout> volume::load_layout()
 {
   std::optional size = config.size();
@@ -80,32 +222,7 @@ std::optional<volume_layout> volume::load_layout()
 
   std::optional loaded_config = config::from_bytes(bytes);
   if (!loaded_config) { return std::nullopt; }
-
-  // at the moment we only support configurations that have
-  // exactly one block and one record file.
-  // This might change in the future
-  if (loaded_config->blockfiles.size() != 1) {
-    // error: to many/few block files
-    return std::nullopt;
-  }
-
-  if (loaded_config->recordfiles.size() != 1) {
-    // error: to many/few record files
-    return std::nullopt;
-  }
-
-  if (loaded_config->info.block_header_size != sizeof(bareos_block_header)
-      || loaded_config->info.dedup_block_header_size != sizeof(block_header)) {
-    // error: bad block header size
-    return std::nullopt;
-  }
-
-  if (loaded_config->info.record_header_size != sizeof(bareos_record_header)
-      || loaded_config->info.dedup_record_header_size
-             != sizeof(record_header)) {
-    // error: bad record header size
-    return std::nullopt;
-  }
+  if (!validate_config(*loaded_config)) { return std::nullopt; }
 
   for (auto& rec : loaded_config->unfinished) {
     // FIXME: check wether this write_loc makes any sense!
