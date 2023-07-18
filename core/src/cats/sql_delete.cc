4cdc1461a0b8a428d46147e8438ef4ca2fcaf9fe
Alaa Eddine Elamri <alaeddine.elamri@bareos.com>
2023-07-18 11:59:05 +0200

    dird: cats: stop file purge if there are no eligible jobids

--- core/src/cats/sql_delete.cc
+++ core/src/cats/sql_delete.cc
@@ -221,6 +221,11 @@ bool BareosDb::PurgeMediaRecord(JobControlRecord* jcr, MediaDbRecord* mr)
 
 void BareosDb::PurgeFiles(const char* jobids)
 {
+  if (strcmp(jobids, "") == 0) {
+    Dmsg0(100, "No jobids to use for purging files\n");
+    return;
+  }
+
   PoolMem query(PM_MESSAGE);
 
   Mmsg(query, "DELETE FROM File WHERE JobId IN (%s)", jobids);
