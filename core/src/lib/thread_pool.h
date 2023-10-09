b1d7733150e23b64533021f07af10cc17e0aef39
Sebastian Sura <sebastian.sura@bareos.com>
2023-10-09 06:59:59 +0200

    thread-pool: split thread_pool into thread_pool+work_group
    Previously thread_pool took care of two different concerns:
    1) Being able to reuse threads for work
    2) Sharing threads for concurrent non-blocking computations
    
    To be able to support both of these concerns we needed to add a lot of
    complexity to the class.  Splitting it into two, a thread_pool, that
    only lets us borrow threads for a while, and a work group, with which
    you can share threads to do non-blocking computations, made the code
    much easier to understand.

--- core/src/lib/thread_pool.h
+++ core/src/lib/thread_pool.h
@@ -36,108 +36,218 @@
 #include <future>
 #include <type_traits>
 #include <exception>
+#include <memory>
 
 #include "lib/thread_util.h"
+#include "lib/channel.h"
+#include "include/baconfig.h"
 
-struct worker_pool {
-  std::vector<std::thread> threads{};
-  std::size_t num_borrowed{0};
-  std::size_t min_workers{0};
-  std::size_t dead_workers{0};
-
-  std::size_t num_actual_workers() const
+/* this class is basically std::function<void(void)>; except that
+ * it does not require the function to be copyable.
+ * We can change this to be an alias for std::move_only_function<void(void)>
+ * once it becomes available (C++23). */
+class task {
+ public:
+  /* ensure that we cannot wrap a task with a task;
+   * this is needed to disamiguate between creating a task
+   * and move-constructing one. */
+  template <typename F,
+            typename = std::enable_if_t<!std::is_reference_v<F>>,
+            typename = std::enable_if_t<!std::is_same_v<F, task>>>
+  task(F&& f) : ptr{new impl<F>(std::move(f))}
   {
-    return threads.size() - num_borrowed - dead_workers;
+  }
+
+  task(task&&) = default;
+  task& operator=(task&&) = default;
+
+  void operator()() { ptr->operator()(); }
+
+ private:
+  // impl_base + impl are used to type erase F
+  struct impl_base {
+    virtual void operator()() = 0;
+    virtual ~impl_base() = default;
+  };
+
+  template <typename F>
+  struct impl
+      : impl_base
+      , F {
+    impl(F&& f) : F(std::move(f)) {}
+
+    // using F::operator(); does not work :(
+    void operator()() override { F::operator()(); }
+  };
+
+  std::unique_ptr<impl_base> ptr;
+};
+
+struct thread_pool {
+  // f does not need to be copyable
+  template <typename F> void borrow_thread(F&& f)
+  {
+    with_free_threads(1, [f = std::move(f)](work_unit& unit) mutable {
+      unit.submit(std::move(f));
+    });
+  }
+
+  // f needs to be copyable here
+  template <typename F> void borrow_threads(std::size_t size, F&& f)
+  {
+    with_free_threads(
+        size, [f = std::move(f)](work_unit& unit) mutable { unit.submit(f); });
+  }
+
+  ~thread_pool()
+  {
+    for (auto sync : units) { sync->lock()->close(); }
+
+    for (auto& thread : threads) { thread.join(); }
+  }
+
+ private:
+  template <typename ThreadFn>
+  void with_free_threads(std::size_t size, ThreadFn f)
+  {
+    std::size_t found = 0;
+    for (std::size_t i = 0; i < threads.size() && found < size; ++i) {
+      if (auto locked = units[i]->try_lock();
+          locked && locked.value()->is_waiting()) {
+        f(*locked.value());
+        found += 1;
+      }
+    }
+
+    // if there were not enough free threads, we need to create new ones
+    for (std::size_t i = found; i < size; ++i) {
+      auto idx = threads.size();
+      add_thread();
+
+      ASSERT(idx == threads.size() - 1);
+      f(*units[idx]->lock());
+    }
+  }
+
+  class work_unit {
+   public:
+    template <typename F> void submit(F f)
+    {
+      ASSERT(state == work_state::WAITING);
+      fun = std::move(f);
+      state = work_state::WORKING;
+      state_changed.notify_all();
+    }
+
+    void close()
+    {
+      state = work_state::CLOSED;
+      fun.reset();
+      state_changed.notify_all();
+    }
+
+    bool is_waiting() const { return state == work_state::WAITING; }
+
+    bool is_closed() const { return state == work_state::CLOSED; }
+
+    void do_work()
+    {
+      ASSERT(state == work_state::WORKING);
+      ASSERT(fun.has_value());
+      fun.value()();
+      fun.reset();
+      state = work_state::WAITING;
+      state_changed.notify_all();
+    }
+
+    std::condition_variable state_changed;
+
+   private:
+    enum class work_state
+    {
+      WORKING,
+      WAITING,
+      CLOSED
+    };
+    work_state state{work_state::WAITING};
+    std::optional<task> fun;
+  };
+
+  std::vector<synchronized<work_unit>*> units;
+  std::vector<std::thread> threads{};
+
+  void add_thread()
+  {
+    auto* sync = new synchronized<work_unit>();
+    units.push_back(sync);
+    threads.emplace_back(
+        [](thread_pool* pool, synchronized<work_unit>* unit) {
+          pool->pool_work(*unit);
+        },
+        this, sync);
+  }
+
+  void pool_work(synchronized<work_unit>& unit)
+  {
+    auto locked = unit.lock();
+    for (;;) {
+      locked.wait(locked->state_changed,
+                  [](const work_unit& w) { return !w.is_waiting(); });
+
+      if (locked->is_closed()) { return; }
+
+      locked->do_work();
+    }
   }
 };
 
-class thread_pool {
- public:
-  // fixme(ssura): change this to std::move_only_function (c++23)
-  using task = std::function<void()>;
+struct work_group {
+  void work_until_completion()
+  {
+    std::optional<task> my_task;
+    while (my_task = task_out.lock()->get(), my_task != std::nullopt) {
+      my_task->operator()();
+    }
+  }
 
-  thread_pool(std::size_t num_threads = 0);
+  template <typename F, typename T = std::invoke_result_t<F>>
+  std::future<T> submit(F&& f)
+  {
+    std::promise<T> prom;
+    std::future ret = prom.get_future();
+    task t{[prom = std::move(prom), f = std::move(f)]() mutable {
+      try {
+        if constexpr (std::is_same_v<T, void>) {
+          f();
+          prom.set_value();
+        } else {
+          prom.set_value(f());
+        }
+      } catch (...) {
+        prom.set_exception(std::current_exception());
+      }
+    }};
 
-  void ensure_num_workers(std::size_t num_threads);
-  void finish();   // wait until all submitted tasks are accomplished
-  ~thread_pool();  // stops as fast as possible; dropping outstanding tasks
+    task_in.emplace(std::move(t));
+    return ret;
+  }
 
-  void enqueue(task&& t);  // queue task to be worked on by worker threads;
-                           // should not block
-  void borrow_thread(task&& t);  // steal thread to work on task t; can block
+  void shutdown() { task_in.close(); }
 
- private:
-  std::condition_variable worker_death;
-  synchronized<worker_pool> workers{};
+  channel::input<task> task_in;
+  synchronized<channel::output<task>> task_out;
 
-  std::condition_variable queue_or_death;
-  synchronized<std::optional<std::deque<task>>> queue{std::in_place};
+  work_group(channel::channel_pair<task> tasks)
+      : task_in{std::move(tasks.first)}, task_out{std::move(tasks.second)}
+  {
+  }
+  work_group(std::size_t cap)
+      : work_group(channel::CreateBufferedChannel<task>(cap))
+  {
+  }
 
-  std::size_t tasks_submitted{0};
-  std::condition_variable on_task_completion;
-  synchronized<std::size_t> tasks_completed{0u};
-
-  static void pool_work(std::size_t id, thread_pool* pool);
-  static void borrow_then_pool_work(task&& t,
-                                    std::size_t id,
-                                    thread_pool* pool);
-
-  std::optional<task> dequeue();
-  std::optional<task> finish_and_dequeue();
+  // You need to shutdown the work group before destroying it.
+  ~work_group() { ASSERT(task_in.closed()); }
 };
-
-template <typename F>
-auto enqueue(thread_pool& pool, F&& f) -> std::future<std::invoke_result_t<F>>
-{
-  using result_type = std::invoke_result_t<F>;
-  // todo(ssura): in c++23 we can use std::move_only_function
-  //              and pass the promise directly into the function.
-  //              currently this approach does not work because
-  //              std::function requires the function to be copyable,
-  //              but std::promise obviously is not.
-  std::shared_ptr p = std::make_shared<std::promise<result_type>>();
-  std::future fut = p->get_future();
-  pool.enqueue([f = std::move(f), mp = std::move(p)]() mutable {
-    try {
-      if constexpr (std::is_same_v<result_type, void>) {
-        f();
-        mp->set_value();
-      } else {
-        mp->set_value(f());
-      }
-    } catch (...) {
-      mp->set_exception(std::current_exception());
-    }
-  });
-  return fut;
-}
-
-template <typename F>
-auto borrow_thread(thread_pool& pool, F&& f)
-    -> std::future<std::invoke_result_t<F>>
-{
-  using result_type = std::invoke_result_t<F>;
-  // todo(ssura): in c++23 we can use std::move_only_function
-  //              and pass the promise directly into the function.
-  //              currently this approach does not work because
-  //              std::function requires the function to be copyable,
-  //              but std::promise obviously is not.
-  std::shared_ptr p = std::make_shared<std::promise<result_type>>();
-  std::future fut = p->get_future();
-  pool.borrow_thread([f = std::move(f), mp = std::move(p)]() mutable {
-    try {
-      if constexpr (std::is_same_v<result_type, void>) {
-        f();
-        mp->set_value();
-      } else {
-        mp->set_value(f());
-      }
-    } catch (...) {
-      mp->set_exception(std::current_exception());
-    }
-  });
-  return fut;
-}
-
 
 #endif  // BAREOS_LIB_THREAD_POOL_H_
