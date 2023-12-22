27443a5ee4aa72c6de2d1dbab322899e5c092723
Joerg Steffens <joerg.steffens@bareos.com>
2023-12-22 16:53:25 +0100

    cmake: use options

--- core/cmake/BareosGenerateDebianInfo.cmake
+++ core/cmake/BareosGenerateDebianInfo.cmake
@@ -75,7 +75,7 @@ else()
     list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-storage-python-plugins-common")
   endif()
 
-  if(HAVE_TRAYMONITOR)
+  if(TRAYMONITOR)
     list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-traymonitor")
   endif()
 
