37c8b6bb6e95e76422218a1091e5c7b5015e2540
Sebastian Sura <sebastian.sura@bareos.com>
2024-01-09 13:49:24 +0100

    backends: add dedup device skeleton

---
+++ core/src/stored/backends/dedup_device.h
@@ -0,0 +1,65 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2023-2024 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation, which is
+   listed in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#ifndef BAREOS_STORED_BACKENDS_DEDUP_DEVICE_H_
+#define BAREOS_STORED_BACKENDS_DEDUP_DEVICE_H_
+
+#include "stored/dev.h"
+
+#include <unordered_map>
+
+namespace storagedaemon {
+
+class dedup_device : public Device {
+ public:
+  dedup_device() = default;
+  ~dedup_device() { close(nullptr); }
+
+  // Interface from Device
+  SeekMode GetSeekMode() const override { return SeekMode::FILE_BLOCK; }
+  bool CanReadConcurrently() const override { return false; }
+  bool MountBackend(DeviceControlRecord* dcr, int timeout) override;
+  bool UnmountBackend(DeviceControlRecord* dcr, int timeout) override;
+  bool ScanForVolumeImpl(DeviceControlRecord* dcr) override;
+  int d_close(int) override;
+  int d_open(const char* pathname, int flags, int mode) override;
+  int d_ioctl(int fd, ioctl_req_t request, char* mt = NULL) override;
+  boffset_t d_lseek(DeviceControlRecord* dcr,
+                    boffset_t offset,
+                    int whence) override;
+  ssize_t d_read(int fd, void* buffer, size_t count) override;
+  ssize_t d_write(int fd, const void* buffer, size_t count) override;
+  bool d_truncate(DeviceControlRecord* dcr) override;
+  bool rewind(DeviceControlRecord* dcr) override;
+  bool UpdatePos(DeviceControlRecord* dcr) override;
+  bool Reposition(DeviceControlRecord* dcr,
+                  uint32_t rfile,
+                  uint32_t rblock) override;
+  bool eod(DeviceControlRecord* dcr) override;
+  bool d_flush(DeviceControlRecord*) override;
+
+ private:
+  bool mounted{false};
+};
+
+} /* namespace storagedaemon */
+
+#endif  // BAREOS_STORED_BACKENDS_DEDUP_DEVICE_H_
