0b651029cccb94609dd1e10750683213e7e133ab
Philipp Storz <Philipp.Storz@bareos.com>
2024-01-10 12:19:27 +0100

    service start stop works

--- systemtests/cmake/BareosSystemtestFunctions.cmake
+++ systemtests/cmake/BareosSystemtestFunctions.cmake
@@ -532,11 +532,15 @@ function(add_systemtest name file)
       COMMAND ${BASH_EXE} ${file}
       WORKING_DIRECTORY ${directory}
     )
+    if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+      set_property(TEST ${name} PROPERTY ENVIRONMENT
+        "PATH=%PATH%\;$CMAKE_BINARY_DIR\\core\\src\\dird\\Debug")
+    endif()
+    set_tests_properties(
+      ${name} PROPERTIES TIMEOUT "${SYSTEMTEST_TIMEOUT}" COST 1.0
+                         SKIP_RETURN_CODE 77
+    )
   endif()
-  set_tests_properties(
-    ${name} PROPERTIES TIMEOUT "${SYSTEMTEST_TIMEOUT}" COST 1.0
-                       SKIP_RETURN_CODE 77
-  )
 endfunction()
 
 function(add_systemtest_from_directory tests_basedir prefix test_subdir)
