edc5ffba594ae94bb18c824ed37290dbd51aee3f
Sebastian Sura <sebastian.sura@bareos.com>
2024-01-15 14:00:52 +0100

    dedup-volume: add simple config to test open/close

--- core/src/stored/backends/dedup/volume.cc
+++ core/src/stored/backends/dedup/volume.cc
@@ -22,51 +22,432 @@
 extern "C" {
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 }
 
+#include <system_error>
+#include <cerrno>
+
 #include "volume.h"
+#include "util.h"
 
 namespace dedup {
 namespace {
+struct raii_fd {
+  raii_fd(int fd) : fd{fd} {}
 
+  int fileno() { return fd; }
+
+  int release()
+  {
+    auto old = fd;
+    fd = -1;
+    return old;
+  }
+
+  operator bool() const { return fd >= 0; }
+
+  ~raii_fd()
+  {
+    if (fd >= 0) { close(fd); }
+  }
+
+  int fd{-1};
+};
+std::vector<char> LoadFile(int fd)
+{
+  std::vector<char> loaded;
+
+  auto current_size = loaded.size();
+  for (;;) {
+    auto next_size = current_size + 1024 * 1024;
+
+    loaded.resize(next_size);
+
+    auto res = read(fd, loaded.data() + current_size, next_size - current_size);
+
+    if (res < 0) {
+      std::string errctx = "while reading";
+      throw std::system_error(errno, std::generic_category(), errctx);
+    } else if (res == 0) {
+      break;
+    } else {
+      current_size += res;
+    }
+  }
+
+  loaded.resize(current_size);
+  return loaded;
 };
 
-volume::volume(open_type type,
-               int creation_mode,
-               const char* path,
-               std::size_t blocksize)
-    : sys_path{path}, blocksize{blocksize}
+void WriteFile(int fd, const std::vector<char>& written)
+{
+  std::size_t progress = 0;
+  while (progress < written.size()) {
+    auto res = write(fd, written.data() + progress, written.size() - progress);
+    if (res < 0) {
+      std::string errctx = "while writing";
+      throw std::system_error(errno, std::generic_category(), errctx);
+    } else if (res == 0) {
+      break;
+    } else {
+      progress += res;
+    }
+  }
+}
+
+int OpenRelative(open_context ctx, const char* path)
+{
+  int fd = openat(ctx.dird, path, ctx.flags);
+
+  if (fd < 0) {
+    std::string errctx = "while opening '";
+    errctx += path;
+    errctx += "'";
+    throw std::system_error(errno, std::generic_category(), errctx);
+  }
+
+  return fd;
+}
+
+};  // namespace
+
+volume::volume(open_type type, const char* path) : sys_path{path}
+{
+  bool read_only = type == open_type::ReadOnly;
+  int flags = (read_only) ? O_RDONLY : O_RDWR;
+  int dir_flags = O_RDONLY | O_DIRECTORY;
+  dird = open(path, dir_flags);
+
+  if (dird < 0) {
+    std::string errctx = "Cannot open '";
+    errctx += path;
+    errctx += "'";
+    throw std::system_error(errno, std::generic_category(), errctx);
+  }
+
+  if (raii_fd conf_fd = openat(dird, "config", flags); conf_fd) {
+    conf.emplace(LoadFile(conf_fd.fileno()));
+  } else {
+    std::string errctx = "Cannot open '";
+    errctx += path;
+    errctx += "/config'";
+    throw std::system_error(errno, std::generic_category(), errctx);
+  }
+
+  backing.emplace(
+      open_context{
+          .read_only = read_only,
+          .flags = flags,
+          .dird = dird,
+      },
+      conf.value());
+}
+
+data::data(open_context ctx, const config& conf)
+{
+  auto& bf = conf.blockfile();
+  if (bf.Start != 0) { throw std::runtime_error("blockfile start != 0."); }
+
+  auto& rf = conf.recordfile();
+  if (rf.Start != 0) { throw std::runtime_error("recordfile start != 0."); }
+
+  auto& dfs = conf.datafiles();
+  if (dfs.size() != 2) { throw std::runtime_error("too many datafiles."); }
+  const config::data_file* unalf = nullptr;
+  const config::data_file* alf = nullptr;
+
+  for (auto& df : dfs) {
+    if (df.BlockSize == 1) {
+      unalf = &df;
+    } else if (df.BlockSize > 1) {
+      alf = &df;
+    }
+  }
+
+  if (!unalf) { throw std::runtime_error("no unaligned file."); }
+
+  if (!alf) { throw std::runtime_error("no aligned file."); }
+
+  raii_fd bfd = OpenRelative(ctx, bf.relpath.c_str());
+  raii_fd rfd = OpenRelative(ctx, rf.relpath.c_str());
+  raii_fd afd = OpenRelative(ctx, alf->relpath.c_str());
+  raii_fd ufd = OpenRelative(ctx, unalf->relpath.c_str());
+
+  if (!ctx.read_only && alf->ReadOnly) {
+    throw std::runtime_error("aligned file is readonly, but rw requested.");
+  }
+  if (!ctx.read_only && unalf->ReadOnly) {
+    throw std::runtime_error("unaligned file is readonly, but rw requested.");
+  }
+
+  blocks = decltype(blocks){ctx.read_only, bfd.fileno(), bf.End};
+  records = decltype(records){ctx.read_only, rfd.fileno(), rf.End};
+  aligned = decltype(aligned){ctx.read_only, afd.fileno(), alf->Size};
+  unaligned = decltype(unaligned){ctx.read_only, ufd.fileno(), unalf->Size};
+}
+
+void volume::create_new(int creation_mode,
+                        const char* path,
+                        std::size_t blocksize)
 {
   int dir_mode
       = creation_mode | S_IXUSR;  // directories need execute permissions
 
-  int flags = 0;
-  bool read_only = type == open_type::ReadOnly;
-  if (read_only) {
-    flags |= O_RDONLY;
-  } else {
-    flags |= O_RDWR | O_CREAT;
+  if (mkdir(path, dir_mode) < 0) {
+    std::string errctx = "Cannot create directory: '";
+    errctx += path;
+    errctx += "'";
+    throw std::system_error(errno, std::generic_category(), errctx);
   }
 
-  int dir_flags = flags | O_DIRECTORY;
-  dird = open(path, dir_flags, dir_mode);
+  int flags = O_RDWR | O_CREAT;
+  int dir_flags = O_RDONLY | O_DIRECTORY;
+  raii_fd dird = open(path, dir_flags);
 
-  if (dird < 0) { throw std::invalid_argument("dird < 0"); }
-
-  int aligned_fd = openat(dird, "aligned.data", flags, creation_mode);
-  int unaligned_fd = openat(dird, "unaligned.data", flags, creation_mode);
-  int records_fd = openat(dird, "records.data", flags, creation_mode);
-  int blocks_fd = openat(dird, "blocks.data", flags, creation_mode);
-
-  if (aligned_fd < 0 || unaligned_fd < 0 || records_fd < 0 || blocks_fd < 0) {
-    throw std::invalid_argument("fd < 0");
+  if (!dird) {
+    std::string errctx = "Cannot open '";
+    errctx += path;
+    errctx += "'";
+    throw std::system_error(errno, std::generic_category(), errctx);
   }
 
-  aligned = decltype(aligned){read_only, aligned_fd};
-  unaligned = decltype(unaligned){read_only, unaligned_fd};
-  records = decltype(records){read_only, records_fd};
-  blocks = decltype(blocks){read_only, blocks_fd};
+  config def{blocksize};
+
+  auto data = def.serialize();
+
+  raii_fd conf_fd = openat(dird.fileno(), "config", flags, creation_mode);
+  if (!conf_fd) {
+    std::string errctx = "Cannot open '";
+    errctx += path;
+    errctx += "/config'";
+    throw std::system_error(errno, std::generic_category(), errctx);
+  }
+  WriteFile(conf_fd.fileno(), data);
+
+  if (raii_fd block_fd = openat(dird.fileno(), def.blockfile().relpath.c_str(),
+                                flags, creation_mode);
+      !block_fd) {
+    std::string errctx = "Cannot open '";
+    errctx += path;
+    errctx += "/";
+    errctx += def.blockfile().relpath;
+    errctx += "'";
+    throw std::system_error(errno, std::generic_category(), errctx);
+  }
+  if (raii_fd record_fd
+      = openat(dird.fileno(), def.recordfile().relpath.c_str(), flags,
+               creation_mode);
+      !record_fd) {
+    std::string errctx = "Cannot open '";
+    errctx += path;
+    errctx += "/";
+    errctx += def.recordfile().relpath;
+    errctx += "'";
+    throw std::system_error(errno, std::generic_category(), errctx);
+  }
+  for (auto& dfile : def.datafiles()) {
+    if (raii_fd block_fd
+        = openat(dird.fileno(), dfile.relpath.c_str(), flags, creation_mode);
+        !block_fd) {
+      std::string errctx = "Cannot open '";
+      errctx += path;
+      errctx += "/";
+      errctx += dfile.relpath;
+      errctx += "'";
+      throw std::system_error(errno, std::generic_category(), errctx);
+    }
+  }
 }
 
+namespace {
+template <typename T> using net = network_order::network<T>;
 
+struct net_string {
+  net<std::uint32_t> Start;
+  net<std::uint32_t> Size;
+  net_string() = default;
+  net_string(std::vector<char>& string_area,
+             const char* data,
+             std::uint32_t size)
+      : Start{(uint32_t)string_area.size()}, Size{size}
+  {
+    string_area.insert(string_area.end(), data, data + size);
+  }
+  std::string unserialize(std::string_view string_area)
+  {
+    if (string_area.size() < std::size_t{Start} + Size) {
+      throw std::runtime_error("string area too small (size="
+                               + std::to_string(string_area.size())
+                               + ", want= [" + std::to_string(Start) + ", "
+                               + std::to_string(Start + Size) + "])");
+    }
+    return std::string{string_area.substr(Start, Size)};
+  }
+};
+
+struct serializable_block_file {
+  net_string RelPath;
+  net<decltype(config::block_file::Start)> Start;
+  net<decltype(config::block_file::End)> End;
+  net<decltype(config::block_file::Idx)> Idx;
+
+  serializable_block_file() = default;
+  serializable_block_file(const config::block_file& bf,
+                          std::vector<char>& string_area)
+      : RelPath(string_area, bf.relpath.data(), bf.relpath.size())
+      , Start{bf.Start}
+      , End{bf.End}
+      , Idx{bf.Idx}
+  {
+  }
+
+  config::block_file unserialize(std::string_view string_area)
+  {
+    return {RelPath.unserialize(string_area), Start, End, Idx};
+  }
+};
+struct serializable_record_file {
+  net_string RelPath;
+  net<decltype(config::record_file::Start)> Start;
+  net<decltype(config::record_file::End)> End;
+  net<decltype(config::record_file::Idx)> Idx;
+
+  serializable_record_file() = default;
+  serializable_record_file(const config::record_file& rf,
+                           std::vector<char>& string_area)
+      : RelPath(string_area, rf.relpath.data(), rf.relpath.size())
+      , Start{rf.Start}
+      , End{rf.End}
+      , Idx{rf.Idx}
+  {
+  }
+
+  config::record_file unserialize(std::string_view string_area)
+  {
+    return {RelPath.unserialize(string_area), Start, End, Idx};
+  }
+};
+struct serializable_data_file {
+  net_string RelPath;
+  net<decltype(config::data_file::Size)> Size;
+  net<decltype(config::data_file::BlockSize)> BlockSize;
+  net<decltype(config::data_file::Idx)> Idx;
+  bool ReadOnly;
+
+  serializable_data_file() = default;
+  serializable_data_file(const config::data_file& df,
+                         std::vector<char>& string_area)
+      : RelPath(string_area, df.relpath.data(), df.relpath.size())
+      , Size{df.Size}
+      , BlockSize{df.BlockSize}
+      , Idx{df.Idx}
+      , ReadOnly{df.ReadOnly}
+  {
+  }
+  config::data_file unserialize(std::string_view string_area)
+  {
+    return {
+        RelPath.unserialize(string_area), Size, BlockSize, Idx, ReadOnly,
+    };
+  }
+};
+
+struct config_header {
+  net<std::uint32_t> string_size{};
+  net<std::uint32_t> num_blockfiles{};
+  net<std::uint32_t> num_recordfiles{};
+  net<std::uint32_t> num_datafiles{};
+};
+};  // namespace
+
+
+std::vector<char> config::serialize()
+{
+  std::vector<char> serial;
+
+  config_header hdr;
+
+  serializable_block_file bf{bfile, serial};
+  serializable_record_file rf{rfile, serial};
+  std::vector<serializable_data_file> dfs;
+  for (auto dfile : dfiles) { dfs.emplace_back(dfile, serial); }
+
+  hdr.string_size = serial.size();
+
+  {
+    auto* as_char = reinterpret_cast<const char*>(&bf);
+    serial.insert(serial.end(), as_char, as_char + sizeof(bf));
+    hdr.num_blockfiles = hdr.num_blockfiles + 1;
+  }
+  {
+    auto* as_char = reinterpret_cast<const char*>(&rf);
+    serial.insert(serial.end(), as_char, as_char + sizeof(rf));
+    hdr.num_recordfiles = hdr.num_recordfiles + 1;
+  }
+  for (auto& df : dfs) {
+    auto* as_char = reinterpret_cast<const char*>(&df);
+    serial.insert(serial.end(), as_char, as_char + sizeof(df));
+    hdr.num_datafiles = hdr.num_datafiles + 1;
+  }
+
+  {
+    auto* as_char = reinterpret_cast<const char*>(&hdr);
+    serial.insert(serial.begin(), as_char, as_char + sizeof(hdr));
+  }
+
+  return serial;
+}
+
+config::config(const std::vector<char>& serial)
+{
+  chunked_reader stream(serial.data(), serial.size());
+
+  config_header hdr;
+  if (!stream.read(&hdr, sizeof(hdr))) {
+    throw std::runtime_error("config file to small.");
+  }
+
+  if (hdr.num_blockfiles != 1) {
+    throw std::runtime_error("bad config file (num blockfiles != 1)");
+  }
+  if (hdr.num_recordfiles != 1) {
+    throw std::runtime_error("bad config file (num recordfiles != 1)");
+  }
+  if (hdr.num_datafiles != 2) {
+    throw std::runtime_error("bad config file (num datafiles != 2)");
+  }
+
+  const char* string_begin = stream.get(hdr.string_size);
+  if (!string_begin) { throw std::runtime_error("config file to small."); }
+  std::string_view string_area(string_begin, hdr.string_size);
+
+  {
+    serializable_block_file bf;
+    if (!stream.read(&bf, sizeof(bf))) {
+      throw std::runtime_error("config file to small.");
+    }
+
+    bfile = bf.unserialize(string_area);
+  }
+  {
+    serializable_record_file rf;
+    if (!stream.read(&rf, sizeof(rf))) {
+      throw std::runtime_error("config file to small.");
+    }
+
+    rfile = rf.unserialize(string_area);
+  }
+
+  for (std::size_t i = 0; i < hdr.num_datafiles; ++i) {
+    serializable_data_file df;
+    if (!stream.read(&df, sizeof(df))) {
+      throw std::runtime_error("config file to small.");
+    }
+
+    dfiles.push_back(df.unserialize(string_area));
+  }
+
+  if (!stream.finished()) { throw std::runtime_error("config file to big."); }
+}
 };  // namespace dedup
