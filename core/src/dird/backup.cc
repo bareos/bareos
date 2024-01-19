e8f5a017b1ae36133786f5ea640002b50fc5d755
Sebastian Sura <sebastian.sura@bareos.com>
2024-01-19 08:09:41 +0100

    backup: fix printing bad jobid
    jobids are 32bit, but are printed as 64bit -> bad things happens on
    windows.
    
    (cherry picked from commit 78b41049dfcaac67429242d1fcf59873a3d6eec0)

--- core/src/dird/backup.cc
+++ core/src/dird/backup.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -475,7 +475,7 @@ static bool ConfigureMessageThread(JobControlRecord* jcr)
  */
 bool DoNativeBackup(JobControlRecord* jcr)
 {
-  Jmsg(jcr, M_INFO, 0, T_("Start Backup JobId %llu, Job=%s\n"), jcr->JobId,
+  Jmsg(jcr, M_INFO, 0, T_("Start Backup JobId %u, Job=%s\n"), jcr->JobId,
        jcr->Job);
 
   jcr->setJobStatusWithPriorityCheck(JS_Running);
