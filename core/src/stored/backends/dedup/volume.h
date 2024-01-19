a5faf1f67fe1f906ea06174452989d4775f85301
Sebastian Sura <sebastian.sura@bareos.com>
2024-01-19 10:24:30 +0100

    dedup-volume: fix CommitBlock/AbortBlock error
    It was previously not possible to abort once a commit was
    started (since the savestate was moved immediately on the CommitBlock
    call).  This was fixed by instead waiting for the commit to finish
    before issueing the move.
    
    (cherry picked from commit 0fe8762f110119dd1291a23ccf03f90001fb3d18)

--- core/src/stored/backends/dedup/volume.h
+++ core/src/stored/backends/dedup/volume.h
@@ -190,7 +190,7 @@ class volume {
 
   // writing interface
   save_state BeginBlock(block_header header);
-  void CommitBlock(save_state save);
+  void CommitBlock(save_state&& save);
   void AbortBlock(save_state save);
   void PushRecord(record_header header, const char* data, std::size_t size);
 
