c6536d2d473cd9bb3fcf1157ce1ca30bb7998962
Sebastian Sura <sebastian.sura@bareos.com>
2024-02-07 14:12:25 +0100

    mac: fix reservation problems

--- core/src/stored/mac.cc
+++ core/src/stored/mac.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2006-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -436,6 +436,7 @@ bool DoMacRun(JobControlRecord* jcr)
   bool ok = true;
   bool acquire_fail = false;
   BareosSocket* dir = jcr->dir_bsock;
+  if (!jcr->sd_impl->dcr) { TryReserveAfterUse(jcr, true); }
   Device* dev = jcr->sd_impl->dcr->dev;
 
   switch (jcr->getJobType()) {
