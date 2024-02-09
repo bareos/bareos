250e3e5ffb5c9370acf2d275037ce6622ccdc106
Andreas Rogge <andreas.rogge@bareos.com>
2024-02-09 16:32:44 +0100

    cmake: add DebugEdit module
    by using the DebugEdit module we add a POST_BUILD command to each CMake
    target that generates ELF binaries if we have debugedit available.
    This command will basically undo the job of -fdebug-prefix-map.
    The idea here is that we want to have all object files with canonical
    paths in ccache, but the resulting binaries should have the
    non-canonical path embedded so debuginfo/debugsource generation will
    work correctly.

--- cmake/DebugEdit.cmake
+++ cmake/DebugEdit.cmake
@@ -1,6 +1,6 @@
 #   BAREOS® - Backup Archiving REcovery Open Sourced
 #
-#   Copyright (C) 2017-2024 Bareos GmbH & Co. KG
+#   Copyright (C) 2024-2024 Bareos GmbH & Co. KG
 #
 #   This program is Free Software; you can redistribute it and/or
 #   modify it under the terms of version three of the GNU Affero General Public
@@ -16,24 +16,30 @@
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #   02110-1301, USA.
-message("Entering ${CMAKE_CURRENT_SOURCE_DIR}")
 
-set(TEST_SRC # drivetype_test.cc
-    # fstype_test.cc
+find_program(DEBUGEDIT_PROG debugedit HINTS /usr/lib/rpm)
+
+option(DEBUGEDIT_REWRITE
+       "Rewrite DW_AT_comp_dir in ELF objects to the real path" ON
 )
 
-add_executable(test_findlib ${TEST_SRC})
-target_link_libraries(test_findlib PRIVATE bareosfind bareos GTest::gtest_main)
-
-add_test(NAME test_findlib COMMAND "test_findlib")
-set_property(
-  TEST test_findlib
-  PROPERTY
-    ENVIRONMENT
-    "LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}/src/lib:${CMAKE_BINARY_DIR}/src/findlib"
-  PROPERTY ENVIRONMENT "GTEST_COLOR=yes"
+if(DEBUGEDIT_REWRITE
+   AND DEBUGEDIT_PROG
+   AND NOT HAVE_WIN32
 )
-
-add_dependencies(check test_findlib)
-
-include(DebugEdit)
+  get_directory_property(directory_targets BUILDSYSTEM_TARGETS)
+  foreach(target ${directory_targets})
+    get_target_property(target_type ${target} TYPE)
+    if(target_type STREQUAL EXECUTABLE
+       OR target_type STREQUAL SHARED_LIBRARY
+       OR target_type STREQUAL MODULE_LIBRARY
+    )
+      add_custom_command(
+        TARGET ${target}
+        POST_BUILD
+        COMMAND ${DEBUGEDIT_PROG} -b /usr/src/bareos -d ${CMAKE_SOURCE_DIR}
+                $<TARGET_FILE:${target}>
+      )
+    endif()
+  endforeach()
+endif()
