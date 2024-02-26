df51679a6533c98e6469f882af4762728d35a7ad
Sebastian Sura <sebastian.sura@bareos.com>
2024-02-26 11:56:54 +0100

    ansi-label: fix printing wrong error message
    (cherry picked from commit 37d518fda276048b04d0c55417b1e0bfcf58019b)

--- core/src/stored/ansi_label.cc
+++ core/src/stored/ansi_label.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2005-2009 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -64,10 +64,8 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
   char* VolName = dcr->VolumeName;
   bool ok = false;
 
-  /*
-   * Read VOL1, HDR1, HDR2 labels, but ignore the data
-   * If tape read the following EOF mark, on disk do not read.
-   */
+  /* Read VOL1, HDR1, HDR2 labels, but ignore the data
+   * If tape read the following EOF mark, on disk do not read. */
   Dmsg0(100, "Read ansi label.\n");
   if (!dev->IsTape()) { return VOL_OK; }
 
@@ -85,7 +83,7 @@ int ReadAnsiIbmLabel(DeviceControlRecord* dcr)
       Dmsg1(100, "Read device got: ERR=%s\n", be.bstrerror());
       Mmsg2(jcr->errmsg, _("Read error on device %s in ANSI label. ERR=%s\n"),
             dev->archive_device_string, be.bstrerror());
-      Jmsg(jcr, M_ERROR, 0, "%s", dev->errmsg);
+      Jmsg(jcr, M_ERROR, 0, "%s", jcr->errmsg);
       dev->VolCatInfo.VolCatErrors++;
       return VOL_IO_ERROR;
     }
@@ -300,10 +298,8 @@ bool WriteAnsiIbmLabels(DeviceControlRecord* dcr, int type, const char* VolName)
   time_t now;
   int len, status, label_type;
 
-  /*
-   * If the Device requires a specific label type use it,
-   * otherwise, use the type requested by the Director
-   */
+  /* If the Device requires a specific label type use it,
+   * otherwise, use the type requested by the Director */
   if (dcr->device_resource->label_type != B_BAREOS_LABEL) {
     label_type = dcr->device_resource->label_type; /* force label type */
   } else {
@@ -325,10 +321,8 @@ bool WriteAnsiIbmLabels(DeviceControlRecord* dcr, int type, const char* VolName)
         return false;
       }
 
-      /*
-       * ANSI labels have 6 characters, and are padded with spaces 'vol1\0' =>
-       * 'vol1   \0'
-       */
+      /* ANSI labels have 6 characters, and are padded with spaces 'vol1\0' =>
+       * 'vol1   \0' */
       strcpy(ansi_volname, VolName);
       for (int i = len; i < 6; i++) { ansi_volname[i] = ' '; }
       ansi_volname[6] = '\0'; /* only for debug */
