11bbbee978e99b2dcf5ee39cd6dc5f8f39724282
Sebastian Sura <sebastian.sura@bareos.com>
2024-03-18 14:33:05 +0100

    fvec: fix not initialising bytes_allocated
    (cherry picked from commit 205d4e321bbbd530a2e33ed6ce72b7d8532be9dd)

--- core/src/stored/backends/dedup/fvec.h
+++ core/src/stored/backends/dedup/fvec.h
@@ -349,8 +349,9 @@ template <typename T> class fvec : access {
       // this should not happen
       throw std::runtime_error("mmap returned nullptr.");
     }
+    bytes_allocated = filesize;
 #ifdef MADV_HUGEPAGE
-    madvise(buffer, filesize, MADV_HUGEPAGE);
+    madvise(buffer, bytes_allocated, MADV_HUGEPAGE);
 #endif
   }
 };
