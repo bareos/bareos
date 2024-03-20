2869fa68afe766e79e6248f266f07977c201431c
Sebastian Sura <sebastian.sura@bareos.com>
2024-03-20 08:46:21 +0100

    sd-backend-tests: fix not initialising reservation lock
    (cherry picked from commit 7b38bd4b686671631e0ed7b425cfba455d7cc39a)

--- core/src/tests/sd_backend_tests.h
+++ core/src/tests/sd_backend_tests.h
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2022-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2022-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -36,6 +36,7 @@ void sd::SetUp()
 {
   using namespace storagedaemon;
   OSDependentInit();
+  InitReservationsLock();
 
   debug_level = 900;
 
