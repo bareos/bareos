ed9cf464ca5bcaf2f77ae61ad6892c43032df796
Joerg Steffens <joerg.steffens@bareos.com>
2024-03-27 14:59:12 +0100

    bareos-check-sources
    (cherry picked from commit 47d8d05bbc7608d45dc17bee5b007ed902923635)

--- core/src/cats/bdb_postgresql.h
+++ core/src/cats/bdb_postgresql.h
@@ -3,7 +3,7 @@
 
    Copyright (C) 2009-2011 Free Software Foundation Europe e.V.
    Copyright (C) 2016-2016 Planets Communications B.V.
-   Copyright (C) 2016-2021 Bareos GmbH & Co. KG
+   Copyright (C) 2016-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -89,7 +89,7 @@ class BareosDbPostgresql : public BareosDbPrivateInterface {
   bool SqlCopyEnd() override;
 
   bool CheckDatabaseEncoding(JobControlRecord* jcr);
-  
+
  public:
   BareosDbPostgresql(JobControlRecord* jcr,
                      const char* db_driver,
