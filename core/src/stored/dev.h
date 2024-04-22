318492e37189522b55a8d6e958f1a03bb0e19fc1
Andreas Rogge <andreas.rogge@bareos.com>
2024-04-22 15:38:35 +0200

    stored: add customization point for device setup
    This adds a new virtual function setup() to device that will be called
    when the device is created. Here the backend developer can run any
    device-specific initialization, like parsing options, checking device
    availability, etc.
    Returning false from that function will fail the device initialization.

--- core/src/stored/dev.h
+++ core/src/stored/dev.h
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -209,6 +209,7 @@ class Device {
 
  public:
   Device() = default;
+  virtual bool setup() { return true; }; // optional device-specific setup
   virtual ~Device();
   Device(const Device&) = delete;
   Device& operator=(const Device&) = delete;
