6d110fefe7d4435a1fdf48ef2afe88a67b38abd9
Andreas Rogge <andreas.rogge@bareos.com>
2024-04-24 17:09:33 +0200

    stored: dplcompat: implement backup and recycle
    This version is now able to support all Bareos backend operations.

--- core/src/stored/backends/crud_storage.cc
+++ core/src/stored/backends/crud_storage.cc
@@ -22,13 +22,28 @@ class BPipeHandle {
   }
   FILE* getReadFd() { return bpipe->rfd; }
   FILE* getWriteFd() { return bpipe->wfd; }
+  std::string getOutput()
+  {
+    close_write();
+    std::string output;
+    char iobuf[1024];
+    while (!feof(bpipe->rfd)) {
+      size_t rsize = fread(iobuf, 1, 1024, bpipe->rfd);
+      if (rsize > 0 && !ferror(bpipe->rfd)) { output += std::string(iobuf, rsize); }
+    }
+    return output;
+  }
   bool timed_out() { return bpipe->timer_id && bpipe->timer_id->killed; }
+  void close_write() {
+    ASSERT(bpipe);
+    CloseWpipe(bpipe);
+  }
   int close()
   {
     ASSERT(bpipe);
-    int ret = CloseBpipe(bpipe) &~b_errno_exit;
+    int ret = CloseBpipe(bpipe) & ~b_errno_exit;
 
-    if(ret & b_errno_signal) {
+    if (ret & b_errno_signal) {
       ret &= ~b_errno_signal;
       ret *= -1;
     }
@@ -64,17 +79,14 @@ bool CrudStorage::test_connection()
   Dmsg0(200, "test_connection called\n");
   std::string cmdline = fmt::format("'{}' testconnection", m_program);
   auto bph{BPipeHandle(cmdline.c_str(), 30, "r")};
-  { // TODO: this should probably go into BPipeHandle
-    auto rfh = bph.getReadFd();
-    std::vector<char> output{};
-    while (!feof(rfh)) {
-      size_t offset = output.size();
-      output.resize(offset + 1024);
-      (void)!fread(output.data() + offset, 1, 1024, rfh);
-    }
-  }
+  auto output = bph.getOutput();
   auto ret = bph.close();
-  Dmsg1(200, "testconnection returned %d\n", ret);
+  Dmsg1(200,
+        "testconnection returned %d\n"
+        "== Output ==\n"
+        "%s"
+        "============\n",
+        ret, output.c_str());
   return ret == 0;
 }
 
@@ -101,18 +113,68 @@ auto CrudStorage::stat(std::string_view obj_name, std::string_view obj_part)
 
 auto CrudStorage::list(std::string_view obj_name) -> std::map<std::string, Stat>
 {
-  Dmsg1(200, "list %s called\n", obj_name.data());
-  // get list of volume files
-  return {};
+  // TODO: better error handling, we should know if something actually went
+  // wrong on the way or if the list is simply empty.
+  Dmsg1(5, "list %s called\n", obj_name.data());
+  std::string cmdline = fmt::format("'{}' list '{}'", m_program, obj_name);
+  auto bph{BPipeHandle(cmdline.c_str(), 30, "r")};
+  auto rfh = bph.getReadFd();
+
+  std::map<std::string, Stat> result;
+  while (!feof(rfh)) {
+    Stat stat;
+    auto obj_part = std::string(128, '\0');
+    if (int n = fscanf(rfh, "%128s %zu\n", obj_part.data(), &stat.size);
+        n != 2) {
+      Dmsg1(200, "fscanf() returned %d\n", n);
+      return {};
+    }
+    obj_part.resize(std::strlen(obj_part.c_str()));
+    result[obj_part] = stat;
+
+    Dmsg1(5, "volume=%s part=%s size=%zu\n", obj_name.data(), obj_part.c_str(),
+          stat.size);
+  }
+
+  if (auto ret = bph.close(); ret != 0) {
+    Dmsg1(200, "list returned %d\n", ret);
+    return {};
+  }
+  return result;
 }
 
 bool CrudStorage::upload(std::string_view obj_name,
                          std::string_view obj_part,
-                         gsl::span<char> /* obj_data */)
+                         gsl::span<char> obj_data)
 {
-  Dmsg1(200, "upload %s/%s called\n", obj_name.data(), obj_part.data());
-  // upload data to somewhere
-  return true;
+  Dmsg1(5, "upload %s/%s called\n", obj_name.data(), obj_part.data());
+  std::string cmdline
+      = fmt::format("'{}' upload '{}' '{}'", m_program, obj_name, obj_part);
+
+  auto bph{BPipeHandle(cmdline.c_str(), 30, "rw")};
+  auto wfh = bph.getWriteFd();
+
+  constexpr size_t max_write_size{256 * 1024};
+  size_t remaining_bytes{obj_data.size()};
+
+  while (remaining_bytes > 0) {
+    size_t write_size = std::min(max_write_size, remaining_bytes);
+    if (write_size
+        != fwrite(obj_data.data() + (obj_data.size() - remaining_bytes), 1,
+                  write_size, wfh)) {
+      return false;
+    }
+    remaining_bytes -= write_size;
+  }
+  auto output = bph.getOutput();
+  auto ret = bph.close();
+  Dmsg1(200,
+        "upload returned %d\n"
+        "== Output ==\n"
+        "%s"
+        "============\n",
+        ret, output.c_str());
+  return ret == 0;
 }
 
 std::optional<gsl::span<char>> CrudStorage::download(std::string_view obj_name,
@@ -160,6 +222,17 @@ std::optional<gsl::span<char>> CrudStorage::download(std::string_view obj_name,
 
 bool CrudStorage::remove(std::string_view obj_name, std::string_view obj_part)
 {
-  Dmsg1(200, "remove %s/%s called\n", obj_name.data(), obj_part.data());
-  return true;
+  Dmsg1(5, "remove %s/%s called\n", obj_name.data(), obj_part.data());
+  std::string cmdline = fmt::format("'{}' remove '{}' '{}'", m_program, obj_name, obj_part);
+  auto bph{BPipeHandle(cmdline.c_str(), 30, "r")};
+  auto output = bph.getOutput();
+  auto ret = bph.close();
+
+  Dmsg1(200,
+        "remove returned %d\n"
+        "== Output ==\n"
+        "%s"
+        "============\n",
+        ret, output.c_str());
+  return ret == 0;
 }
