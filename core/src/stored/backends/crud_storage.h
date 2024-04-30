3b92aa8d418c69712b020f46541625418efe80a8
Andreas Rogge <andreas.rogge@bareos.com>
2024-04-30 13:34:09 +0200

    dplcompat: improve error-reporting with tl::expected

--- core/src/stored/backends/crud_storage.h
+++ core/src/stored/backends/crud_storage.h
@@ -4,6 +4,9 @@
 #include <unordered_map>
 #include <gsl/span>
 #include "lib/bstringlist.h"
+#include "tl/expected.hpp"
+
+static constexpr int dlvl = 10;
 
 class CrudStorage {
   struct Stat {
@@ -13,18 +16,21 @@ class CrudStorage {
   std::unordered_map<std::string, std::string> m_env_vars{};
 
  public:
-  bool set_program(const std::string& program);
-  BStringList get_supported_options();
-  bool set_option(const std::string& name, const std::string& value);
-  bool test_connection();
-  std::optional<Stat> stat(std::string_view obj_name,
-                           std::string_view obj_part);
-  std::map<std::string, Stat> list(std::string_view /* obj_name */);
-  bool upload(std::string_view obj_name,
-              std::string_view obj_part,
-              gsl::span<char> obj_data);
-  std::optional<gsl::span<char>> download(std::string_view obj_name,
-                                          std::string_view obj_part,
-                                          gsl::span<char> buffer);
-  bool remove(std::string_view obj_name, std::string_view obj_part);
+  tl::expected<void, std::string> set_program(const std::string& program);
+  tl::expected<BStringList, std::string> get_supported_options();
+  tl::expected<void, std::string> set_option(const std::string& name,
+                                             const std::string& value);
+  tl::expected<void, std::string> test_connection();
+  tl::expected<Stat, std::string> stat(std::string_view obj_name,
+                                       std::string_view obj_part);
+  tl::expected<std::map<std::string, Stat>, std::string> list(
+      std::string_view obj_name);
+  tl::expected<void, std::string> upload(std::string_view obj_name,
+                                         std::string_view obj_part,
+                                         gsl::span<char> obj_data);
+  tl::expected<gsl::span<char>, std::string> download(std::string_view obj_name,
+                                                      std::string_view obj_part,
+                                                      gsl::span<char> buffer);
+  tl::expected<void, std::string> remove(std::string_view obj_name,
+                                         std::string_view obj_part);
 };
