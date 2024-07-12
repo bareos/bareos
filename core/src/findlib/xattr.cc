9764b64c7d7b0598d122c61092d417d6d912829c
Andreas Rogge <andreas.rogge@bareos.com>
2024-07-12 11:15:02 +0200

    findlib: remove xattr_parse_data_t
    inside `XattrData` there was
    ```
    union {
      xattr_build_data_t* build;
      xattr_parse_data_t* parse;
    };
    ```
    both of these types contained a `uint32_t nr_errors` that was moved into
    `XattrData`, leaving `xattr_parse_data_t` empty, so it could be removed.

--- core/src/findlib/xattr.cc
+++ core/src/findlib/xattr.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2008-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2025 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -2812,7 +2812,7 @@ BxattrExitCode ParseXattrStreams(JobControlRecord* jcr,
   } else {
     /* Increment error count but don't log an error again for the same
      * filesystem. */
-    if (xattr_data->u.parse) xattr_data->u.parse->nr_errors++;
+    xattr_data->nr_errors++;
     retval = BxattrExitCode::kSuccess;
     goto bail_out;
   }
