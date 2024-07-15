b2aa55d2e3eb5974763f56aa1d4d42cad8867b16
Bruno Friedmann <bruno.friedmann@bareos.com>
2024-07-15 16:48:29 +0200

    fix copyright year

--- core/src/plugins/filed/python/mariabackup/bareos-fd-mariabackup.py
+++ core/src/plugins/filed/python/mariabackup/bareos-fd-mariabackup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015-2023 Bareos GmbH & Co. KG
+# Copyright (C) 2015-2024 Bareos GmbH & Co. KG
 #
 # This program is Free Software; you can redistribute it and/or
 # modify it under the terms of version three of the GNU Affero General Public
