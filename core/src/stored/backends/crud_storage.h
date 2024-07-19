210423facbcc35d25bac18afce347b5feff3bbdd
Andreas Rogge <andreas.rogge@bareos.com>
2024-07-19 14:36:33 +0200

    dplcompat: add program_timeout option
    The timeout for the program is now configurable using the option
    program_timeout.

--- core/src/stored/backends/crud_storage.h
+++ core/src/stored/backends/crud_storage.h
@@ -3,6 +3,7 @@
 #include <string_view>
 #include <unordered_map>
 #include <gsl/span>
+#include <cstdint>
 #include "lib/bstringlist.h"
 #include "tl/expected.hpp"
 
@@ -13,10 +14,12 @@ class CrudStorage {
     size_t size{0};
   };
   std::string m_program{"/bin/false"};
+  uint32_t m_program_timeout{30};
   std::unordered_map<std::string, std::string> m_env_vars{};
 
  public:
   tl::expected<void, std::string> set_program(const std::string& program);
+  void set_program_timeout(uint32_t timeout);
   tl::expected<BStringList, std::string> get_supported_options();
   tl::expected<void, std::string> set_option(const std::string& name,
                                              const std::string& value);
