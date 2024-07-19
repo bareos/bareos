3b3ae96ac5a1671c18c15da9490afac9176aa6c5
Sebastian Sura <sebastian.sura@bareos.com>
2024-07-19 07:24:41 +0200

    parser: fix not setting `rcode_' in the second pass
    (cherry picked from commit b436ef591d92e77c4c7dc8b3d7aec180a47be38f)

--- core/src/lib/parse_conf_init_resource.cc
+++ core/src/lib/parse_conf_init_resource.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -202,6 +202,8 @@ void ConfigurationParser::SetAllResourceDefaultsIterateOverItems(
       SetBit(res_item_index,
              (*items[res_item_index].allocated_resource)->inherit_content_);
     }
+
+    (*items[res_item_index].allocated_resource)->rcode_ = rcode;
 
     res_item_index++;
 
