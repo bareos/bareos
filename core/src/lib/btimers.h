88bb85e2f225ea3b8e4c6c237c7ac58afe5f65d7
Andreas Rogge <andreas.rogge@bareos.com>
2024-07-19 18:17:05 +0200

    btimers: give keepalive its proper name

--- core/src/lib/btimers.h
+++ core/src/lib/btimers.h
@@ -41,7 +41,7 @@ struct btimer_t {
   watchdog_t* wd; /**< Parent watchdog */
   int type;
   bool killed;
-  std::atomic<bool> cop; /**< child operates properly flag */
+  std::atomic<bool> keepalive;
   pid_t pid;             /**< process id if TYPE_CHILD */
   pthread_t tid;         /**< thread id if TYPE_PTHREAD */
   BareosSocket* bsock;   /**< Pointer to BareosSocket */
@@ -49,7 +49,7 @@ struct btimer_t {
 };
 
 btimer_t* StartChildTimer(JobControlRecord* jcr, pid_t pid, uint32_t wait);
-void TimerChildOperatesProperly(btimer_t& t);
+void TimerKeepalive(btimer_t& t);
 void StopChildTimer(btimer_t* wid);
 btimer_t* StartThreadTimer(JobControlRecord* jcr, pthread_t tid, uint32_t wait);
 void StopThreadTimer(btimer_t* wid);
