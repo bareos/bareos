48295071c4decdf2da9b82a5fe617955ec1462c5
Andreas Rogge <andreas.rogge@bareos.com>
2024-07-24 17:06:09 +0200

    chunked_device: flush all volumes in is_written()
    when ReleaseDevice() calls d_flush() on the volume, it should only
    return if all buffers were flushed correctly. However, as a volume
    change does not imply d_flush(), we might have chunks from a
    previously mounted volume still in the queue.
    This change ensures that not only the chunks of the current volume are
    flushed, but that the queue is completely empty.

--- core/src/stored/backends/chunked_device.cc
+++ core/src/stored/backends/chunked_device.cc
@@ -1072,6 +1072,8 @@ bool ChunkedDevice::is_written()
               current_volname_);
         return false;
       }
+      Dmsg0(100, "storage is pending, as there are queued write requests for previous volumes.\n");
+      return false;
     }
   }
 
