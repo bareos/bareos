5f8a435a9036c7ea248e183cf2e32bc8a9f8d9c3
Andreas Rogge <andreas.rogge@bareos.com>
2024-10-29 10:49:20 +0100

    cmake: downgrade tl-expected requirement to 1.0.0
    Relax the required tl-expected version to 1.0.0 so we can use the
    packaged version that Debian/Ubuntu ships.

--- cmake/BareosCpmPackages.cmake
+++ cmake/BareosCpmPackages.cmake
@@ -61,8 +61,9 @@ CPMAddPackage(
 
 CPMAddPackage(
   NAME tl-expected
-  VERSION "1.1.0"
+  VERSION "1.0.0"
   GITHUB_REPOSITORY "TartanLlama/expected"
+  GIT_TAG "v1.1.0"
   DOWNLOAD_ONLY YES
 )
 if(tl-expected_ADDED)
