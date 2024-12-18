c6f1ec962c912ea3eefe1bd3d18247097a54ae9c
Sebastian Sura <sebastian.sura@bareos.com>
2024-12-18 13:12:10 +0100

    ndmp: remove ndmjob application

--- cmake/BareosSetVariableDefaults.cmake
+++ cmake/BareosSetVariableDefaults.cmake
@@ -410,8 +410,6 @@ option(xattr "Enable extended file attributes (xattr) support" ON)
 mark_as_advanced(xattr)
 option(scsi-crypto "Enable scsi-crypto" ON)
 option(ndmp "Enable NDMP support" ON)
-option(build_ndmjob "Building ndmpjob" OFF)
-mark_as_advanced(build_ndmjob)
 
 option(traymonitor "Build bareos-traymonitor" OFF)
 
