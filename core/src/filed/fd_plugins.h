2c43b3354a8da44fe431cf2969cc1e99f2609eb6
Sebastian Sura <sebastian.sura@bareos.com>
2025-01-22 09:37:16 +0100

    fd-plugins: enable auto grpc
    When a plugin cannot be found, the grpc plugin is loaded and a
    grpc compat module is defined, then we try to load that plugin with
    that compat module.
    
    To facilitate this we must make sure that the plugin handling is able
    to modify the plugin command strings both into the plugin and out of
    the plugin as we need to add/remove the grpc prefix.
    
    We also remove the `reverse` option of `GeneratePluginEvent` as it was
    unused and would mess with the (already complicated) grpc fallback
    logic.

--- core/src/filed/fd_plugins.h
+++ core/src/filed/fd_plugins.h
@@ -3,7 +3,7 @@
 
    Copyright (C) 2007-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2025 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -271,8 +271,7 @@ void NewPlugins(JobControlRecord* jcr);
 void FreePlugins(JobControlRecord* jcr);
 bRC GeneratePluginEvent(JobControlRecord* jcr,
                         bEventType event,
-                        void* value = NULL,
-                        bool reverse = false);
+                        void* value = NULL);
 bool SendPluginName(JobControlRecord* jcr, BareosSocket* sd, bool start);
 bool PluginNameStream(JobControlRecord* jcr, char* name);
 int PluginCreateFile(JobControlRecord* jcr,
