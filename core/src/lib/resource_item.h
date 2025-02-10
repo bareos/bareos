0577836c0f939be3f28609786a7b2146514c2a5f
Sebastian Sura <sebastian.sura@bareos.com>
2025-02-10 15:21:02 +0100

    resource-item: add new way to specify resource items

--- core/src/lib/resource_item.h
+++ core/src/lib/resource_item.h
@@ -26,10 +26,99 @@
 
 #include <vector>
 #include <string>
+#include <optional>
+#include <utility>
+
+#include "lib/parse_conf.h"
 
 struct s_password;
 template <typename T> class alist;
 template <typename T> class dlist;
+
+namespace config {
+struct DefaultValue {
+  const char* value;
+};
+
+struct Version {
+  size_t major, minor, patch;
+};
+
+struct DeprecatedSince {
+  Version version;
+};
+
+struct IntroducedIn {
+  Version version;
+};
+
+struct Code {
+  size_t value;
+};
+
+struct Required {};
+};  // namespace config
+
+
+template <typename What, typename... Args> struct occurances {
+  static constexpr size_t value{(std::is_same_v<What, Args> + ...)};
+};
+
+template <typename What, typename... Args> struct is_present {
+  static constexpr bool value{occurances<What, Args...>::value > 0};
+};
+
+
+template <typename T, typename... Ts> T* get_if(std::tuple<Ts...>& tuple)
+{
+  if constexpr (is_present<T, Ts...>::value) {
+    return &std::get<T>(tuple);
+  } else {
+    return nullptr;
+  }
+}
+
+struct ResourceItemFlags {
+  std::optional<config::Version> introduced_in{};
+  std::optional<config::Version> deprecated_since{};
+  const char* default_value{};
+  std::optional<size_t> extra{};
+  bool required{};
+
+  template <typename... Types> ResourceItemFlags(Types&&... values)
+  {
+    static_assert((is_present<Types, config::DefaultValue, config::IntroducedIn,
+                              config::DeprecatedSince, config::Code,
+                              config::Required>::value
+                   && ...),
+                  "only allowed flags may be used");
+
+    static_assert(occurances<config::DefaultValue, Types...>::value <= 1,
+                  "flag may only be specified once");
+    static_assert(occurances<config::IntroducedIn, Types...>::value <= 1,
+                  "flag may only be specified once");
+    static_assert(occurances<config::DeprecatedSince, Types...>::value <= 1,
+                  "flag may only be specified once");
+    static_assert(occurances<config::Code, Types...>::value <= 1,
+                  "flag may only be specified once");
+    static_assert(occurances<config::Required, Types...>::value <= 1,
+                  "flag may only be specified once");
+
+    std::tuple<Types...> tup{std::forward<Types>(values)...};
+
+    if (auto* defval = get_if<config::DefaultValue>(tup)) {
+      default_value = defval->value;
+    }
+    if (auto* introduced = get_if<config::IntroducedIn>(tup)) {
+      introduced_in = introduced->version;
+    }
+    if (auto* deprecated = get_if<config::DeprecatedSince>(tup)) {
+      deprecated_since = deprecated->version;
+    }
+    if (auto* code = get_if<config::Code>(tup)) { extra = code->value; }
+    if (auto* _ = get_if<config::Required>(tup)) { required = true; }
+  }
+};
 
 /*
  * This is the structure that defines the record types (items) permitted within
@@ -40,20 +129,38 @@ struct ResourceItem {
                const int type_,
                std::size_t offset_,
                BareosResource** allocated_resource_,
+               const char* description_,
+               ResourceItemFlags&& resource_flags)
+      : name{name_}
+      , type{type_}
+      , offset{offset_}
+      , allocated_resource{allocated_resource_}
+      , description{description_}
+  {
+    if (resource_flags.deprecated_since) { flags |= CFG_ITEM_DEPRECATED; }
+    if (resource_flags.required) { flags |= CFG_ITEM_REQUIRED; }
+    default_value = resource_flags.default_value;
+    code = resource_flags.extra.value_or(0);
+  }
+
+  ResourceItem(const char* name_,
+               const int type_,
+               std::size_t offset_,
+               BareosResource** allocated_resource_,
                int32_t code_,
                uint32_t flags_,
                const char* default_value_,
                const char* versions_,
                const char* description_)
-    : name{name_}
-    , type{type_}
-    , offset{offset_}
-    , allocated_resource{allocated_resource_}
-    , code{code_}
-    , flags{flags_}
-    , default_value{default_value_}
-    , versions{versions_}
-    , description{description_}
+      : name{name_}
+      , type{type_}
+      , offset{offset_}
+      , allocated_resource{allocated_resource_}
+      , code{code_}
+      , flags{flags_}
+      , default_value{default_value_}
+      , versions{versions_}
+      , description{description_}
   {
   }
 
