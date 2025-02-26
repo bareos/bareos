4d45a0bab5ba31ba96daf9bc5361149c426be6c8
Andreas Rogge <andreas.rogge@bareos.com>
2025-02-26 21:29:27 +0100

    [WIP] dll autofixup (will probably never work properly)

--- cmake/BareosTargetTools.cmake
+++ cmake/BareosTargetTools.cmake
@@ -123,3 +123,106 @@ macro(get_all_targets_recursive targets dir)
   )
   list(APPEND ${targets} ${current_targets})
 endmacro()
+
+function(find_dll_for_lib out_var implib)
+  find_program(
+    DLLTOOL_EXECUTABLE
+    NAMES dlltool dlltool.exe
+    DOC "The path to the DLLTOOL utility"
+  )
+
+  if(NOT DLLTOOL_EXECUTABLE)
+    message(FATAL_ERROR "Required DLLTOOL to resolve DLL names not available")
+  endif()
+  execute_process(
+    COMMAND "${DLLTOOL_EXECUTABLE}" -I "${implib}"
+    OUTPUT_VARIABLE dll_name
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  cmake_path(GET implib PARENT_PATH implib_dir)
+  if(EXISTS "${implib_dir}/${dll_name}")
+    set(dll_path "${implib_dir}/${dll_name}")
+  elseif(EXISTS "${implib_dir}/../bin/${dll_name}")
+    set(dll_path "${implib_dir}/../bin/${dll_name}")
+  else()
+    message(FATAL_ERROR "No DLL for ${implib} found")
+  endif()
+  cmake_path(NORMAL_PATH dll_path)
+  set(${out_var}
+      ${dll_path}
+      PARENT_SCOPE
+  )
+endfunction()
+
+# Fixup for targets created by broken find modules.
+# This will try to find the matching .dll for the .lib or .dll.a, set up a
+# proper imported shared_library target and make that new target an interface
+# requirement of the original one.
+
+function(fixup_target_dlls target)
+  if(WIN32)
+    if(NOT TARGET "${target}")
+      message(FATAL_ERROR "Target ${target} not found.")
+    endif()
+    get_target_property(type "${target}" TYPE)
+    get_target_property(imported ${target} IMPORTED)
+    if(imported AND type STREQUAL "UNKNOWN_LIBRARY")
+      add_library(dll_fixup_${target} SHARED IMPORTED)
+      target_link_libraries(${target} INTERFACE dll_fixup_${target})
+
+      get_property(
+        loc_release_set
+        TARGET ${target}
+        PROPERTY IMPORTED_LOCATION_RELEASE
+        SET
+      )
+      if(loc_release_set)
+        get_target_property(loc_release ${target} IMPORTED_LOCATION_RELEASE)
+        find_dll_for_lib(dll_release ${loc_release})
+        set_target_properties(
+          dll_fixup_${target}
+          PROPERTIES IMPORTED_LOCATION ${dll_release}
+                     IMPORTED_IMPLIB ${loc_release}
+                     IMPORTED_LOCATION_RELEASE ${dll_release}
+                     IMPORTED_IMPLIB_RELEASE ${loc_release}
+        )
+      endif()
+
+      get_property(
+        loc_set
+        TARGET ${target}
+        PROPERTY IMPORTED_LOCATION
+        SET
+      )
+      if(loc_set)
+        get_target_property(loc ${target} IMPORTED_LOCATION)
+        find_dll_for_lib(dll ${loc})
+        set_target_properties(
+          dll_fixup_${target}
+          PROPERTIES IMPORTED_LOCATION ${dll}
+                     IMPORTED_IMPLIB ${loc}
+        )
+      endif()
+
+      get_property(
+        loc_debug_set
+        TARGET ${target}
+        PROPERTY IMPORTED_LOCATION_DEBUG
+        SET
+      )
+      if(loc_debug_set)
+        get_target_property(loc_debug ${target} IMPORTED_LOCATION_DEBUG)
+        find_dll_for_lib(dll_debug ${loc_debug})
+        set_target_properties(
+          dll_fixup_${target} PROPERTIES IMPORTED_LOCATION_DEBUG ${dll_debug}
+                                         IMPORTED_IMPLIB_DEBUG ${loc_debug}
+        )
+      endif()
+    else()
+      message(
+        FATAL_ERROR "Target ${target} with type ${type} cannot be fixed up"
+      )
+    endif()
+
+  endif()
+endfunction()
