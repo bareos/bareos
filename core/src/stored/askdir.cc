97169138d5e55d2c37d622f8de156f776aca4e86
Florian Burger <florian.burger@bareos.com>
2025-03-12 09:01:11 +0100

    stored: fix manual "Unlabeled" volume status
    If you manually set the volume status of a volume to "Unlabeled", the
    sd corrects this status after successfully writing to the volume.

--- core/src/stored/askdir.cc
+++ core/src/stored/askdir.cc
@@ -302,6 +302,9 @@ bool StorageDaemonDeviceControlRecord::DirUpdateVolumeInfo(
   if (label == is_labeloperation::True) {
     bstrncpy(vol->VolCatStatus, "Append", sizeof(vol->VolCatStatus));
   }
+  else if (bstrncmp(vol->VolCatStatus, "Unlabeled", 10) && vol->VolCatBytes > 0) {
+    bstrncpy(vol->VolCatStatus, "Append", sizeof(vol->VolCatStatus));
+  }
   vol->VolLastWritten = time(NULL);
   PmStrcpy(volume_name, vol->VolCatName);
   BashSpaces(volume_name);
