73282af0119189d1af4d3289680e08a6b2f2c299
Florian Burger <florian.burger@bareos.com>
2025-03-24 08:59:35 +0100

    refactor: apply bareos-check-sources

--- core/src/dird/ua_label.cc
+++ core/src/dird/ua_label.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2003-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2025 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -80,7 +80,7 @@ static inline bool update_database(UaContext* ua,
     if (mr->VolBytes > 0 && bstrcmp(mr->VolStatus, "Unlabeled")) {
       bstrncpy(mr->VolStatus, "Append", sizeof(mr->VolStatus));
     }
-    
+
     if (ua->db->CreateMediaRecord(ua->jcr, mr)) {
       ua->InfoMsg(T_("Catalog record for Volume \"%s\", Slot %hd successfully "
                      "created.\n"),
