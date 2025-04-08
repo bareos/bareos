e12ebfab09088b81b1df41479f23e82c9ea327df
Sebastian Sura <sebastian.sura@bareos.com>
2025-04-08 14:56:49 +0200

    cli-test: fix test on freebsd

--- core/src/tests/cli_test.cc
+++ core/src/tests/cli_test.cc
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2022-2024 Bareos GmbH & Co. KG
+   Copyright (C) 2022-2025 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -22,18 +22,25 @@
 #if defined(HAVE_MINGW)
 #  include "include/bareos.h"
 #  include "gtest/gtest.h"
+#  include "gmock/gmock.h"
 #else
 #  include "gtest/gtest.h"
+#  include "gmock/gmock.h"
 #  include "include/bareos.h"
 #endif
 
 #include "lib/cli.h"
 
 namespace {
-std::string&& rtrim(std::string&& s, const char* t = " \t\n\r\f\v")
+std::string rtrim(std::string s, const char* whitespace = " \t\n\r\f\v")
 {
-  s.erase(s.find_last_not_of(t) + 1);
-  return std::move(s);
+  s.erase(s.find_last_not_of(whitespace) + 1);
+  return s;
+}
+std::string trim(std::string s, const char* whitespace = " \t\n\r\f\v")
+{
+  return s.erase(s.find_last_not_of(whitespace) + 1)
+      .erase(0, s.find_first_not_of(whitespace));
 }
 }  // namespace
 
@@ -41,6 +48,13 @@ TEST(CLI, HelpMessageDisplaysWithCorrectFormat)
 {
   CLI::App app;
   InitCLIApp(app, "test app");
+  auto default_help = app.help();
+
+  // check that the help is included
+  EXPECT_THAT(default_help, testing::HasSubstr("-h,-?,--help"));
+  EXPECT_THAT(default_help,
+              testing::HasSubstr("Print this help message and exit."));
+
   std::string random_default{"random default"};
   std::string random_option_text{"a random option."};
   bool random_flag;
@@ -54,15 +68,7 @@ TEST(CLI, HelpMessageDisplaysWithCorrectFormat)
       ->expected(5);
 
 
-  /* clang-format off */
   std::string expected_help{
-      "test app\n"
-      "Usage: [OPTIONS]\n\n"
-      "Options:\n"
-      "    -h,-?,--help\n"
-      "        Print this help message and exit. \n\n"
-      "    --version\n"
-      "        Display program version information and exit \n\n"
       "    -x\n"
       "        Excludes: -y\n"
       "        "+random_option_text+" \n\n"
@@ -71,9 +77,23 @@ TEST(CLI, HelpMessageDisplaysWithCorrectFormat)
       "        REQUIRED\n"
       "        Needs: -x\n"
       "        Excludes: -x\n"
-      "        "+random_option_text};
-  /* clang-format on */
+      "        "+random_option_text
+  };
+
+  auto help = app.help();
+
+  ASSERT_GE(help.size(), default_help.size());
+  ASSERT_EQ(rtrim(help.substr(0, default_help.size())), rtrim(default_help));
+
+  auto new_help = help.substr(default_help.size());
 
   EXPECT_STREQ(app.get_description().c_str(), "test app");
-  EXPECT_STREQ(rtrim(app.help()).c_str(), expected_help.c_str());
+
+
+  // cli output is not very stable
+  // we try to make this test more stable by not required an exact match
+  // regarding trailing & preceeding whitespace
+  EXPECT_EQ(trim(new_help), trim(expected_help)) << new_help << "\n---\n"
+                                                 << help << "\n---\n"
+                                                 << default_help << "\n---\n";
 }
