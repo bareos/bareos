d73282d592276ed8f0db45c1143fcb86d13fc9a9
Florian Burger <florian.burger@bareos.com>
2025-04-14 10:05:43 +0200

    scheduler: add hourly support

--- core/src/dird/schedule.cc
+++ core/src/dird/schedule.cc
@@ -27,65 +27,77 @@ namespace directordaemon {
 
 // (constructor)
 Schedule::Schedule()
-   : day_mask(std::make_tuple(All<WeekOfYear>(), All<DayOfWeek>())), times({ { TimeOfDay(0, 0) } }) {}
-Schedule::Schedule(Mask<MonthOfYear> month_of_year, Mask<WeekOfMonth> week_of_month, Mask<DayOfWeek> day_of_week, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(month_of_year, week_of_month, day_of_week)), times(time) {}
-Schedule::Schedule(Mask<MonthOfYear> month_of_year, Mask<DayOfWeek> day_of_week, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(month_of_year, All<WeekOfMonth>(), day_of_week)), times(time) {}
-Schedule::Schedule(Mask<MonthOfYear> month_of_year, Mask<DayOfMonth> day_of_month, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(month_of_year, day_of_month)), times(time) {}
-Schedule::Schedule(Mask<WeekOfYear> week_of_year, Mask<DayOfWeek> day_of_week, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(week_of_year, day_of_week)), times(time) {}
-Schedule::Schedule(Mask<WeekOfMonth> week_of_month, Mask<DayOfWeek> day_of_week, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(All<MonthOfYear>(), week_of_month, day_of_week)), times(time) {}
-Schedule::Schedule(Mask<DayOfMonth> day_of_month, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(All<MonthOfYear>(), day_of_month)), times(time) {}
-Schedule::Schedule(Mask<DayOfWeek> day_of_week, List<TimeOfDay> time)
-   : day_mask(std::make_tuple(All<WeekOfYear>(), day_of_week)), times(time) {}
-Schedule::Schedule(List<TimeOfDay> time)
-   : day_mask(std::make_tuple(All<WeekOfYear>(), All<DayOfWeek>())), times(time) {}
+  : day_mask(std::make_tuple(All<WeekOfYear>(), All<DayOfWeek>())), times(List<TimeOfDay>({ { TimeOfDay(0, 0) } })) {}
+Schedule::Schedule(Mask<MonthOfYear> month_of_year, Mask<WeekOfMonth> week_of_month, Mask<DayOfWeek> day_of_week, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(month_of_year, week_of_month, day_of_week)), times(time) {}
+Schedule::Schedule(Mask<MonthOfYear> month_of_year, Mask<DayOfWeek> day_of_week, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(month_of_year, All<WeekOfMonth>(), day_of_week)), times(time) {}
+Schedule::Schedule(Mask<MonthOfYear> month_of_year, Mask<DayOfMonth> day_of_month, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(month_of_year, day_of_month)), times(time) {}
+Schedule::Schedule(Mask<WeekOfYear> week_of_year, Mask<DayOfWeek> day_of_week, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(week_of_year, day_of_week)), times(time) {}
+Schedule::Schedule(Mask<WeekOfMonth> week_of_month, Mask<DayOfWeek> day_of_week, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(All<MonthOfYear>(), week_of_month, day_of_week)), times(time) {}
+Schedule::Schedule(Mask<DayOfMonth> day_of_month, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(All<MonthOfYear>(), day_of_month)), times(time) {}
+Schedule::Schedule(Mask<DayOfWeek> day_of_week, std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(All<WeekOfYear>(), day_of_week)), times(time) {}
+Schedule::Schedule(std::variant<List<TimeOfDay>, Hourly> time)
+  : day_mask(std::make_tuple(All<WeekOfYear>(), All<DayOfWeek>())), times(time) {}
 
 // TriggersOnDay
 bool Schedule::TriggersOnDay(DateTime date_time) const {
-   if (day_mask.index() == 0) {
-      const auto& value = std::get<0>(day_mask);
-      bool matches_month = Contains(std::get<0>(value), MonthOfYear(date_time.month));
-      bool matches_week = Contains(std::get<1>(value), WeekOfMonth(date_time.week_of_month));
-      matches_week |= Contains(std::get<1>(value), WeekOfMonth::kLast) && date_time.OnLast7DaysOfMonth();
-      bool matches_day = Contains(std::get<2>(value), DayOfWeek(date_time.day_of_week));
-      return matches_month && matches_week && matches_day;
-   }
-   else if (day_mask.index() == 1) {
-      const auto& value = std::get<1>(day_mask);
-      bool matches_month = Contains(std::get<0>(value), MonthOfYear(date_time.month));
-      bool matches_day = Contains(std::get<1>(value), DayOfMonth(date_time.day_of_month));
-      return matches_month && matches_day;
-   }
-   else if (day_mask.index() == 2) {
-      const auto& value = std::get<2>(day_mask);
-      bool matches_week = Contains(std::get<0>(value), WeekOfYear(date_time.week_of_year));
-      bool matches_day = Contains(std::get<1>(value), DayOfWeek(date_time.day_of_week));
-      return matches_week && matches_day;
-   }
-   return false;
+  if (day_mask.index() == 0) {
+    const auto& value = std::get<0>(day_mask);
+    bool matches_month = Contains(std::get<0>(value), MonthOfYear(date_time.month));
+    bool matches_week = Contains(std::get<1>(value), WeekOfMonth(date_time.week_of_month));
+    matches_week |= Contains(std::get<1>(value), WeekOfMonth::kLast) && date_time.OnLast7DaysOfMonth();
+    bool matches_day = Contains(std::get<2>(value), DayOfWeek(date_time.day_of_week));
+    return matches_month && matches_week && matches_day;
+  }
+  else if (day_mask.index() == 1) {
+    const auto& value = std::get<1>(day_mask);
+    bool matches_month = Contains(std::get<0>(value), MonthOfYear(date_time.month));
+    bool matches_day = Contains(std::get<1>(value), DayOfMonth(date_time.day_of_month));
+    return matches_month && matches_day;
+  }
+  else if (day_mask.index() == 2) {
+    const auto& value = std::get<2>(day_mask);
+    bool matches_week = Contains(std::get<0>(value), WeekOfYear(date_time.week_of_year));
+    bool matches_day = Contains(std::get<1>(value), DayOfWeek(date_time.day_of_week));
+    return matches_week && matches_day;
+  }
+  return false;
 }
 // GetMatchingTimes
 std::vector<time_t> Schedule::GetMatchingTimes(time_t from, time_t to) const {
-   std::vector<time_t> list;
-   for (time_t day = from; day < to + 60 * 60 * 24; day += 60 * 60 * 24) {
-   DateTime date_time(day);
-   if (TriggersOnDay(date_time)) {
-      for (const auto& time : times.items) {
-         date_time.hour = time.hour;
-         date_time.minute = time.minute;
-         time_t runtime = date_time.GetTime();
-         if (from <= runtime && runtime <= to) {
-         list.push_back(runtime);
-         }
+  std::vector<time_t> list;
+  for (time_t day = from; day < to + 60 * 60 * 24; day += 60 * 60 * 24) {
+    DateTime date_time(day);
+    if (TriggersOnDay(date_time)) {
+      if (std::holds_alternative<Hourly>(times)) {
+        for (int hour = 0; hour < 24; ++hour) {
+          date_time.hour = hour;
+          date_time.minute = 0;
+          time_t runtime = date_time.GetTime();
+          if (from <= runtime && runtime <= to) {
+            list.push_back(runtime);
+          }
+        }
       }
-   }
-   }
-   return list;
+      else {
+        for (const auto& time : std::get<List<TimeOfDay>>(times).items) {
+          date_time.hour = time.hour;
+          date_time.minute = time.minute;
+          time_t runtime = date_time.GetTime();
+          if (from <= runtime && runtime <= to) {
+            list.push_back(runtime);
+          }
+        }
+      }
+    }
+  }
+  return list;
 }
    
 } // namespace directordaemon
