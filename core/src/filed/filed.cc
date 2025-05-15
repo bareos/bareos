0dec3cd9b73959a05bdb28470627848fd633b0bd
Sebastian Sura <sebastian.sura@bareos.com>
2025-05-15 13:02:49 +0200

    add crash

--- core/src/filed/filed.cc
+++ core/src/filed/filed.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2010 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2025 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -238,6 +238,9 @@ int main(int argc, char* argv[])
 
   // if configured, start threads and connect to Director.
   StartConnectToDirectorThreads();
+
+  volatile char* ptr = nullptr;
+  *ptr = 0;
 
   // start socket server to listen for new connections.
   StartSocketServer(me->FDaddrs);
