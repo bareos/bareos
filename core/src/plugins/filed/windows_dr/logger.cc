1c5c77ea5f75891381d0a3d77ca9a63b9a12dc27
Sebastian Sura <sebastian.sura@bareos.com>
2025-06-26 07:06:51 +0200

    [fixup] logger: actually include logger

---
+++ core/src/plugins/filed/windows_dr/logger.cc
@@ -0,0 +1,186 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2025-2025 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include "logger.h"
+#include <cstdint>
+#include "fmt/base.h"
+#include "indicators/setting.hpp"
+
+#include <indicators/progress_bar.hpp>
+// #include <indicators/dynamic_progress.hpp>
+#include <indicators/cursor_control.hpp>
+#include <stack>
+
+
+// idea:
+// [ ... ] disk x/y
+// [ ... <=> ... ] making part table
+// [====> ...] extracting disk data
+
+
+namespace progressbar {
+using namespace indicators;
+
+struct logger : public GenericLogger {
+  enum class Output {
+    StdOut,
+    StdErr,
+  };
+
+  static constexpr Output Current = Output::StdErr;
+
+  static constexpr std::ostream& stream() {
+    if constexpr (Current == Output::StdOut) {
+      return std::cout;
+    } else if constexpr (Current == Output::StdErr) {
+      return std::cerr;
+    }
+  }
+
+  static FILE* handle() {
+    if constexpr (Current == Output::StdOut) {
+      return stdout;
+    } else if constexpr (Current == Output::StdErr) {
+      return stderr;
+    }
+  }
+
+  static void erase_line()
+  {
+    std::fputs("\r\033[K", handle());
+  }
+
+  static void show_cursor(bool show)
+  {
+    std::fputs(show ? "\033[?25h" : "\033[?25l", handle());
+  }
+
+  void Begin(std::size_t FileSize) override
+  {
+    progress_bar.emplace(FileSize);
+  }
+  void Progressed(std::size_t Amount) override
+  {
+    if (progress_bar) { progress_bar->progress(Amount); }
+  }
+  void End() override
+  {
+    if (!progress_bar) { return; }
+
+    progress_bar.reset();
+  }
+
+  void SetStatus(std::string_view status) override
+  {
+    if (!progress_bar) { return; }
+    progress_bar->bar.set_option(option::PostfixText(status));
+  }
+
+  void Info(std::string_view Message) override
+  {
+    if (progress_bar) {
+      stream() << termcolor::reset;
+      erase_line();
+      stream().flush();
+    }
+    fmt::println(stderr, "{}", Message);
+    if (progress_bar) {
+      progress_bar->print();
+    }
+  }
+
+  struct progress_bar {
+    std::size_t goal{0};
+    std::size_t current{0};
+
+    ProgressBar bar;
+
+    progress_bar() = default;
+    progress_bar(std::size_t goal_)
+        : goal{goal_}
+        , bar{
+      option::BarWidth{50}, option::Start{"["}, option::Fill{"="},
+          option::Lead{">"}, option::Remainder{" "}, option::End{"]"},
+          option::ForegroundColor{Color::green}, option::ShowPercentage{true},
+          option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
+          option::MaxProgress(goal_),
+          option::ShowPercentage(true),
+
+          // using anything but cout is not really supported ...
+          option::Stream(stream()),
+
+          option::ShowElapsedTime(true),
+          option::ShowRemainingTime(true),
+          }
+    {
+      show_cursor(false);
+      cursor_hidden = true;
+    }
+    progress_bar(progress_bar const&) = delete;
+    progress_bar(progress_bar &&) = delete;
+    progress_bar& operator=(progress_bar const&) = delete;
+    progress_bar& operator=(progress_bar &&) = delete;
+
+    void progress(std::size_t amount)
+    {
+      if (!bar.is_completed()) {
+        current += amount;
+        bar.set_progress(current);
+      }
+    }
+
+    void print()
+    {
+      if (!bar.is_completed()) {
+        bar.print_progress();
+      }
+    }
+
+    ~progress_bar() {
+      if (!bar.is_completed()) {
+        bar.mark_as_completed();
+      }
+      if (cursor_hidden) {
+        show_cursor(true);
+      }
+    }
+
+    bool cursor_hidden{};
+  };
+
+
+  logger()
+  {
+    // indicators::show_console_cursor(false);
+  }
+  ~logger()
+  {
+    // indicators::show_console_cursor(true);
+  }
+  std::optional<progress_bar> progress_bar;
+};
+
+GenericLogger* get()
+{
+  static logger Instance;
+  return std::addressof(Instance);
+}
+};  // namespace progressbar
