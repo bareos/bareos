f9ef146ed50897a818eb8399be47ca24fe147168
Sebastian Sura <sebastian.sura@bareos.com>
2025-07-04 11:15:05 +0200

    [start] linux plugin

---
+++ core/src/plugins/filed/windows_dr/plugin_linux.cc
@@ -0,0 +1,286 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2025-2025 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include "include/bareos.h"
+#include "filed/fd_plugins.h"
+#include "bareos_api.h"
+#include "include/filetypes.h"
+#include "parser.h"
+
+#define err_msg(ctx, ...) JobLog((ctx), M_ERROR, __VA_ARGS__)
+
+bool AmICompatibleWith(filedaemon::PluginApiDefinition* core_info)
+{
+  if (core_info->size != sizeof(*core_info)
+      || core_info->version != FD_PLUGIN_INTERFACE_VERSION) {
+    return false;
+  }
+
+  return true;
+}
+
+const PluginInformation my_info = {
+    .size = sizeof(my_info),
+    .version = FD_PLUGIN_INTERFACE_VERSION,
+    .plugin_magic = FD_PLUGIN_MAGIC,
+    .plugin_license = "Bareos AGPLv3",
+    .plugin_author = "Sebastian Sura",
+    .plugin_date = "Juli 2025",
+    .plugin_version = "0.1.0",
+    .plugin_description
+    = "This plugin allows you to backup your windows system for disaster "
+      "recovery.",
+    .plugin_usage = "...",
+};
+
+struct plugin_arguments {
+  static plugin_arguments parse(std::string_view) { return {}; }
+};
+
+struct plugin_logger : public GenericLogger {
+  void Begin(std::size_t FileSize) override { (void)FileSize; }
+  void Progressed(std::size_t Amount) override { (void)Amount; }
+  void End() override {}
+
+  void SetStatus(std::string_view Status) override { (void)Status; }
+  void Info(std::string_view Message) override
+  {
+    JobLog(ctx, M_INFO, "{}", Message);
+  }
+
+  plugin_logger(PluginContext* ctx_) : ctx{ctx_} {}
+
+ private:
+  PluginContext* ctx;
+};
+
+struct session_ctx {
+  plugin_logger logger;
+
+  session_ctx(PluginContext* ctx) : logger{ctx} {}
+
+  ~session_ctx() {}
+};
+
+struct plugin_ctx {
+  void set_plugin_args(plugin_arguments) {}
+
+  void begin_session(PluginContext* ctx) { current_session.emplace(ctx); }
+
+  std::size_t session_write(std::span<char> data)
+  {
+    (void)data;
+    return 0;
+  }
+
+  bool has_session() const { return current_session.has_value(); }
+
+  std::optional<session_ctx> current_session;
+};
+
+plugin_ctx* get_private_context(PluginContext* ctx)
+{
+  return reinterpret_cast<plugin_ctx*>(ctx->plugin_private_context);
+}
+
+void set_private_context(PluginContext* ctx, plugin_ctx* priv_ctx)
+{
+  ctx->plugin_private_context = priv_ctx;
+}
+
+bRC newPlugin(PluginContext* ctx)
+{
+  set_private_context(ctx, new plugin_ctx);
+  RegisterBareosEvent(ctx, filedaemon::bEventPluginCommand);
+  RegisterBareosEvent(ctx, filedaemon::bEventNewPluginOptions);
+  RegisterBareosEvent(ctx, filedaemon::bEventPluginCommand);
+  RegisterBareosEvent(ctx, filedaemon::bEventJobStart);
+  // RegisterBareosEvent(ctx, filedaemon::bEventStartBackupJob);
+  RegisterBareosEvent(ctx, filedaemon::bEventRestoreCommand);
+  RegisterBareosEvent(ctx, filedaemon::bEventEstimateCommand);
+  RegisterBareosEvent(ctx, filedaemon::bEventBackupCommand);
+  RegisterBareosEvent(ctx, filedaemon::bEventRestoreObject);
+  return bRC_OK;
+}
+
+bRC freePlugin(PluginContext* ctx)
+{
+  auto* pctx = get_private_context(ctx);
+  set_private_context(ctx, nullptr);
+  delete pctx;
+  return bRC_OK;
+}
+
+bRC getPluginValue(PluginContext*, filedaemon::pVariable, void*)
+{
+  /* UNUSED */
+  return bRC_Error;
+}
+bRC setPluginValue(PluginContext*, filedaemon::pVariable, void*)
+{
+  /* UNUSED */
+  return bRC_Error;
+}
+
+bRC handlePluginEvent(PluginContext* ctx, filedaemon::bEvent* event, void* data)
+{
+  auto* pctx = get_private_context(ctx);
+  switch (event->eventType) {
+    case filedaemon::bEventPluginCommand: {
+      auto arguments = plugin_arguments::parse(static_cast<const char*>(data));
+      pctx->set_plugin_args(std::move(arguments));
+      return bRC_OK;
+    } break;
+
+    case filedaemon::bEventNewPluginOptions:
+      [[fallthrough]];
+    case filedaemon::bEventBackupCommand:
+      [[fallthrough]];
+    case filedaemon::bEventEstimateCommand:
+      [[fallthrough]];
+    case filedaemon::bEventRestoreCommand: {
+      auto arguments = plugin_arguments::parse(static_cast<const char*>(data));
+      pctx->set_plugin_args(std::move(arguments));
+      return bRC_OK;
+    }
+  }
+  return bRC_Error;
+}
+
+bRC startBackupFile(PluginContext* ctx, filedaemon::save_pkt* sp)
+{
+  (void)sp;
+  err_msg(ctx, "backups are only supported on windows");
+  return bRC_Error;
+}
+bRC endBackupFile(PluginContext* ctx)
+{
+  (void)ctx;
+  return bRC_Error;
+}
+bRC startRestoreFile(PluginContext* ctx, const char* file_name)
+{
+  (void)ctx;
+  (void)file_name;
+  return bRC_Error;
+}
+bRC endRestoreFile(PluginContext* ctx)
+{
+  (void)ctx;
+  return bRC_Error;
+}
+bRC pluginIO(PluginContext* ctx, filedaemon::io_pkt* pkt)
+{
+  auto* pctx = get_private_context(ctx);
+  (void)pctx;
+  switch (pkt->func) {
+    case filedaemon::IO_OPEN: {
+    } break;
+    case filedaemon::IO_READ: {
+    } break;
+    case filedaemon::IO_WRITE: {
+    } break;
+    case filedaemon::IO_CLOSE: {
+    } break;
+  }
+  return bRC_Error;
+}
+bRC createFile(PluginContext* ctx, filedaemon::restore_pkt* pkt)
+{
+  (void)ctx;
+  (void)pkt;
+  return bRC_OK;
+}
+bRC setFileAttributes(PluginContext* ctx, filedaemon::restore_pkt* pkt)
+{
+  (void)ctx;
+  (void)pkt;
+  return bRC_OK;
+}
+bRC checkFile(PluginContext* ctx, char* file_name)
+{
+  (void)ctx;
+  (void)file_name;
+  return bRC_Error;
+}
+bRC getAcl(PluginContext* ctx, filedaemon::acl_pkt* pkt)
+{
+  (void)ctx;
+  (void)pkt;
+  return bRC_Error;
+}
+bRC setAcl(PluginContext* ctx, filedaemon::acl_pkt* pkt)
+{
+  (void)ctx;
+  (void)pkt;
+  return bRC_Error;
+}
+bRC getXattr(PluginContext* ctx, filedaemon::xattr_pkt* pkt)
+{
+  (void)ctx;
+  (void)pkt;
+  return bRC_Error;
+}
+bRC setXattr(PluginContext* ctx, filedaemon::xattr_pkt* pkt)
+{
+  (void)ctx;
+  (void)pkt;
+  return bRC_Error;
+}
+
+const filedaemon::PluginFunctions my_functions = {
+    .size = sizeof(my_functions),
+    .version = FD_PLUGIN_INTERFACE_VERSION,
+    .newPlugin = &newPlugin,
+    .freePlugin = &freePlugin,
+    .getPluginValue = &getPluginValue,
+    .setPluginValue = &setPluginValue,
+    .handlePluginEvent = &handlePluginEvent,
+    .startBackupFile = &startBackupFile,
+    .endBackupFile = &endBackupFile,
+    .startRestoreFile = &startRestoreFile,
+    .endRestoreFile = &endRestoreFile,
+    .pluginIO = &pluginIO,
+    .createFile = &createFile,
+    .setFileAttributes = &setFileAttributes,
+    .checkFile = &checkFile,
+    .getAcl = &getAcl,
+    .setAcl = &setAcl,
+    .getXattr = &getXattr,
+    .setXattr = &setXattr,
+};
+
+extern "C" int loadPlugin(filedaemon::PluginApiDefinition* core_info,
+                          filedaemon::CoreFunctions* core_funcs,
+                          PluginInformation** plugin_info,
+                          filedaemon::PluginFunctions** plugin_funcs)
+{
+  if (!SetupBareosApi(core_funcs)) { return -1; }
+
+  if (!AmICompatibleWith(core_info)) { return -1; }
+
+  *plugin_info = const_cast<PluginInformation*>(&my_info);
+  *plugin_funcs = const_cast<filedaemon::PluginFunctions*>(&my_functions);
+
+  return 0;
+}
+
+extern "C" int unloadPlugin() { return 0; }
