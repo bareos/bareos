2c5c0ddfb95f4bc6b9b070932d6f56cddf96bd4e
Bruno Friedmann <bruno.friedmann@bareos.com>
2025-07-31 14:08:31 +0200

    ndmp: renew systemtest ndmp-bareos ndmp-native
    - modularize ndmp-bareos ndmp-native
    - ndmp can be now tested with omnios
    - cmake: ndmp introduce -Dndmp_config parameter
    
    Based on CTestNDMPConfig.cmake.template you can create a file
    with all the parameter set for being able to build and run systemtest
    ndmp-bareos ndmp-native on either remote isilon or omnios.
    
    new variable in Ctest config replace environment.local.in, which is
    dropped
    
    `Auth Type` parameter is now configurable
    
    In your CTestNDMPConfig.cmake you can set the two used auth type
    `ndmp_data_agent_auth_type` and `ndmp_tape_and_robot_auth_type`
    
    - introduce "DOFULLFAILED"
    
    Setting DOFULLFAILED allow to trigger a first full without data
    preparation which then will failed, and will be rerun automatically.
    Checking for bug no Level in rerun
    
    printf was introduce to limit error exit due to null byte warning in
    grep command: to be checked if really printf is needed
    
    run copy, client and fileset were added due to misused of fileset id
    when the selection is created on first run.
    (fileset isilon is added to database while not used for omnios jobs)
    
    - use printf on all restore check so it filter the null byte warning
      on omnios
    - factorize all common ndmp function in scripts/function-ndmp
    - add test comparing with bscan original and copy
    - systemtest function: use .api2 and .sql for last_job_id
    - use add_alphabetic_requirements
    - in restore when= make date format FreeBSD friendly
    
    wip: check_for_zombie_jobs is not able to handle ndmp status client
         do be uncommented once fixed
    wip: ndmp-native references issues internal/issues#445
         internal/issues#446 internal/issues#447
    wip: ndmp-bareos add internal/issues#453 internal/issues#447
    wip: internal/issues#349 copy loose data

--- core/src/dird/job.cc
+++ core/src/dird/job.cc
@@ -1540,7 +1540,6 @@ void DirdFreeJcrPointers(JobControlRecord* jcr)
 
   BfreeAndNull(jcr->sd_auth_key);
   BfreeAndNull(jcr->where);
-  BfreeAndNull(jcr->dir_impl->backup_format);
   BfreeAndNull(jcr->RestoreBootstrap);
   BfreeAndNull(jcr->ar);
 
