85f968cbbb8acded452466b2aa4b98500b99db30
Stephan Duehr <stephan.duehr@bareos.com>
2025-08-08 21:34:27 +0200

    VMware Plugin: Adapt to pyVmomi 9
    The import of the VmomiJSONEncoder was changed in pyVmomi9, this change
    adapts the Bareos VMware plugin so that it works with both the new and
    the old pyVmomi versions.

--- core/src/plugins/filed/python/vmware/bareos-fd-vmware.py
+++ core/src/plugins/filed/python/vmware/bareos-fd-vmware.py
@@ -51,7 +51,12 @@ from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
 from pyVmomi import vmodl
 import pyVim.task
-from pyVmomi.VmomiSupport import VmomiJSONEncoder
+
+# adapt to change in pyVmomi 9.x
+try:
+    from pyVmomi.VmomiJSONEncoder import VmomiJSONEncoder
+except ImportError:
+    from pyVmomi.VmomiSupport import VmomiJSONEncoder
 
 # if OrderedDict is not available from collection (eg. SLES11),
 # the additional package python-ordereddict must be used
