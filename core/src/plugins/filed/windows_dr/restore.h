87d8db2cfa331d9c19e8a9a715bc0b6b0dd5454e
Sebastian Sura <sebastian.sura@bareos.com>
2025-09-09 16:38:24 +0200

    [todo] implement barri backup to files on windows

--- core/src/plugins/filed/windows_dr/restore.h
+++ core/src/plugins/filed/windows_dr/restore.h
@@ -22,21 +22,13 @@
 #ifndef BAREOS_PLUGINS_FILED_WINDOWS_DR_RESTORE_H_
 #define BAREOS_PLUGINS_FILED_WINDOWS_DR_RESTORE_H_
 
-#if !defined(NEW_RESTORE)
-#  include <istream>
-#  include "plugins/filed/windows_dr/parser.h"
-
-
-void do_restore(std::istream& stream, GenericLogger* logger, bool raw_file);
-#else
-#  include "restore_options.h"
-#  include <span>
+#include "restore_options.h"
+#include <span>
 
 struct data_writer;
 
 data_writer* writer_begin(restore_options options);
 std::size_t writer_write(data_writer* writer, std::span<char> data);
 void writer_end(data_writer* writer);
-#endif
 
 #endif  // BAREOS_PLUGINS_FILED_WINDOWS_DR_RESTORE_H_
