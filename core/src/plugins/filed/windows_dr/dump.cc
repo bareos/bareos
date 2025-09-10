c31c3d8a73ea17e3e1302297eb3f271118c87222
Sebastian Sura <sebastian.sura@bareos.com>
2025-09-10 08:39:39 +0200

    barri-dump: move util functions in their own header

--- core/src/plugins/filed/windows_dr/dump.cc
+++ core/src/plugins/filed/windows_dr/dump.cc
@@ -51,46 +51,7 @@
 #include "com.h"
 #include "remove_holes.h"
 
-namespace {
-std::string FromUtf16(std::wstring_view utf16)
-{
-  // WideCharToMultiByte does not handle empty strings
-  if (utf16.size() == 0) { return {}; }
-
-  // if the buffer is to small (or not supplied) the function returns
-  // the number of bytes required
-  DWORD required = WideCharToMultiByte(CP_UTF8, 0, utf16.data(), utf16.size(),
-                                       nullptr, 0, nullptr, nullptr);
-  if (required == 0) { return "<impossible conversion>"; }
-  std::string utf8(required, '\0');
-
-  // if the buffer is big enough the function returns the number of
-  // bytes written
-  DWORD written
-      = WideCharToMultiByte(CP_UTF8, 0, utf16.data(), utf16.size(), utf8.data(),
-                            utf8.size(), nullptr, nullptr);
-
-  if (written != required) { return "<impossible conversion>"; }
-
-  utf8.resize(written);
-  return utf8;
-}
-
-std::string FromUtf16(const wchar_t* utf16p)
-{
-  if (!utf16p) { return {}; }
-  return FromUtf16(std::wstring_view{utf16p});
-}
-
-bool GetBit(unsigned char* data, std::size_t index)
-{
-  return (data[index / CHAR_BIT] >> (index % CHAR_BIT)) & 0x1;
-}
-
-template <class... Ts> struct overloads : Ts... {
-  using Ts::operator()...;
-};
-};  // namespace
+#include "util.h"
 
 std::size_t compute_plan_size(const insert_plan& plan)
 {
