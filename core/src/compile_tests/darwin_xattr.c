b43d4fb33f9c4aa7077eb2a4a4dd815f73424013
Andreas Rogge <andreas.rogge@bareos.com>
2025-09-25 15:04:50 +0200

    linux/macos: merge xattr defines
    Linux and MacOS both use getxattr()/setxattr() but there are subtle
    differences. Thus we need to check for both individually.

---
+++ core/src/compile_tests/darwin_xattr.c
@@ -0,0 +1,42 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2025-2025 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include <sys/types.h>
+#include <sys/xattr.h>
+
+void* ptr;
+void* ptr2;
+void* ptr3;
+void* ptr4;
+
+int main(int argc, char** argv)
+{
+  (void)argc;
+  (void)argv;
+
+  // MacOS/Darwin extended attribute functions
+  getxattr(ptr, ptr2, ptr3, 0, 0, 0);
+  getxattr(ptr, ptr2, ptr3, 0, 0, XATTR_NOFOLLOW);
+  listxattr(ptr, ptr2, 0, 0);
+  listxattr(ptr, ptr2, 0, XATTR_NOFOLLOW);
+  setxattr(ptr, ptr2, ptr3, 0, 0, 0);
+  setxattr(ptr, ptr2, ptr3, 0, 0, XATTR_NOFOLLOW);
+}
