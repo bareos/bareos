ebf69b87784c4da719d203487b30d3740528da71
Andreas Rogge <andreas.rogge@bareos.com>
2025-09-25 15:37:48 +0200

    remove HAVE_SYS_TIME_H
    this is true for all our platforms, so no conditional needed.
    Also TIME_WITH_SYS_TIME is never set, so we can remove that, too.

--- core/src/include/baconfig.h
+++ core/src/include/baconfig.h
@@ -197,16 +197,7 @@ typedef int(INTHANDLER)();
 #  define INADDR_NONE ((unsigned long)-1)
 #endif
 
-#ifdef TIME_WITH_SYS_TIME
-#  include <sys/time.h>
-#  include <time.h>
-#else
-#  ifdef HAVE_SYS_TIME_H
-#    include <sys/time.h>
-#  else
-#    include <time.h>
-#  endif
-#endif
+#include <sys/time.h>
 
 #ifndef O_BINARY
 #  define O_BINARY 0
