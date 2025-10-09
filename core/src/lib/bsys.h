52536cd7fdfb7afb89c0ba28f836041724ad18fc
Andreas Rogge <andreas.rogge@bareos.com>
2025-10-09 18:04:23 +0200

    lib: refactor drop() privileges
    * stop compiling an empty stub for windows
    * skip calling drop() on windows
    * remove now unused HAVE_GRP_H/HAVE_PWD_H

--- core/src/lib/bsys.h
+++ core/src/lib/bsys.h
@@ -48,7 +48,6 @@ void WritePidFile(int pidfile_fd,
                   const char* pidfile_path,
                   const char* progname);
 int DeletePidFile(std::string pidfile_path);
-void drop(char* uid, char* gid, bool keep_readall_caps);
 int Bmicrosleep(int32_t sec, int32_t usec);
 char* bfgets(char* s, int size, FILE* fd);
 char* bfgets(POOLMEM*& s, FILE* fd);
