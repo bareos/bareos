49c15c16adf907f09b559b1af0724bc962819075
Andreas Rogge <andreas.rogge@bareos.com>
2025-10-10 10:49:10 +0200

    cmake: remove HAVE_GETHOSTBYNAME2
    this is never used.

--- core/cmake/BareosCheckFunctions.cmake
+++ core/cmake/BareosCheckFunctions.cmake
@@ -23,7 +23,6 @@ if(NOT MSVC)
     ${PROJECT_SOURCE_DIR}/src/compile_tests/core_system_interfaces.c
   )
   if(HAVE_CORE_SYSTEM_INTERFACES)
-    set(HAVE_GETHOSTBYNAME2 1)
     set(HAVE_GETPAGESIZE 1)
     set(HAVE_LOCALTIME_R 1)
     set(HAVE_NANOSLEEP 1)
