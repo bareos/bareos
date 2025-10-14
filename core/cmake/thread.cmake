2916012f921659982763b455a5955ee4391199ea
Andreas Rogge <andreas.rogge@bareos.com>
2025-10-14 15:55:32 +0200

    cmake: use __has_include(<pthread_np.h>)

--- core/cmake/thread.cmake
+++ core/cmake/thread.cmake
@@ -24,28 +24,17 @@ include(CMakePushCheckState)
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
 
-# check for extra non-portable header-file
-check_include_files("pthread.h;pthread_np.h" HAVE_PTHREAD_NP_H)
-
 # pthread_attr_get_np - e.g. on FreeBSD
 cmake_push_check_state()
 set(CMAKE_REQUIRED_LIBRARIES ${PTHREAD_LIBRARIES})
-if(HAVE_PTHREAD_NP_H)
-  check_c_source_compiles(
-    "
-    #include <pthread.h>
-    #include <pthread_np.h>
-    int main() { pthread_attr_t a; pthread_attr_get_np(pthread_self(), &a); }
-    "
-    HAVE_PTHREAD_ATTR_GET_NP
-  )
-else()
-  check_c_source_compiles(
-    "
-    #include <pthread.h>
-    int main() { pthread_attr_t a; pthread_attr_get_np(pthread_self(), &a); }
-    "
-    HAVE_PTHREAD_ATTR_GET_NP
-  )
-endif()
+check_c_source_compiles(
+  "
+  #include <pthread.h>
+  #if __has_include(<pthread_np.h>)
+  #  include <pthread_np.h>
+  #endif
+  int main() { pthread_attr_t a; pthread_attr_get_np(pthread_self(), &a); }
+  "
+  HAVE_PTHREAD_ATTR_GET_NP
+)
 cmake_pop_check_state()
