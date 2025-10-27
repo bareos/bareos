04c97ab3f204127cc4d475ed54288e4473495593
Andreas Rogge <andreas.rogge@bareos.com>
2025-10-27 15:36:56 +0100

    apply bareos-check-sources
    (cherry picked from commit abfd6727c14c985c4fdb9a9f49437f418677c4c9)

--- core/src/droplet/libdroplet/src/backend/cdmi/crcmodel.c
+++ core/src/droplet/libdroplet/src/backend/cdmi/crcmodel.c
@@ -43,7 +43,8 @@
 LOCAL ulong reflect P_((ulong v, int b));
 /* Returns the value v with the bottom b [0,32] bits reflected. */
 /* Example: reflect(0x3e23L,3) == 0x3e26                        */
-LOCAL ulong reflect(ulong v, int b) {
+LOCAL ulong reflect(ulong v, int b)
+{
   int i;
   ulong t = v;
   for (i = 0; i < b; i++) {
@@ -61,19 +62,19 @@ LOCAL ulong reflect(ulong v, int b) {
 LOCAL ulong widmask P_((p_cm_t));
 /* Returns a longword whose value is (2^p_cm->cm_width)-1.     */
 /* The trick is to do this portably (e.g. without doing <<32). */
-LOCAL ulong widmask(p_cm_t p_cm) {
+LOCAL ulong widmask(p_cm_t p_cm)
+{
   return (((1L << (p_cm->cm_width - 1)) - 1L) << 1) | 1L;
 }
 
 /******************************************************************************/
 
-void cm_ini(p_cm_t p_cm) {
-  p_cm->cm_reg = p_cm->cm_init;
-}
+void cm_ini(p_cm_t p_cm) { p_cm->cm_reg = p_cm->cm_init; }
 
 /******************************************************************************/
 
-void cm_nxt(p_cm_t p_cm, int ch) {
+void cm_nxt(p_cm_t p_cm, int ch)
+{
   int i;
   ulong uch = (ulong)ch;
   ulong topbit = BITMASK(p_cm->cm_width - 1);
@@ -91,13 +92,15 @@ void cm_nxt(p_cm_t p_cm, int ch) {
 
 /******************************************************************************/
 
-void cm_blk(p_cm_t p_cm, p_ubyte_ blk_adr, ulong blk_len) {
+void cm_blk(p_cm_t p_cm, p_ubyte_ blk_adr, ulong blk_len)
+{
   while (blk_len--) cm_nxt(p_cm, *blk_adr++);
 }
 
 /******************************************************************************/
 
-ulong cm_crc(p_cm_t p_cm) {
+ulong cm_crc(p_cm_t p_cm)
+{
   if (p_cm->cm_refot)
     return p_cm->cm_xorot ^ reflect(p_cm->cm_reg, p_cm->cm_width);
   else
@@ -106,7 +109,8 @@ ulong cm_crc(p_cm_t p_cm) {
 
 /******************************************************************************/
 
-ulong cm_tab(p_cm_t p_cm, int index) {
+ulong cm_tab(p_cm_t p_cm, int index)
+{
   int i;
   ulong r;
   ulong topbit = BITMASK(p_cm->cm_width - 1);
