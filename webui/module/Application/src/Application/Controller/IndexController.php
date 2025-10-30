84e4039c59d4a0e4af0ccae8ab117b0360c7868d
Sebastian Sura <sebastian.sura@bareos.com>
2025-10-30 07:36:36 +0100

    webui: fix copyright information

--- webui/module/Application/src/Application/Controller/IndexController.php
+++ webui/module/Application/src/Application/Controller/IndexController.php
@@ -5,7 +5,7 @@
  *
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @copyright Copyright (c) 2013-2023 Bareos GmbH & Co. KG
+ * @copyright Copyright (C) 2013-2025 Bareos GmbH & Co. KG
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
--- webui/module/Application/src/Application/View/Helper/UpdateAlert.php
+++ webui/module/Application/src/Application/View/Helper/UpdateAlert.php
@@ -5,7 +5,7 @@
  * bareos-webui - Bareos Web-Frontend
  *
  * @link      https://github.com/bareos/bareos for the canonical source repository
- * @copyright Copyright (C) 2013-2024 Bareos GmbH & Co. KG (http://www.bareos.org/)
+ * @copyright Copyright (C) 2013-2025 Bareos GmbH & Co. KG (http://www.bareos.org/)
  * @license   GNU Affero General Public License (http://www.gnu.org/licenses/)
  * @author    Frank Bergkemper
  *
