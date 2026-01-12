cae5c6fb7d3526baf97e51a70f9a76ca45f46cda
Sebastian Sura <sebastian.sura@bareos.com>
2026-01-12 07:44:05 +0100

    python-sd: use new python-common module

---
+++ core/src/plugins/stored/python/module.h
@@ -0,0 +1,36 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2026-2026 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#ifndef BAREOS_PLUGINS_STORED_PYTHON_MODULE_H_
+#define BAREOS_PLUGINS_STORED_PYTHON_MODULE_H_
+
+#if defined(HAVE_WIN32)
+#  include "include/bareos.h"
+#  include <Python.h>
+#else
+#  include <Python.h>
+#  include "include/bareos.h"
+#endif
+#include "stored/sd_plugins.h"
+
+PyObject* make_module(PluginContext* ctx, storagedaemon::CoreFunctions* funs);
+
+#endif  // BAREOS_PLUGINS_STORED_PYTHON_MODULE_H_
