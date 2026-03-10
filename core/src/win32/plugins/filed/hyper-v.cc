75a7edac92f26c1d6089174b4f57a786b78ec01d
Sebastian Sura <sebastian.sura@bareos.com>
2026-03-10 07:55:19 +0100

    hyper-v: fix typo

--- core/src/win32/plugins/filed/hyper-v.cc
+++ core/src/win32/plugins/filed/hyper-v.cc
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2025-2025 Bareos GmbH & Co. KG
+   Copyright (C) 2025-2026 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -1686,7 +1686,7 @@ class VirtualSystemSnapshotService;
 
 struct Snapshot : ClassObject {
   static constexpr std::wstring_view class_name{
-      L"CIM_VirtualSystemSettingData"};
+      L"Msvm_VirtualSystemSettingData"};
 };
 
 struct VirtualSystemSnapshotSettingData {
