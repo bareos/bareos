191538323e7c6267ddd9634492367ad5c676132a
Philipp Storz <philipp.storz@bareos.com>
2026-04-17 20:03:10 +0200

    Restore webproxy systemd unit
    Restore the bareos-webui-proxy systemd service
    file and Debian packaging snippets that were
    missing in this PR.
    
    This brings back installation and startup wiring
    for the WebUI proxy package.

--- cmake/BareosGenerateDebianInfo.cmake
+++ cmake/BareosGenerateDebianInfo.cmake
@@ -83,6 +83,7 @@ else()
 
   if(NOT client-only)
     list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui")
+    list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui-proxy")
   endif()
 
   if(NOT client-only AND VIXDISKLIB_FOUND)
