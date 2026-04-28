a8ea597fda21b43932ec067f4f0da2a37d49b28d
Philipp Storz <philipp.storz@bareos.com>
2026-04-28 18:11:32 +0200

    Normalize volume encryption flag
    Add HasEncryptionKey to the short volume list query so API2\nvolume records always expose the same attribute set as the\nlong query.\n\nAdd a focused SQL query test to keep both predefined volume\nqueries aligned.

---
+++ core/src/tests/sql_query_test.cc
@@ -0,0 +1,55 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2026-2026 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+ */
+
+#include <gtest/gtest.h>
+
+#include <string_view>
+
+#include "cats/postgresql.h"
+
+namespace {
+
+#if HAVE_POSTGRESQL
+constexpr std::string_view kHasEncryptionKeyColumn = "END AS HasEncryptionKey";
+
+BareosDbPostgresql CreateDb()
+{
+  return BareosDbPostgresql(nullptr, "", "", "", nullptr, nullptr, 0, nullptr,
+                            false, false, false, false, false);
+}
+
+TEST(SqlQueryTest, VolumeQueriesAlwaysExposeHasEncryptionKey)
+{
+  auto db = CreateDb();
+
+  const auto short_query = std::string_view(
+      db.get_predefined_query(BareosDb::SQL_QUERY::list_volumes_select_0));
+  const auto long_query = std::string_view(
+      db.get_predefined_query(BareosDb::SQL_QUERY::list_volumes_select_long_0));
+
+  EXPECT_NE(short_query.find(kHasEncryptionKeyColumn), std::string_view::npos);
+  EXPECT_NE(long_query.find(kHasEncryptionKeyColumn), std::string_view::npos);
+}
+#else
+TEST(SqlQueryTest, DISABLED_VolumeQueriesAlwaysExposeHasEncryptionKey) {}
+#endif
+
+}  // namespace
