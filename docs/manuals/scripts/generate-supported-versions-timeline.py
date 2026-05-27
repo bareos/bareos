ac3090b3a7e2c255d00883286b428173035eca96
Philipp Storz <philipp.storz@bareos.com>
2026-05-27 13:30:01 +0200

    Fix timeline script for Python 3.8
    Add postponed evaluation of annotations so the docs
    generator runs on Jenkins with Python 3.8.

--- docs/manuals/scripts/generate-supported-versions-timeline.py
+++ docs/manuals/scripts/generate-supported-versions-timeline.py
@@ -18,6 +18,8 @@
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #   02110-1301, USA.
 
+from __future__ import annotations
+
 """Generate a support timeline SVG for Bareos major releases."""
 
 import argparse
