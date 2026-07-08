1e6f7418ab5271211dad1bfd9d9bb38a58283da4
Philipp Storz <philipp.storz@bareos.com>
2026-07-08 12:19:47 +0200

    packaging: use /bareos-webui-new URL for Vue webui
    In the bareos-25 backport the PHP webui must remain at
    /bareos-webui so existing users are unaffected. The Vue
    webui (technical preview) is placed at /bareos-webui-new.
    
    Changes:
    - Rename Vue Apache conf to bareos-webui-new.conf.in,
      update all URL/path references to /bareos-webui-new
    - Restore PHP Apache conf to bareos-webui.conf.in at
      /bareos-webui
    - webui-vue/CMakeLists.txt: install to bareos-webui-new
    - webui/CMakeLists.txt: install to bareos-webui
    - Debian: rename packages (bareos-webui-new for Vue,
      bareos-webui for PHP), update install/postinst files
    - RPM spec: same package rename
    - Selenium tests: update default base URLs
    - pkglists: replace bareos-webui-vue with bareos-webui-new
    - systemtests: update PHP webui path reference

--- cmake/BareosGenerateDebianInfo.cmake
+++ cmake/BareosGenerateDebianInfo.cmake
@@ -1,6 +1,6 @@
 #   BAREOS® - Backup Archiving REcovery Open Sourced
 #
-#   Copyright (C) 2018-2025 Bareos GmbH & Co. KG
+#   Copyright (C) 2018-2026 Bareos GmbH & Co. KG
 #
 #   This program is Free Software; you can redistribute it and/or
 #   modify it under the terms of version three of the GNU Affero General Public
@@ -91,9 +91,9 @@ else()
   endif()
 
   if(NOT client-only)
-    list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui-php")
-    list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui-proxy")
     list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui")
+    list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui-proxy")
+    list(APPEND DEBIAN_CONTROL_SNIPPETS "bareos-webui-new")
   endif()
 
   if(NOT client-only AND VIXDISKLIB_FOUND)
