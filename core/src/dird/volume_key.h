c2acec32950d998a5d6629ebc2012cac55a56bdc
Andreas Rogge <andreas.rogge@bareos.com>
2026-07-10 09:33:34 +0200

    dird: refactor volume-key generation
    pull out of ua_label.cc so it is reusable.

--- core/src/dird/volume_key.h
+++ core/src/dird/volume_key.h
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2018-2026 Bareos GmbH & Co. KG
+   Copyright (C) 2026-2026 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -18,10 +18,15 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301, USA.
 */
-#ifndef BAREOS_LIB_PASSPHRASE_H_
-#define BAREOS_LIB_PASSPHRASE_H_
 
-#include <cstdint>
-char* generate_crypto_passphrase(uint16_t length);
+#ifndef BAREOS_DIRD_VOLUME_KEY_H_
+#define BAREOS_DIRD_VOLUME_KEY_H_
 
-#endif  // BAREOS_LIB_PASSPHRASE_H_
+struct MediaDbRecord;
+
+namespace directordaemon {
+class UaContext;
+bool GenerateNewEncryptionKey(UaContext* ua, MediaDbRecord* mr);
+}  // namespace directordaemon
+
+#endif  // BAREOS_DIRD_VOLUME_KEY_H_
