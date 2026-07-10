323567faaa9648db437e2fb8701573eec036b3b0
Andreas Rogge <andreas.rogge@bareos.com>
2026-07-10 09:33:34 +0200

    dird: refactor volume-key generation
    pull out of ua_label.cc so it is reusable.
    
    (cherry picked from commit c2acec32950d998a5d6629ebc2012cac55a56bdc)

--- core/src/dird/ua_label.cc
+++ core/src/dird/ua_label.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2003-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2025 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2026 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -45,8 +45,7 @@
 #include "dird/ua_label.h"
 #include "dird/ua_select.h"
 #include "include/auth_protocol_types.h"
-#include "lib/crypto_wrap.h"
-#include "lib/passphrase.h"
+#include "volume_key.h"
 #include "lib/util.h"
 
 namespace directordaemon {
@@ -182,64 +181,6 @@ static inline bool native_send_label_request(UaContext* ua,
   mr->VolBytes = VolBytes;
 
   return retval;
-}
-
-/*
- * Generate a new encryption key for use in volume encryption.
- * We don't ask the user for a encryption key but generate a semi
- * random passphrase of the wanted length which is way stronger.
- * When the config has a wrap key we use that to wrap the newly
- * created passphrase using RFC3394 aes wrap and always convert
- * the passphrase into a base64 encoded string. This key is
- * stored in the database and is passed to the storage daemon
- * when needed. The storage daemon has the same wrap key per
- * director so it can unwrap the passphrase for use.
- *
- * Changing the wrap key will render any previously created
- * crypto keys useless so only change the wrap key after initial
- * setting when you know what you are doing and always store
- * the old key somewhere save so you can use bscrypto to
- * convert them for the new wrap key.
- */
-static bool GenerateNewEncryptionKey(UaContext* ua, MediaDbRecord* mr)
-{
-  int length;
-  char* passphrase;
-
-  passphrase = generate_crypto_passphrase(DEFAULT_PASSPHRASE_LENGTH);
-  if (!passphrase) {
-    ua->ErrorMsg(
-        T_("Failed to generate new encryption passphrase for Volume %s.\n"),
-        mr->VolumeName);
-    return false;
-  }
-
-  // See if we need to wrap the passphrase.
-  if (me->keyencrkey.value) {
-    char* wrapped_passphrase;
-
-    length = DEFAULT_PASSPHRASE_LENGTH + 8;
-    wrapped_passphrase = (char*)malloc(length);
-    memset(wrapped_passphrase, 0, length);
-    if (auto error = AesWrap(
-            (unsigned char*)me->keyencrkey.value, DEFAULT_PASSPHRASE_LENGTH / 8,
-            (unsigned char*)passphrase, (unsigned char*)wrapped_passphrase)) {
-      ua->ErrorMsg(T_("Failed to wrap passphrase ERR=%s.\n"), error->c_str());
-      free(passphrase);
-      return false;
-    }
-
-    free(passphrase);
-    passphrase = wrapped_passphrase;
-  } else {
-    length = DEFAULT_PASSPHRASE_LENGTH;
-  }
-
-  // The passphrase is always base64 encoded.
-  BinToBase64(mr->EncrKey, sizeof(mr->EncrKey), passphrase, length, true);
-
-  free(passphrase);
-  return true;
 }
 
 bool SendLabelRequest(UaContext* ua,
