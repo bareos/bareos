08ab9ca317df5d9a217e7498cc1ee753f69d2b6e
Philipp Storz <philipp.storz@bareos.com>
2026-07-14 18:11:05 +0200

    webui-vue: fix selenium test copyright headers
    Use the repository-required copyright range format in both
    selenium test files so bareos-check-sources recognizes the
    headers correctly.
    
    (cherry picked from commit 312697d66f2beb7692caf2e7b3e4286b38cbfb4c)

--- webui-vue/tests/selenium/webui-vue-selenium-test.sh
+++ webui-vue/tests/selenium/webui-vue-selenium-test.sh
@@ -2,7 +2,7 @@
 
 #   BAREOS® - Backup Archiving REcovery Open Sourced
 #
-#   Copyright (C) 2026 Bareos GmbH & Co. KG
+#   Copyright (C) 2026-2026 Bareos GmbH & Co. KG
 #
 #   This program is free software; you can redistribute it and/or modify it
 #   under the terms of version three of the GNU Affero General Public License
