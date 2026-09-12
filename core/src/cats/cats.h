bd951ac7e012e039d9112dcc3b181b2d03735261
Philipp Storz <philipp.storz@bareos.com>
2026-09-12 21:08:07 +0200

    cats: escape client list filter
    (cherry picked from commit 89dc6ff999964ce664d1975f2696a79093f7c230)

--- core/src/cats/cats.h
+++ core/src/cats/cats.h
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2016 Planets Communications B.V.
-   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2026 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -863,7 +863,7 @@ class BareosDb : public BareosDbQueryEnum {
                     const CollapseMode collapse = CollapseMode::NoCollapse);
 
   void ListClientRecords(JobControlRecord* jcr,
-                         char* clientname,
+                         const char* clientname,
                          OutputFormatter* sendit,
                          e_list_type type);
   void ListCopiesRecords(JobControlRecord* jcr,
