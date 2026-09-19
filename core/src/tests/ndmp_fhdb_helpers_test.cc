f4d6249e43f8d79f4ac6d60fe6bf72c97030ab1d
Philipp Storz <philipp.storz@bareos.com>
2026-09-19 22:19:34 +0200

    tests: add attribs and NDMP fhdb helper unit tests
    Add attribs_test.cc: round-trip EncodeStat()/DecodeStat() tests for
    zero, small, >4GB, and non-block-aligned file sizes -- no existing
    lstat-encoding test previously existed.
    
    Add ndmp_fhdb_helpers_test.cc: covers NdmpConvertFstat() populating
    st_size/st_blksize/st_blocks from a valid ndmp9_file_stat, and
    leaving them at zero when the size field is marked invalid.
    
    Register both new GoogleTest targets in CMakeLists.txt.

---
+++ core/src/tests/ndmp_fhdb_helpers_test.cc
@@ -0,0 +1,91 @@
+/*
+   BAREOS® - Backup Archiving REcovery Open Sourced
+
+   Copyright (C) 2026-2026 Bareos GmbH & Co. KG
+
+   This program is Free Software; you can redistribute it and/or
+   modify it under the terms of version three of the GNU Affero General Public
+   License as published by the Free Software Foundation and included
+   in the file LICENSE.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+#include "gtest/gtest.h"
+#include "include/bareos.h"
+#include "include/filetypes.h"
+
+#include "dird/dird.h"
+#include "ndmp/ndmagents.h"
+#include "dird/ndmp_dma_priv.h"
+#include "lib/attribs.h"
+#include "lib/mem_pool.h"
+
+namespace {
+struct stat DecodeConvertedFstat(ndmp9_file_stat& fstat)
+{
+  int8_t FileType = FT_REG;
+  PoolMem attribs(PM_FNAME);
+
+  directordaemon::NdmpConvertFstat(&fstat, 1, &FileType, attribs);
+
+  struct stat statp{};
+  int32_t LinkFI;
+  DecodeStat(attribs.c_str(), &statp, sizeof(statp), &LinkFI);
+  return statp;
+}
+
+ndmp9_file_stat MakeRegularFileStat()
+{
+  ndmp9_file_stat fstat{};
+  fstat.ftype = NDMP9_FILE_REG;
+  fstat.mode.valid = NDMP9_VALIDITY_VALID;
+  fstat.mode.value = 0644;
+  return fstat;
+}
+} /* namespace */
+
+TEST(ndmp_fhdb_helpers, size_is_converted_to_st_size_and_st_blocks)
+{
+  ndmp9_file_stat fstat = MakeRegularFileStat();
+  fstat.size.valid = NDMP9_VALIDITY_VALID;
+  fstat.size.value = 513;
+
+  struct stat statp = DecodeConvertedFstat(fstat);
+
+  EXPECT_EQ(statp.st_size, 513);
+  EXPECT_EQ(statp.st_blksize, 512);
+  EXPECT_EQ(statp.st_blocks, 2); /* ceil(513 / 512) */
+}
+
+TEST(ndmp_fhdb_helpers, exact_block_multiple_size_does_not_round_up)
+{
+  ndmp9_file_stat fstat = MakeRegularFileStat();
+  fstat.size.valid = NDMP9_VALIDITY_VALID;
+  fstat.size.value = 1024;
+
+  struct stat statp = DecodeConvertedFstat(fstat);
+
+  EXPECT_EQ(statp.st_size, 1024);
+  EXPECT_EQ(statp.st_blocks, 2);
+}
+
+TEST(ndmp_fhdb_helpers, invalid_size_is_not_guessed)
+{
+  ndmp9_file_stat fstat = MakeRegularFileStat();
+  fstat.size.valid = NDMP9_VALIDITY_INVALID;
+  fstat.size.value = 999999; /* must be ignored, .valid is false */
+
+  struct stat statp = DecodeConvertedFstat(fstat);
+
+  EXPECT_EQ(statp.st_size, 0);
+  EXPECT_EQ(statp.st_blocks, 0);
+}
